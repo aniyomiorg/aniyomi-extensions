2cec32f66cab9969dc4afe7bade33e71e5e25108
Gianmarco David <blackgins@gmail.com>
2017-02-19 12:14:51 +0100

    Added extensions for mangaeden(en) and perveden(it+en) (#11)
    * Added mangaeden(it) extension
    
    * Not overriding id anymore
    
    * Fixed search selector for the case the query has no results
    
    * Added mangaeden(en) extension
    
    * Removed id override
    
    * Added perveden it+en extensions
    
    * Minor modifications: today/yesterday management, null safety and code enhancements
    
    * Type and status are now multiselect
    
    * Genres, types and statuses now are functions
    
    * Genres/types are now multiselect and minor modifications
    
    * Renamed into Manga Eden, fixed missing cover imgs and checkbox filter now are shown at the end
    
    * Changes done on mangaeden(it) has been reflected on mangaeden(en) and perveden(it/en)
    
    * Removed unnecessary code and fixed english date format
    
    * Added text filter support for categories (from paronos)
    
    * Fixed page image url
    
    * Added text filter support for categories also in perveden/en (from paronos)

--- en-mangaeden/src/eu/kanade/tachiyomi/extension/en/mangaeden/Mangaeden.kt
+++ en-mangaeden/src/eu/kanade/tachiyomi/extension/en/mangaeden/Mangaeden.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.it.mangaeden
+package eu.kanade.tachiyomi.extension.en.mangaeden
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
@@ -17,11 +17,11 @@ class Mangaeden : ParsedHttpSource() {
 
     override val baseUrl = "http://www.mangaeden.com"
 
-    override val lang = "it"
+    override val lang = "en"
 
     override val supportsLatest = true
 
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/it/it-directory/?order=3&page=$page", headers)
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/en/en-directory/?order=3&page=$page", headers)
 
     override fun latestUpdatesSelector() = searchMangaSelector()
 
@@ -29,7 +29,7 @@ class Mangaeden : ParsedHttpSource() {
 
     override fun latestUpdatesNextPageSelector() = searchMangaNextPageSelector()
 
-    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/it/it-directory/?page=$page", headers)
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/en/en-directory/?order=1&page=$page", headers)
 
     override fun popularMangaSelector() = searchMangaSelector()
 
@@ -38,7 +38,7 @@ class Mangaeden : ParsedHttpSource() {
     override fun popularMangaNextPageSelector() = searchMangaNextPageSelector()
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = HttpUrl.parse("$baseUrl/it/it-directory/").newBuilder().addQueryParameter("title", query)
+        val url = HttpUrl.parse("$baseUrl/en/en-directory/").newBuilder().addQueryParameter("title", query)
         (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
             when (filter) {
                 is StatusList -> filter.state
@@ -50,7 +50,7 @@ class Mangaeden : ParsedHttpSource() {
                         .map { it.id.toString() }
                         .forEach { url.addQueryParameter("type", it) }
                 is GenreList -> filter.state
-                        .filterNot { it.isIgnored() }
+                        .filter { !it.isIgnored() }
                         .forEach { genre -> url.addQueryParameter(if (genre.isIncluded()) "categoriesInc" else "categoriesExcl", genre.id) }
                 is TextField -> url.addQueryParameter(filter.key, filter.state)
                 is OrderBy -> filter.state?.let {
@@ -77,40 +77,40 @@ class Mangaeden : ParsedHttpSource() {
     override fun mangaDetailsParse(document: Document) = SManga.create().apply {
         val infos = document.select("div.rightbox")
 
-        author = infos.select("a[href^=/it/it-directory/?author]").first()?.text()
-        artist = infos.select("a[href^=/it/it-directory/?artist]").first()?.text()
-        genre = infos.select("a[href^=/it/it-directory/?categoriesInc]").map { it.text() }.joinToString()
+        author = infos.select("a[href^=/en/en-directory/?author]").first()?.text()
+        artist = infos.select("a[href^=/en/en-directory/?artist]").first()?.text()
+        genre = infos.select("a[href^=/en/en-directory/?categoriesInc]").map { it.text() }.joinToString()
         description = document.select("h2#mangaDescription").text()
-        status = parseStatus(infos.select("h4:containsOwn(Stato)").first()?.nextSibling().toString())
+        status = parseStatus(infos.select("h4:containsOwn(Status)").first()?.nextSibling().toString())
         val img = infos.select("div.mangaImage2 > img").first()?.attr("src")
         if (!img.isNullOrBlank()) thumbnail_url = img.let { "http:$it" }
     }
 
     private fun parseStatus(status: String) = when {
-        status.contains("In Corso", true) -> SManga.ONGOING
-        status.contains("Completato", true) -> SManga.COMPLETED
+        status.contains("Ongoing", true) -> SManga.ONGOING
+        status.contains("Completed", true) -> SManga.COMPLETED
         else -> SManga.UNKNOWN
     }
 
     override fun chapterListSelector() = "div#leftContent > table > tbody > tr"
 
     override fun chapterFromElement(element: Element) = SChapter.create().apply {
-        val a = element.select("a[href^=/it/it-manga/]").first()
+        val a = element.select("a[href^=/en/en-manga/]").first()
 
-        setUrlWithoutDomain(a?.attr("href").orEmpty())
+        setUrlWithoutDomain(a.attr("href"))
         name = a?.select("b")?.first()?.text().orEmpty()
         date_upload = element.select("td.chapterDate").first()?.text()?.let { parseChapterDate(it.trim()) } ?: 0L
     }
 
     private fun parseChapterDate(date: String): Long =
-            if ("Oggi" in date) {
+            if ("Today" in date) {
                 Calendar.getInstance().apply {
                     set(Calendar.HOUR_OF_DAY, 0)
                     set(Calendar.MINUTE, 0)
                     set(Calendar.SECOND, 0)
                     set(Calendar.MILLISECOND, 0)
                 }.timeInMillis
-            } else if ("Ieri" in date) {
+            } else if ("Yesterday" in date) {
                 Calendar.getInstance().apply {
                     add(Calendar.DATE, -1)
                     set(Calendar.HOUR_OF_DAY, 0)
@@ -119,13 +119,13 @@ class Mangaeden : ParsedHttpSource() {
                     set(Calendar.MILLISECOND, 0)
                 }.timeInMillis
             } else try {
-                SimpleDateFormat("d MMM yyyy", Locale.ITALIAN).parse(date).time
+                SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH).parse(date).time
             } catch (e: ParseException) {
                 0L
             }
 
     override fun pageListParse(document: Document): List<Page> = mutableListOf<Page>().apply {
-        document.select("option[value^=/it/it-manga/]").forEach {
+        document.select("option[value^=/en/en-manga/]").forEach {
             add(Page(size, "$baseUrl${it.attr("value")}"))
         }
     }
@@ -135,16 +135,16 @@ class Mangaeden : ParsedHttpSource() {
     private class NamedId(name: String, val id: Int) : Filter.CheckBox(name)
     private class Genre(name: String, val id: String) : Filter.TriState(name)
     private class TextField(name: String, val key: String) : Filter.Text(name)
-    private class OrderBy : Filter.Sort("Ordina per", arrayOf("Titolo manga", "Visite", "Capitoli", "Ultimo capitolo"),
+    private class OrderBy : Filter.Sort("Order by", arrayOf("Manga title", "Views", "Chapters", "Latest chapter"),
             Filter.Sort.Selection(1, false))
 
     private class StatusList(statuses: List<NamedId>) : Filter.Group<NamedId>("Stato", statuses)
     private class Types(types: List<NamedId>) : Filter.Group<NamedId>("Tipo", types)
-    private class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Generi", genres)
+    private class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Genres", genres)
 
     override fun getFilterList() = FilterList(
-            TextField("Autore", "author"),
-            TextField("Artista", "artist"),
+            TextField("Author", "author"),
+            TextField("Artist", "artist"),
             OrderBy(),
             Types(types()),
             StatusList(statuses()),
@@ -160,45 +160,44 @@ class Mangaeden : ParsedHttpSource() {
     )
 
     private fun statuses() = listOf(
-            NamedId("In corso", 1),
-            NamedId("Completato", 2),
-            NamedId("Sospeso", 0)
+            NamedId("Ongoing", 1),
+            NamedId("Completed", 2),
+            NamedId("Suspended", 0)
     )
 
     private fun genres() = listOf(
-            Genre("Avventura", "4e70ea8cc092255ef70073d3"),
-            Genre("Azione", "4e70ea8cc092255ef70073c3"),
-            Genre("Bara", "4e70ea90c092255ef70074b7"),
-            Genre("Commedia", "4e70ea8cc092255ef70073d0"),
-            Genre("Demenziale", "4e70ea8fc092255ef7007475"),
-            Genre("Dounshinji", "4e70ea93c092255ef70074e4"),
-            Genre("Drama", "4e70ea8cc092255ef70073f9"),
-            Genre("Ecchi", "4e70ea8cc092255ef70073cd"),
-            Genre("Fantasy", "4e70ea8cc092255ef70073c4"),
-            Genre("Harem", "4e70ea8cc092255ef70073d1"),
-            Genre("Hentai", "4e70ea90c092255ef700749a"),
-            Genre("Horror", "4e70ea8cc092255ef70073ce"),
-            Genre("Josei", "4e70ea90c092255ef70074bd"),
-            Genre("Magico", "4e70ea93c092255ef700751b"),
-            Genre("Mecha", "4e70ea8cc092255ef70073ef"),
-            Genre("Misteri", "4e70ea8dc092255ef700740a"),
-            Genre("Musica", "4e70ea8fc092255ef7007456"),
-            Genre("Psicologico", "4e70ea8ec092255ef7007439"),
-            Genre("Raccolta", "4e70ea90c092255ef70074ae"),
-            Genre("Romantico", "4e70ea8cc092255ef70073c5"),
-            Genre("Sci-Fi", "4e70ea8cc092255ef70073e4"),
-            Genre("Scolastico", "4e70ea8cc092255ef70073e5"),
-            Genre("Seinen", "4e70ea8cc092255ef70073ea"),
-            Genre("Sentimentale", "4e70ea8dc092255ef7007432"),
-            Genre("Shota", "4e70ea90c092255ef70074b8"),
-            Genre("Shoujo", "4e70ea8dc092255ef7007421"),
-            Genre("Shounen", "4e70ea8cc092255ef70073c6"),
-            Genre("Sovrannaturale", "4e70ea8cc092255ef70073c7"),
-            Genre("Splatter", "4e70ea99c092255ef70075a3"),
-            Genre("Sportivo", "4e70ea8dc092255ef7007426"),
-            Genre("Storico", "4e70ea8cc092255ef70073f4"),
-            Genre("Vita Quotidiana", "4e70ea8ec092255ef700743f"),
-            Genre("Yaoi", "4e70ea8cc092255ef70073de"),
-            Genre("Yuri", "4e70ea9ac092255ef70075d1")
+            Genre("Action", "4e70e91bc092255ef70016f8"),
+            Genre("Adult", "4e70e92fc092255ef7001b94"),
+            Genre("Adventure", "4e70e918c092255ef700168e"),
+            Genre("Comedy", "4e70e918c092255ef7001675"),
+            Genre("Doujinshi", "4e70e928c092255ef7001a0a"),
+            Genre("Drama", "4e70e918c092255ef7001693"),
+            Genre("Ecchi", "4e70e91ec092255ef700175e"),
+            Genre("Fantasy", "4e70e918c092255ef7001676"),
+            Genre("Gender Bender", "4e70e921c092255ef700184b"),
+            Genre("Harem", "4e70e91fc092255ef7001783"),
+            Genre("Historical", "4e70e91ac092255ef70016d8"),
+            Genre("Horror", "4e70e919c092255ef70016a8"),
+            Genre("Josei", "4e70e920c092255ef70017de"),
+            Genre("Martial Arts", "4e70e923c092255ef70018d0"),
+            Genre("Mature", "4e70e91bc092255ef7001705"),
+            Genre("Mecha", "4e70e922c092255ef7001877"),
+            Genre("Mystery", "4e70e918c092255ef7001681"),
+            Genre("One Shot", "4e70e91dc092255ef7001747"),
+            Genre("Psychological", "4e70e919c092255ef70016a9"),
+            Genre("Romance", "4e70e918c092255ef7001677"),
+            Genre("School Life", "4e70e918c092255ef7001688"),
+            Genre("Sci-fi", "4e70e91bc092255ef7001706"),
+            Genre("Seinen", "4e70e918c092255ef700168b"),
+            Genre("Shoujo", "4e70e918c092255ef7001667"),
+            Genre("Shounen", "4e70e918c092255ef700166f"),
+            Genre("Slice of Life", "4e70e918c092255ef700167e"),
+            Genre("Smut", "4e70e922c092255ef700185a"),
+            Genre("Sports", "4e70e91dc092255ef700172e"),
+            Genre("Supernatural", "4e70e918c092255ef700166a"),
+            Genre("Tragedy", "4e70e918c092255ef7001672"),
+            Genre("Webtoons", "4e70ea70c092255ef7006d9c"),
+            Genre("Yaoi", "4e70e91ac092255ef70016e5"),
+            Genre("Yuri", "4e70e92ac092255ef7001a57")
     )
 }
