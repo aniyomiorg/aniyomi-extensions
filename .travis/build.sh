9078f82963370339a517375882568b2b600d7388
len <len@kanade.eu>
2018-02-04 13:55:21 +0100

    Update dependencies and travis scripts

---
+++ .travis/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+./gradlew --no-daemon clean assembleRelease
+
+TOOLS="$(ls -d ${ANDROID_HOME}/build-tools/* | tail -1)"
+
+shopt -s globstar nullglob extglob
+APKS=( **/*"-unsigned.apk"* )
+
+DEST=$PWD/apk
+
+rm -rf $DEST && mkdir -p $DEST
+
+for APK in ${APKS[@]}; do
+    BASENAME=$(basename $APK)
+    APKNAME="${BASENAME%%+(-release*)}.apk"
+    APKDEST="$DEST/$APKNAME"
+
+    ${TOOLS}/zipalign -v -p 4 $APK $APKDEST
+    ${TOOLS}/apksigner sign --ks $STORE_PATH --ks-key-alias $STORE_ALIAS --ks-pass env:STORE_PASS --key-pass env:KEY_PASS $APKDEST
+done
---
+++ .travis/create-repo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+TOOLS="$(ls -d ${ANDROID_HOME}/build-tools/* | tail -1)"
+
+mkdir -p repo/apk
+mkdir -p repo/icon
+
+cp -f apk/* repo/apk || true
+
+cd repo
+
+APKS=( apk/*".apk"* )
+
+for APK in ${APKS[@]}; do
+    FILENAME=$(basename ${APK})
+    BADGING="$(${TOOLS}/aapt dump badging $APK)"
+
+    PACKAGE=$(echo "$BADGING" | grep package:)
+    PKGNAME=$(echo $PACKAGE | grep -Po "name='\K[^']+")
+    VCODE=$(echo $PACKAGE | grep -Po "versionCode='\K[^']+")
+    VNAME=$(echo $PACKAGE | grep -Po "versionName='\K[^']+")
+
+    APPLICATION=$(echo "$BADGING" | grep application:)
+    LABEL=$(echo $APPLICATION | grep -Po "label='\K[^']+")
+
+    LANG=$(echo $APK | grep -Po "tachiyomi-\K[^\.]+")
+
+    ICON=$(echo "$BADGING" | grep -Po "application-icon-320.*'\K[^']+")
+    unzip -p $APK $ICON > icon/${FILENAME%.*}.png
+
+    jq -n \
+        --arg name "$LABEL" \
+        --arg pkg "$PKGNAME" \
+        --arg apk "$FILENAME" \
+        --arg lang "$LANG" \
+        --argjson code $VCODE \
+        --arg version "$VNAME" \
+        '{name:$name, pkg:$pkg, apk:$apk, lang:$lang, code:$code, version:$version}'
+
+done | jq -sr '[.[]]' > index.json
