cd282554c3ca97c09520de12bd2f335f90b3123f
Pavka <pavel-mosein@yandex.ru>
2018-09-22 21:14:35 +0300

    Selfmanga fix packagename (#508)
    Selfmanga fix packagename

--- src/ru/selfmanga/src/eu/kanade/tachiyomi/extension/ru/selfmanga/Selfmanga.kt
+++ src/ru/selfmanga/src/eu/kanade/tachiyomi/extension/ru/selfmanga/Selfmanga.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.source.online.russian
+package eu.kanade.tachiyomi.extension.ru.selfmanga
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
