8364ac2d781db7dee255dbc880137dd9452920f8
dexmanga <48976859+dexmanga@users.noreply.github.com>
2019-04-13 02:22:10 -0700

    create mangadex.org intent filter to open mangadex urls with tachiyomi (#974)
    * create mangadex.org intent filter to open mangadex urls with tachiyomi
    
    * Match tachiyomi's handler

---
+++ src/all/mangadex/src/eu/kanade/tachiyomi/extension/all/mangadex/MangadexUrlActivity.kt
@@ -0,0 +1,42 @@
+package eu.kanade.tachiyomi.extension.all.mangadex
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+
+/**
+ * Springboard that accepts https://mangadex.com/title/xxx intents and redirects them to
+ * the main tachiyomi process. The idea is to not install the intent filter unless
+ * you have this extension installed, but still let the main tachiyomi app control
+ * things.
+ *
+ * Main goal was to make it easier to open manga in Tachiyomi in spite of the DDoS blocking
+ * the usual search screen from working.
+ */
+class MangadexUrlActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val pathSegments = intent?.data?.pathSegments
+        if (pathSegments != null && pathSegments.size > 1) {
+            val titleid = pathSegments[1]
+            val mainIntent = Intent().apply {
+                action = "eu.kanade.tachiyomi.SEARCH"
+                putExtra("query", "id:$titleid")
+                putExtra("filter", packageName)
+            }
+
+            try {
+                startActivity(mainIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e("MangadexUrlActivity", e.toString())
+            }
+        } else {
+            Log.e("MangadexUrlActivity", "could not parse uri from intent $intent")
+        }
+
+        finish()
+        System.exit(0)
+    }
+}
