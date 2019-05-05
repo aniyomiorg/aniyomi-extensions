8f598f16a14bff6e981735ce68b57fb5668f85a9
Edgar Mejía <33939239+edgarMejia@users.noreply.github.com>
2019-05-05 15:15:33 -0600

    Add genre filter. (#1081)
    Add genre filter.

--- src/all/ninemanga/src/eu/kanade/tachiyomi/extension/all/ninemanga/NineManga.kt
+++ src/all/ninemanga/src/eu/kanade/tachiyomi/extension/all/ninemanga/NineManga.kt
@@ -4,6 +4,7 @@ import eu.kanade.tachiyomi.source.model.*
 import okhttp3.Request
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.HttpUrl
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.ParseException
@@ -111,8 +112,27 @@ open class NineManga(override val name: String, override val baseUrl: String, ov
 
     override fun imageUrlParse(document: Document) = document.select("div.pic_box img.manga_pic").first().attr("src").orEmpty()
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+    /*override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         return GET("$baseUrl/search/?name_sel=&wd=$query&author_sel=&author=&artist_sel=&artist=&category_id=&out_category_id=&completed_series=&page=$page.html", headers)
+    }*/
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = HttpUrl.parse("$baseUrl/search/")!!.newBuilder()
+
+        url.addQueryParameter("wd", query)
+        url.addQueryParameter("page", page.toString())
+
+        filters.forEach { filter ->
+            when (filter) {
+                is GenreList -> {
+                    filter.state
+                            .filter { genre -> genre.state }
+                            .forEach { genre -> url.addQueryParameter("category_id", genre.id) }
+                }
+            }
+        }
+
+        return GET(url.build().toString(), headers)
     }
 
     override fun searchMangaSelector() = popularMangaSelector()
@@ -121,7 +141,80 @@ open class NineManga(override val name: String, override val baseUrl: String, ov
 
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
 
-    //TODO: Implement filters list.
-    // Array.from(document.querySelectorAll('.optionbox .typelist:nth-child(3) ul')).map(a => Array.from(a.querySelectorAll('li')).map(b => `Genre("${b.querySelector('label').innerText}", "${a.querySelector('li[cate_id]').getAttribute('cate_id')}")`)).join(',\n')
-    // http://es.ninemanga.com/search/?name_sel=contain&wd=&author_sel=contain&author=&artist_sel=contain&artist=&category_id=&out_category_id=&completed_series=either&type=high
+    open class Genre(name: String, val id: String) : Filter.CheckBox(name)
+    open class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Genre", genres)
+
+    override fun getFilterList() = FilterList(
+            GenreList(getGenreList())
+    )
+
+    // $(document.querySelectorAll('.optionbox .typelist:nth-child(3) ul li.cate_list')).map((i, el)=>`Genre("${$(el).first().text().trim()}","${$(el).attr("cate_id")}")`).get().sort().join(",\n")
+    // http://en.ninemanga.com/search/?type=high
+    open fun getGenreList() = listOf(
+        Genre("4-Koma","56"),
+        Genre("Action","1"),
+        Genre("Adventure","2"),
+        Genre("Anime","3"),
+        Genre("Award Winning","59"),
+        Genre("Bara","84"),
+        Genre("Comedy","4"),
+        Genre("Cooking","5"),
+        Genre("Crime","132"),
+        Genre("Demons","49"),
+        Genre("Doujinshi","45"),
+        Genre("Drama","6"),
+        Genre("Fantasy","8"),
+        Genre("Game","126"),
+        Genre("Gender Bender","9"),
+        Genre("Historical","11"),
+        Genre("Horror","12"),
+        Genre("Isekai","127"),
+        Genre("Josei","13"),
+        Genre("Live Action","14"),
+        Genre("Magic","47"),
+        Genre("Magical Girls","130"),
+        Genre("Manhua","15"),
+        Genre("Manhwa","16"),
+        Genre("Martial Arts","17"),
+        Genre("Matsumoto Tomokicomedy","37"),
+        Genre("Mecha","18"),
+        Genre("Medical","19"),
+        Genre("Military","51"),
+        Genre("Music","20"),
+        Genre("Mystery","21"),
+        Genre("N/A","54"),
+        Genre("None","64"),
+        Genre("One Shot","22"),
+        Genre("Oneshot","57"),
+        Genre("Philosophical","133"),
+        Genre("Psychological","23"),
+        Genre("Reverse Harem","55"),
+        Genre("Romance Shoujo","38"),
+        Genre("Romance","24"),
+        Genre("School Life","25"),
+        Genre("Sci-Fi","26"),
+        Genre("Seinen","27"),
+        Genre("Shoujo Ai","44"),
+        Genre("Shoujo","28"),
+        Genre("Shoujo-Ai","29"),
+        Genre("Shoujoai","48"),
+        Genre("Shounen Ai","42"),
+        Genre("Shounen","30"),
+        Genre("Shounen-Ai","31"),
+        Genre("Shounenai","46"),
+        Genre("Slice Of Life","32"),
+        Genre("Sports","33"),
+        Genre("Staff Pick","60"),
+        Genre("Super Power","62"),
+        Genre("Superhero","131"),
+        Genre("Supernatural","34"),
+        Genre("Suspense","53"),
+        Genre("Thriller","129"),
+        Genre("Tragedy","35"),
+        Genre("Vampire","52"),
+        Genre("Webtoon","58"),
+        Genre("Webtoons","50"),
+        Genre("Wuxia","128"),
+        Genre("[No Chapters]","61")
+    )
 }
