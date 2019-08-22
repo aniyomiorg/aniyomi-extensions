67d4bc05727d7b99fcb1b8918dc17ff4b5e19c20
Stephane Mensah <waptik@users.noreply.github.com>
2019-08-22 11:19:53 +0000

    ComiCake extension: Added LetItGo Scans (#1411)
    ComiCake extension: Added LetItGo Scans

--- src/all/comicake/src/eu/kanade/tachiyomi/extension/all/comicake/ComiCakeFactory.kt
+++ src/all/comicake/src/eu/kanade/tachiyomi/extension/all/comicake/ComiCakeFactory.kt
@@ -9,11 +9,14 @@ class ComiCakeFactory : SourceFactory {
 
 fun getAllComiCake(): List<Source> {
     return listOf(
-            WhimSubs(),
-            PTScans()
+        WhimSubs(),
+        PTScans(),
+        LetItGoScans()
     )
 }
 
 class WhimSubs : ComiCake("WhimSubs", "https://whimsubs.xyz", "en")
 
 class PTScans : ComiCake("ProjectTime Scans", "https://read.ptscans.com", "en", "/")
+
+class LetItGoScans : ComiCake("LetItGo Scans", "https://reader.letitgo.scans.today", "en", "/")
