1fea350f6bf4d84099b68b6bd5f6c0cb14d64370
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2019-09-01 10:20:34 -0400

    MangaCards (add source Inept Bastards) (#1450)
    MangaCards (add source Inept Bastards)

--- src/all/mangacards/src/eu/kanade/tachiyomi/extension/all/mangacards/MangaCardsFactory.kt
+++ src/all/mangacards/src/eu/kanade/tachiyomi/extension/all/mangacards/MangaCardsFactory.kt
@@ -6,10 +6,12 @@ import eu.kanade.tachiyomi.source.SourceFactory
 class MangaCardsFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
         ValhallaScans(),
-        NaniScans()
+        NaniScans(),
+        IneptBastards()
     )
 }
 
 class ValhallaScans : MangaCards("Valhalla Scans", "https://valhallascans.com", "en")
 class NaniScans : MangaCards("NANI? Scans", "https://naniscans.xyz", "en")
+class IneptBastards : MangaCards("Inept Bastards", "https://ineptbastards.xyz", "en")
 
