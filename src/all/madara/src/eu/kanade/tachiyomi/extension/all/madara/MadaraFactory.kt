8723dcab07c1d6e7576ac76349114183c68d8f27
TechnoJo4 <technojo4@gmail.com>
2019-11-10 19:10:13 -0500

    add Hiperdex & bump version (#1764)
    [Add] Hiperdex

--- src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
+++ src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
@@ -52,7 +52,8 @@ class MadaraFactory : SourceFactory {
         AdonisFansub(),
         GetManhwa(),
         AllPornComic(),
-        Milftoon()
+        Milftoon(),
+        Hiperdex()
     )
 }
 
@@ -233,4 +234,41 @@ class AllPornComic : Madara("AllPornComic", "https://allporncomic.com", "en") {
 class Milftoon : Madara("Milftoon", "https://milftoon.xxx", "en") {
     override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/page/$page/?m_orderby=views", headers)
     override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/page/$page/?m_orderby=latest", headers)
+}
+
+class Hiperdex : Madara("Hiperdex", "https://hiperdex.com", "en") {
+    override fun getGenreList() = listOf(
+        Genre( "Adult",  "adult"),
+        Genre( "Action",  "action"),
+        Genre( "Adventure",  "adventure"),
+        Genre( "Bully",  "bully"),
+        Genre( "Comedy",  "comedy"),
+        Genre( "Drama",  "drama"),
+        Genre( "Ecchi",  "ecchi"),
+        Genre( "Fantasy",  "fantasy"),
+        Genre( "Gender Bender",  "gender-bender"),
+        Genre( "Harem",  "harem"),
+        Genre( "Historical",  "historical"),
+        Genre( "Horror",  "horror"),
+        Genre( "Isekai",  "isekai"),
+        Genre( "Josei",  "josei"),
+        Genre( "Martial Arts",  "martial-arts"),
+        Genre( "Mature",  "mature"),
+        Genre( "Mystery",  "mystery"),
+        Genre( "Psychological",  "psychological"),
+        Genre( "Romance",  "romance"),
+        Genre( "School Life",  "school-life"),
+        Genre( "Sci-Fi",  "sci-fi"),
+        Genre( "Seinen",  "seinen"),
+        Genre( "Shoujo",  "shoujo"),
+        Genre( "Shounen",  "shounen"),
+        Genre( "Slice of Life",  "slice-of-life"),
+        Genre( "Smut",  "smut"),
+        Genre( "Sports",  "sports"),
+        Genre( "Supernatural",  "supernatural"),
+        Genre( "Thriller",  "thriller"),
+        Genre( "Tragedy",  "tragedy"),
+        Genre( "Yaoi",  "yaoi"),
+        Genre( "Yuri",  "yuri")
+    )
 }
