d100bdca51b972e706f67c00f9dcdb1f6c21907a
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2020-01-15 07:33:08 -0500

    Fix Rawkuma date parsing (#2064)
    Fix Rawkuma date parsing

--- src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
+++ src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
@@ -10,7 +10,9 @@ import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.util.Locale
 
 class Rawkuma: ParsedHttpSource() {
 
@@ -160,7 +162,15 @@ class Rawkuma: ParsedHttpSource() {
     }
 
     private fun parseDate(date: String): Long {
-        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse(date).time
+        return try {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US).parse(date).time
+        } catch (e: ParseException) {
+            try {
+                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US).parse(date.substringBefore("+")).time
+            } catch (e: ParseException) {
+                0L
+            }
+        }
     }
 
     override fun chapterListSelector() = ".lchx"
