955ef683ac96c8eae2311aa2dc72ca8a4eff7511
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2020-02-21 10:55:16 -0500

    Update baseUrl (#2274)
    Holy/Heaven Manga - Update baseUrl

--- src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HMangaFactory.kt
+++ src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HMangaFactory.kt
@@ -10,6 +10,6 @@ class HMangaFactory : SourceFactory {
     )
 }
 
-class HolyManga : HManga("HolyManga", "https://holymanga.net")
-class HeavenManga : HManga("HeavenManga", "https://heavenmanga.org")
+class HolyManga : HManga("HolyManga", "http://w12.holymanga.net")
+class HeavenManga : HManga("HeavenManga", "http://ww8.heavenmanga.org")
 
