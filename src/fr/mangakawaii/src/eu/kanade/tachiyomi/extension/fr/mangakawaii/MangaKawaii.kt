fc249567fcf0ac73df000e440e61e80054723175
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2020-05-05 06:42:02 -0400

    Mangakawaii - fix search (#3011)

--- src/fr/mangakawaii/src/eu/kanade/tachiyomi/extension/fr/mangakawaii/MangaKawaii.kt
+++ src/fr/mangakawaii/src/eu/kanade/tachiyomi/extension/fr/mangakawaii/MangaKawaii.kt
@@ -41,7 +41,7 @@ class MangaKawaii : ParsedHttpSource() {
         GET("$baseUrl/filterLists?page=$page&sortBy=views&asc=false", headersBuilder().add("X-Requested-With", "XMLHttpRequest").build())
     override fun latestUpdatesRequest(page: Int) = GET(baseUrl, headers)
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val uri = Uri.parse("$baseUrl/search2").buildUpon()
+        val uri = Uri.parse("$baseUrl/search").buildUpon()
             .appendQueryParameter("query", query)
         return GET(uri.toString(), headers)
     }
