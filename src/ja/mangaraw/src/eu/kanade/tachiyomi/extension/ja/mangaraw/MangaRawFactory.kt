028ba2e0842c74ad7e8de38dfb882a4d134d68d2
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-05-08 23:10:27 +0100

    Add MangaRaw (#3071)

---
+++ src/ja/mangaraw/src/eu/kanade/tachiyomi/extension/ja/mangaraw/MangaRawFactory.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.extension.ja.mangaraw
+
+import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceFactory
+
+class MangaRawFactory: SourceFactory {
+    override fun createSources(): List<Source> = listOf(
+        Manga1000(),
+        Manga1001()
+    )
+}
+
+class Manga1000: MangaRaw("Manga1000", "http://manga1000.com")
+class Manga1001: MangaRaw("Manga1001", "http://manga1001.com")
