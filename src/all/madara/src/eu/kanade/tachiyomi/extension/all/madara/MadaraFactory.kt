fc377d56ade5e4cdd14ad5ea17ca9c49a931d096
Agayev033 <aqayev10.qa@gmail.com>
2020-05-19 14:36:10 +0300

    Madara ArazNovel - Add Custom GenreList (#3238)
    * Madara ArazNovel - Add Custom GenreList
    
    * Update build.gradle

--- src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
+++ src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
@@ -660,6 +660,51 @@ class ManhuasWorld : Madara("Manhuas World", "https://manhuasworld.com", "en")
 class ArazNovel : Madara("ArazNovel", "https://www.araznovel.com", "tr", SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())) {
     override fun formBuilder(page: Int, popular: Boolean): FormBody.Builder = super.formBuilder(page, popular)
         .add("vars[meta_query][0][0][value]", "manga")
+    override fun getGenreList() = listOf(
+        Genre("Aksiyon", "action"),
+        Genre("Macera", "adventure"),
+        Genre("Cartoon", "cartoon"),
+        Genre("Comic", "comic"),
+        Genre("Komedi", "comedy"),
+        Genre("Yemek", "cooking"),
+        Genre("Doujinshi", "doujinshi"),
+        Genre("Dram", "drama"),
+        Genre("Ecchi", "ecchi"),
+        Genre("Fantastik", "fantasy"),
+        Genre("Harem", "harem"),
+        Genre("Tarihi", "historical"),
+        Genre("Korku", "horror"),
+        Genre("Manga", "manga"),
+        Genre("Manhua", "manhua"),
+        Genre("Manhwa", "manhwa"),
+        Genre("Olgun", "mature"),
+        Genre("Mecha", "mecha"),
+        Genre("Yetişkin", "adult"),
+        Genre("Gizem", "mystery"),
+        Genre("One Shot", "one-shot"),
+        Genre("Isekai", "isekai"),
+        Genre("Josei", "josei"),
+        Genre("Dedektif", "detective"),
+        Genre("Karanlık", "smut"),
+        Genre("Romantizm", "romance"),
+        Genre("Okul Yaşamı", "school-life"),
+        Genre("Yaşamdan Kesit", "slice-of-life"),
+        Genre("Spor", "sports"),
+        Genre("Doğa Üstü", "supernatural"),
+        Genre("Trajedi", "tragedy"),
+        Genre("Webtoon ", "webtoon"),
+        Genre("Dövüş Sanatları ", "martial-arts"),
+        Genre("Bilim Kurgu", "sci-fi"),
+        Genre("Seinen", "seinen"),
+        Genre("Shoujo", "shoujo"),
+        Genre("Shoujo Ai", "shoujo-ai"),
+        Genre("Shounen", "shounen"),
+        Genre("Shounen Ai", "shounen-ai"),
+        Genre("Soft Yaoi", "soft-yaoi"),
+        Genre("Soft Yuri", "soft-yuri"),
+        Genre("Yaoi", "yaoi"),
+        Genre("Yuri", "yuri")
+		    )
     override fun chapterListParse(response: Response): List<SChapter> {
         return getXhrChapters(response.asJsoup().select("div#manga-chapters-holder").attr("data-id")).let { document ->
             document.select("li.parent").let { elements ->
