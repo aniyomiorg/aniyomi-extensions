e9067498376d54d776d1a268bf638b0f9499d495
Riztard Lanthorn <riyanluqman@gmail.com>
2021-01-15 00:16:33 +0700

    fix bulu manga - chapter image issue (#5419)

--- src/all/zbulu/src/eu/kanade/tachiyomi/extension/all/zbulu/ZbuluFactory.kt
+++ src/all/zbulu/src/eu/kanade/tachiyomi/extension/all/zbulu/ZbuluFactory.kt
@@ -2,6 +2,8 @@ package eu.kanade.tachiyomi.extension.all.zbulu
 
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 
 class ZbuluFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
@@ -15,4 +17,10 @@ class ZbuluFactory : SourceFactory {
 class HolyManga : Zbulu("HolyManga", "https://w15.holymanga.net")
 class HeavenManga : Zbulu("HeavenManga", "http://heaventoon.com")
 class KooManga : Zbulu("Koo Manga", "http://ww1.koomanga.com")
-class BuluManga : Zbulu("Bulu Manga", "https://ww5.bulumanga.net")
+class BuluManga : Zbulu("Bulu Manga", "https://ww5.bulumanga.net") {
+    override fun pageListParse(document: Document): List<Page> {
+        return document.select("div.chapter-content img").mapIndexed { i, img ->
+            Page(i, "", img.attr("abs:src").substringAfter("url="))
+        }
+    }
+}
