49fb99b4f1bc5327b331763366bf175769f0612d
Riztard Lanthorn <riyanluqman@gmail.com>
2021-01-26 19:44:50 +0700

    Scantrad user agent (#5563)
    try triple randomizer

--- src/fr/scantrad/src/eu/kanade/tachiyomi/extension/fr/scantrad/Scantrad.kt
+++ src/fr/scantrad/src/eu/kanade/tachiyomi/extension/fr/scantrad/Scantrad.kt
@@ -37,7 +37,7 @@ class Scantrad : ParsedHttpSource() {
     protected open val userAgentRandomizer = " ${Random.nextInt().absoluteValue}"
 
     override fun headersBuilder() = Headers.Builder().apply {
-        add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36$userAgentRandomizer")
+        add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36$userAgentRandomizer (KHTML, like Gecko) Chrome/81.0.4044.122$userAgentRandomizer Safari/537.36$userAgentRandomizer")
     }
 
     // Popular
