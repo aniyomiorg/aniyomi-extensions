bba235621c8e5aed8a6ba3cb803671e35775e1da
Riztard Lanthorn <riyanluqman@gmail.com>
2021-02-03 00:56:29 +0700

    fix mangakawaii no pages (#5626)

--- src/fr/mangakawaii/src/eu/kanade/tachiyomi/extension/fr/mangakawaii/MangaKawaii.kt
+++ src/fr/mangakawaii/src/eu/kanade/tachiyomi/extension/fr/mangakawaii/MangaKawaii.kt
@@ -100,7 +100,7 @@ class MangaKawaii : ParsedHttpSource() {
 
     override fun pageListParse(response: Response): List<Page> {
         val body = response.asJsoup()
-        var div = body.select("div.text-center.d-none")
+        var div = body.select("div.text-center .d-none + div:not(#ppp)")
         var elements = div.select("img")
 
         val pages = mutableListOf<Page>()
