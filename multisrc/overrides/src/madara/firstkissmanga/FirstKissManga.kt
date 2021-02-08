25966d91deb831fc5385257dd25301a51091f151
Riztard Lanthorn <riyanluqman@gmail.com>
2021-02-08 19:32:20 +0700

    1st Kiss fix dateParse (#5689)

--- multisrc/overrides/src/madara/firstkissmanga/FirstKissManga.kt
+++ multisrc/overrides/src/madara/firstkissmanga/FirstKissManga.kt
@@ -5,6 +5,6 @@ import okhttp3.Headers
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class FirstKissManga : Madara("1st Kiss", "https://1stkissmanga.com", "en", dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.US)) {
+class FirstKissManga : Madara("1st Kiss", "https://1stkissmanga.com", "en") {
     override fun headersBuilder(): Headers.Builder = super.headersBuilder().add("Referer", baseUrl)
 }
