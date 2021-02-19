0abfb4f7010be41637811bcac91a9f831ca4a49f
Riztard Lanthorn <riyanluqman@gmail.com>
2021-02-20 04:41:01 +0700

    add rate limiter to Shield Manga (#5922)

---
+++ multisrc/overrides/madara/shieldmanga/src/ShieldManga.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.en.shieldmanga
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+class ShieldManga : Madara("Shield Manga", "https://shieldmanga.club", "en") {
+    private val rateLimitInterceptor = RateLimitInterceptor(1)
+
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addNetworkInterceptor(rateLimitInterceptor)
+        .build()
+}
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -188,7 +188,7 @@ class MadaraGenerator : ThemeSourceGenerator {
             SingleLang("Rüya Manga", "https://www.ruyamanga.com", "tr", className = "RuyaManga"),
             SingleLang("S2Manga", "https://s2manga.com", "en"),
             SingleLang("Sekte Doujin", "https://sektedoujin.xyz", "id", isNsfw = true),
-            SingleLang("Shield Manga", "https://shieldmanga.club", "en"),
+            SingleLang("Shield Manga", "https://shieldmanga.club", "en", overrideVersionCode = 1),
             SingleLang("ShoujoHearts", "http://shoujohearts.com", "en"),
             SingleLang("SiXiang Scans", "http://www.sixiangscans.com", "en"),
             SingleLang("Siyahmelek", "https://siyahmelek.com", "tr", isNsfw = true, overrideVersionCode = 1),
