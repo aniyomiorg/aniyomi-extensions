08b6f0f8ce5839d5fa880168a73270695e0f2975
Riztard Lanthorn <riyanluqman@gmail.com>
2021-02-21 22:18:06 +0700

    fix mangajar pages issue (#5945)

--- src/en/mangajar/src/eu/kanade/tachiyomi/extension/en/mangajar/MangaJar.kt
+++ src/en/mangajar/src/eu/kanade/tachiyomi/extension/en/mangajar/MangaJar.kt
@@ -143,7 +143,7 @@ class MangaJar : ParsedHttpSource() {
 
     override fun pageListParse(document: Document): List<Page> {
         return document.select("img[data-page]").mapIndexed { i, element ->
-            Page(i, "", element.attr("src"))
+            Page(i, "", element.attr("data-alternative"))
         }
     }
 
