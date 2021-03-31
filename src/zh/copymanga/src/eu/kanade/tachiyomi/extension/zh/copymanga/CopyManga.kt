e870afd06f7aa3c0b31d772d0728bad3dcbb8b52
sunbeams001 <sunbeams001@gmail.com>
2021-03-31 21:51:15 +0800

    coymanga: fix site blacklist by request's User-Agent header (#6367)

--- src/zh/copymanga/src/eu/kanade/tachiyomi/extension/zh/copymanga/CopyManga.kt
+++ src/zh/copymanga/src/eu/kanade/tachiyomi/extension/zh/copymanga/CopyManga.kt
@@ -175,7 +175,7 @@ class CopyManga : ConfigurableSource, HttpSource() {
 
     override fun headersBuilder() = super.headersBuilder()
         .add("Referer", baseUrl)
-        .add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 Tachiyomi/1.0")
+        .add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
 
     // Unused, we can get image urls directly from the chapter page
     override fun imageUrlParse(response: Response) =
