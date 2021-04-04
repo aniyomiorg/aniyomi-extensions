f98d35bea50b54121fe3b13c3caa6bc23dc0a098
Pavka <p.mosein@edadeal.ru>
2021-04-04 15:49:02 +0300

    [Ru] ComX. Fix images (#6415)

--- src/ru/comx/src/eu/kanade/tachiyomi/extension/ru/comx/ComX.kt
+++ src/ru/comx/src/eu/kanade/tachiyomi/extension/ru/comx/ComX.kt
@@ -191,20 +191,23 @@ class ComX : ParsedHttpSource() {
 
     override fun pageListParse(response: Response): List<Page> {
         val html = response.body()!!.string()
-        val beginTag = "comix_images=["
-        val beginIndex = html.indexOf(beginTag)
-        val endTag = "], comix_link='"
-        val endIndex = html.indexOf(endTag, beginIndex)
-        val comixIndex = html.indexOf("', page=", endIndex)
+        val baseImgUrl = "https://img.com-x.life/comix/"
 
-        val link = html.substring(endIndex + endTag.length, comixIndex)
+        val beginTag = "\"images\":["
+        val beginIndex = html.indexOf(beginTag)
+        val endIndex = html.indexOf("]", beginIndex)
+
         val urls: List<String> = html.substring(beginIndex + beginTag.length, endIndex)
-            .split(',')
+            .split(',').map {
+                val img = it.replace("\\", "").replace("\"", "")
+                baseImgUrl + img
+            }
 
         val pages = mutableListOf<Page>()
         for (i in urls.indices) {
-            pages.add(Page(i, "", link + (urls[i].removeSurrounding("'"))))
+            pages.add(Page(i, "", urls[i]))
         }
+
         return pages
     }
 
