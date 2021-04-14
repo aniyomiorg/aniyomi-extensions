a62484dbb33a87b1ff24f2571b6907132eb0de07
Riztard Lanthorn <riyanluqman@gmail.com>
2021-04-14 18:58:54 +0700

    ManhuaPlus: fix page issue (#6539)
    fix page issue for some chapter

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -147,7 +147,7 @@ class MadaraGenerator : ThemeSourceGenerator {
             SingleLang("Manhua ES", "https://manhuaes.com", "en", overrideVersionCode = 4),
             SingleLang("ManhuaFast", "https://manhuafast.com", "en"),
             SingleLang("Manhuaga", "https://manhuaga.com", "en", overrideVersionCode = 1),
-            SingleLang("Manhua Plus", "https://manhuaplus.com", "en", overrideVersionCode = 2),
+            SingleLang("Manhua Plus", "https://manhuaplus.com", "en", overrideVersionCode = 3),
             SingleLang("Manhuas.net", "https://manhuas.net", "en", className = "Manhuasnet"),
             SingleLang("Manhuas World", "https://manhuasworld.com", "en"),
             SingleLang("Manhua SY", "https://www.manhuasy.com", "en"),
