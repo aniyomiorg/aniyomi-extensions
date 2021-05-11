af58998ed1bed906989e32f5b89e13bd9c6d53b6
h-hyuuga <83582211+h-hyuuga@users.noreply.github.com>
2021-05-11 18:28:17 -0400

    Add Luminous Scans, Davey Scans, and Skull Scans (#6985)

---
+++ multisrc/overrides/wpmangareader/luminousscans/src/LuminousScans.kt
@@ -0,0 +1,10 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.luminousscans
+
+import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
+
+
+
+
+class LuminousScans : WPMangaReader("Luminous Scans", "https://www.luminousscans.com", "en", mangaUrlDirectory="/series")
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
@@ -17,11 +17,14 @@ class WPMangaReaderGenerator : ThemeSourceGenerator {
             SingleLang("MangaKita", "https://mangakita.net", "id"),
             SingleLang("Ngomik", "https://ngomik.net", "id"),
             SingleLang("Sekaikomik", "https://www.sekaikomik.club", "id", isNsfw = true, overrideVersionCode = 3),
+            SingleLang("Davey Scans", "https://daveyscans.com/", "id"),
             SingleLang("TurkToon", "https://turktoon.com", "tr"),
             SingleLang("Gecenin Lordu", "https://geceninlordu.com/", "tr", overrideVersionCode = 1),
             SingleLang("Flame Scans", "http://flamescans.org", "en", overrideVersionCode = 1),
             SingleLang("A Pair of 2+", "https://pairof2.com", "en", className = "APairOf2"),
             SingleLang("PMScans", "https://reader.pmscans.com", "en"),
+            SingleLang("Skull Scans", "https://www.skullscans.com", "en"),
+            SingleLang("Luminous Scans", "https://www.luminousscans.com", "en"),
             SingleLang("GS Nation", "https://gs-nation.fr", "fr", overrideVersionCode = 1)
         )
 
