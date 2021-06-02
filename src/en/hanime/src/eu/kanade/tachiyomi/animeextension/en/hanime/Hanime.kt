71c35170697556d76646c0c2b31dad2d2a2432ad
jmir1 <jhmiramon@gmail.com>
2021-06-03 00:56:46 +0200

    add hanime

---
+++ src/en/hanime/src/eu/kanade/tachiyomi/animeextension/en/hanime/Hanime.kt
@@ -0,0 +1,144 @@
+package eu.kanade.tachiyomi.animeextension.en.hanime
+
+import android.util.Log
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
+import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+
+class Hanime : AnimeHttpSource() {
+
+    override val name = "hanime.tv"
+
+    override val baseUrl = ""
+
+    override val lang = "en"
+
+    override val supportsLatest = false
+
+    private fun searchRequestBody(query: String, page: Int, filters: AnimeFilterList): RequestBody {
+        var filterString = ""
+        for (filter in filters.list) {
+            filterString += if (filters.lastIndexOf(filter) == filters.lastIndex) {
+                "\"${filter.name}\""
+            } else {
+                "\"${filter.name}\","
+            }
+        }
+        return """
+            {"search_text": "$query",
+            "tags": [$filterString],
+            "tags_mode":"AND",
+            "brands": [],
+            "blacklist": [],
+            "order_by": "likes",
+            "ordering": "desc",
+            "page": ${page - 1}}
+        """.trimIndent().toRequestBody("application/json".toMediaType())
+    }
+    private val popularRequestHeaders = Headers.headersOf("authority", "search.htv-services.com", "accept", "application/json, text/plain, */*", "content-type", "application/json;charset=UTF-8")
+
+    override fun popularAnimeRequest(page: Int): Request = POST("https://search.htv-services.com/", popularRequestHeaders, searchRequestBody("", page, AnimeFilterList()))
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        val responseString = response.body!!.string()
+        return parseSearchJson(responseString)
+    }
+    fun parseSearchJson(jsonLine: String?): AnimesPage {
+        val jElement: JsonElement = JsonParser.parseString(jsonLine)
+        val jObject: JsonObject = jElement.asJsonObject
+        val nbPages = jObject.get("nbPages").asInt
+        val page = jObject.get("page").asInt
+        val hasNextPage = page < nbPages - 1
+        val arrayString = jObject.get("hits").asString
+        val array = JsonParser.parseString(arrayString)
+        val jObjectb: JsonArray = array.asJsonArray
+        val animeList = mutableListOf<SAnime>()
+        for (item in jObjectb) {
+            val anime = SAnime.create()
+            anime.title = item.asJsonObject.get("name").asString
+            anime.thumbnail_url = item.asJsonObject.get("cover_url").asString
+            anime.setUrlWithoutDomain("https://hanime.tv/videos/hentai/" + item.asJsonObject.get("slug").asString)
+            anime.author = item.asJsonObject.get("brand").asString
+            anime.description = item.asJsonObject.get("description").asString
+            anime.status = SAnime.COMPLETED
+            anime.initialized = true
+            animeList.add(anime)
+        }
+        return AnimesPage(animeList, hasNextPage)
+    }
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request = POST("https://search.htv-services.com/", popularRequestHeaders, searchRequestBody(query, page, filters))
+
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        val responseString = response.body!!.string()
+        return parseSearchJson(responseString)
+    }
+
+    override fun animeDetailsRequest(anime: SAnime): Request {
+        return POST("https://search.htv-services.com/", popularRequestHeaders, searchRequestBody(anime.title, 1, AnimeFilterList()))
+    }
+
+    override fun animeDetailsParse(response: Response): SAnime {
+        val responseString = response.body!!.string()
+        val jElement: JsonElement = JsonParser.parseString(responseString)
+        val jObject: JsonObject = jElement.asJsonObject
+        val arrayString = jObject.get("hits").asString
+        val array = JsonParser.parseString(arrayString)
+        val jObjectb: JsonArray = array.asJsonArray
+        val item = jObjectb[0]
+        val anime = SAnime.create()
+        anime.title = item.asJsonObject.get("name").asString
+        anime.thumbnail_url = item.asJsonObject.get("cover_url").asString
+        anime.setUrlWithoutDomain("https://hanime.tv/videos/hentai/" + item.asJsonObject.get("slug").asString)
+        anime.author = item.asJsonObject.get("brand").asString
+        anime.description = item.asJsonObject.get("description").asString.replace("<p>", "").replace("</p>", "")
+        anime.status = SAnime.COMPLETED
+        anime.initialized = true
+        return anime
+    }
+
+    override fun episodeLinkParse(response: Response): String {
+        val responseString = response.body!!.string()
+        val jElement: JsonElement = JsonParser.parseString(responseString)
+        val jObject: JsonObject = jElement.asJsonObject
+        val server = jObject.get("videos_manifest").asJsonObject.get("servers").asJsonArray[0].asJsonObject
+        val stream = server.get("streams").asJsonArray[1].asJsonObject
+        Log.w("link", stream.get("url").asString)
+        return stream.get("url").asString
+    }
+
+    override fun episodeListRequest(anime: SAnime): Request {
+        val slug = anime.url.substringAfterLast("/")
+        return GET("https://hw.hanime.tv/api/v8/video?id=$slug")
+    }
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val responseString = response.body!!.string()
+        val jElement: JsonElement = JsonParser.parseString(responseString)
+        val jObject: JsonObject = jElement.asJsonObject
+        val episode = SEpisode.create()
+        episode.date_upload = jObject.asJsonObject.get("hentai_video").asJsonObject.get("released_at_unix").asLong * 1000
+        episode.name = jObject.asJsonObject.get("hentai_video").asJsonObject.get("name").asString
+        episode.url = response.request.url.toString()
+        episode.episode_number = 1F
+        return listOf(episode)
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request = throw Exception("no")
+
+    override fun latestUpdatesParse(response: Response): AnimesPage = throw Exception("no")
+}
