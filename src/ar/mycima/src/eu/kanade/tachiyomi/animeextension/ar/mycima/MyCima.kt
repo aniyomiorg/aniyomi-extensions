94ad21d05788af99ce453f55b0c9ed242f6e9377
Ahmed gamal <12355.ahmedgamal.com@gmail.com>
2021-09-02 23:27:20 +0200

    a lot of fixes but still no quality

--- src/ar/mycima/src/eu/kanade/tachiyomi/animeextension/ar/mycima/MyCima.kt
+++ src/ar/mycima/src/eu/kanade/tachiyomi/animeextension/ar/mycima/MyCima.kt
@@ -30,16 +30,16 @@ class MyCima : ParsedAnimeHttpSource() {
 
     override val client: OkHttpClient = network.cloudflareClient
 
-    override fun popularAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem a" // "ul.anime-loop.loop li a"
+    override fun popularAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
 
-    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/anime/page/$page")
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/seriestv/top/?page_number=$page")
 
     override fun popularAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.setUrlWithoutDomain(element.attr("href"))
-        anime.title = element.attr("title")
-        anime.thumbnail_url = element.select("span").first().attr("style").substringAfter("background-image:url(").substringBefore(");")
-        // element.select("div.itemtype_anime_poster img").first().attr("abs:src")
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.select("a").attr("title")
+        anime.thumbnail_url = element.select("a > span.BG--GridItem").attr("data-lazy-style")
+            .substringAfter("background-image:url(").substringBefore(");")
         return anime
     }
 
@@ -67,74 +67,49 @@ class MyCima : ParsedAnimeHttpSource() {
         return iframeResponse.select(videoListSelector()).map { videoFromElement(it) }
     }
 
-    /*override fun videoListParse(response: Response): List<Video> {
-        val responseString = response.body!!.string()
-        val jElement: JsonElement = JsonParser.parseString(responseString)
-        val jObject: JsonObject = jElement.asJsonObject
-        val server = jObject.get("videos_manifest").asJsonObject.get("servers").asJsonArray[0].asJsonObject
-        val streams = server.get("streams").asJsonArray
-        val linkList = mutableListOf<Video>()
-        for (stream in streams) {
-            if (stream.asJsonObject.get("kind").asString != "premium_alert") {
-                linkList.add(Video(stream.asJsonObject.get("url").asString, stream.asJsonObject.get("height").asString + "p", stream.asJsonObject.get("url").asString, null))
-            }
-        }
-        return linkList
-    }*/
-
-    override fun videoListSelector() = "div.vjs-quality-dropdown > ul > li > a, source" // "source"
+    override fun videoListSelector() = "div.Download--Mycima--Single > ul:nth-child(2) > li > a, source"
 
     override fun videoFromElement(element: Element): Video {
-        Log.i("lol", element.attr("src, href"))
-        // element.attr("abs:href")
-        return Video(element.attr("href"), element.select("a").text(), element.attr("src"), null)
+        Log.i("lol", element.attr("href, src"))
+        return Video(element.attr("abs:href"), element.select("resolution").text(), element.attr("abs:src"), null)
     }
 
     override fun videoUrlParse(document: Document) = throw Exception("not used")
 
     override fun searchAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.setUrlWithoutDomain(element.attr("href"))
-        anime.title = element.attr("title")
-        anime.thumbnail_url = element.select("span").attr("style").substringAfter("background-image:url(").substringBefore(")")
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.select("a > strong.hasyear").text()
+        anime.thumbnail_url = element.select("a > span.BG--GridItem").attr("data-lazy-style").substringAfter("background-image:url(").substringBefore(");")
         return anime
     }
 
     override fun searchAnimeNextPageSelector(): String = "ul.page-numbers li a.next"
 
-    override fun searchAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem a"
+    override fun searchAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
 
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request = GET("$baseUrl/search/$query/page/$page/")
 
     override fun animeDetailsParse(document: Document): SAnime {
         val anime = SAnime.create()
-        anime.thumbnail_url = document.select("div.Poster--Single-begin a").first().attr("src")
-        anime.title = document.select("span[itemprop=name]").text()
+        anime.title = document.select("div.Title--Content--Single-begin > h1").text()
         anime.genre = document.select("li:contains(التصنيف) > p > a, li:contains(النوع) > p > a").joinToString(", ") { it.text() }
-        // anime.//description = document.select("div.card-body").text()
-        // anime.author = document.select("li.production span.value").joinToString(", ") { it.text() }
-        // anime.status = parseStatus(document.select("li.status span.value").text())
+        anime.description = document.select("div.AsideContext > div.StoryMovieContent").text()
+        anime.author = document.select("li:contains(شركات الإنتاج) > p > a").joinToString(", ") { it.text() }
         return anime
     }
 
-    /*private fun parseStatus(statusString: String): Int {
-        return when (statusString) {
-            "Ongoing" -> SAnime.ONGOING
-            "Completed" -> SAnime.COMPLETED
-            else -> SAnime.UNKNOWN
-        }
-    }*/
-
-    override fun latestUpdatesNextPageSelector(): String = "ul.pagination li.page-item a[rel=next]"
+    override fun latestUpdatesNextPageSelector(): String = "ul.page-numbers li a.next"
 
     override fun latestUpdatesFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.setUrlWithoutDomain(element.attr("href"))
-        anime.title = element.select("div span").not(".badge").text()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.attr("title")
+        anime.thumbnail_url = element.select("a > span").attr("data-lazy-style").substringAfter("background-image:url(").substringBefore(");")
         return anime
     }
 
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/anime?s=rel-d&page=$page")
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/cima1/$page")
 
-    override fun latestUpdatesSelector(): String = "ul.anime-loop.loop li a"
+    override fun latestUpdatesSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
 }
