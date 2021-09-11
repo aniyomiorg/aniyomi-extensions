15084c9a15d36bde54ef74d66a0a574e9947ec72
Ahmed gamal <12355.ahmedgamal.com@gmail.com>
2021-09-11 17:15:41 +0200

    Add new source MYCIMA but for movies
    Add new source for mycima but for movies as the other extension only supports series and TV shows

---
+++ src/ar/mycimamovies/src/eu/kanade/tachiyomi/animeextension/ar/mycimamovie/MyCimaMovie.kt
@@ -0,0 +1,190 @@
+package eu.kanade.tachiyomi.animeextension.ar.mycimamovie
+
+import com.google.gson.JsonParser
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers.Companion.toHeaders
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.lang.Exception
+
+class MyCimaMovie : ParsedAnimeHttpSource() {
+
+    override val name = "MY Cima Movies"
+
+    override val baseUrl = "https://mycima.actor:2083"
+
+    override val lang = "ar"
+
+    override val supportsLatest = true
+
+    override val client: OkHttpClient = network.cloudflareClient
+
+    // Popular Anime
+
+    override fun popularAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
+
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/category/افلام-كرتون/page/$page/")
+
+    override fun popularAnimeFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.select("a strong.hasyear").text()
+        anime.thumbnail_url =
+            element.select("a > span.BG--GridItem")
+                .attr("data-lazy-style")
+                .substringAfter("background-image:url(")
+                .substringBefore(");")
+        return anime
+    }
+
+    override fun popularAnimeNextPageSelector(): String = "ul.page-numbers li a.next"
+
+    // Episodes
+
+    override fun episodeListSelector() = "div.Poster--Single-begin > a"
+
+    override fun episodeFromElement(element: Element): SEpisode {
+        val episode = SEpisode.create()
+        episode.setUrlWithoutDomain(element.attr("abs:href"))
+        episode.name = "movie"
+        return episode
+    }
+
+    // Video urls
+
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        val iframe = document.selectFirst("iframe").attr("data-lazy-src")
+        val referer = response.request.url.encodedPath
+        val newHeaderList = mutableMapOf(Pair("referer", baseUrl + referer))
+        headers.forEach { newHeaderList[it.first] = it.second }
+        val iframeResponse = client.newCall(GET(iframe, newHeaderList.toHeaders()))
+            .execute().asJsoup()
+        return videosFromElement(iframeResponse.selectFirst(videoListSelector()))
+    }
+
+    override fun videoListSelector() = "body"
+
+    private fun videosFromElement(element: Element): List<Video> {
+        val videoList = mutableListOf<Video>()
+        val script = element.select("script")
+            .firstOrNull { it.data().contains("player.qualityselector({") }
+        if (script != null) {
+            val videosString = script.data().substringAfter("sources: [")
+                .substringBefore("]").substringBeforeLast(",")
+            val videosArray = JsonParser.parseString("[$videosString]").asJsonArray
+            for (video in videosArray) {
+                val format = video.asJsonObject.get("format").asString
+                val url = video.asJsonObject.get("src").asString
+                if (format != "auto") {
+                    videoList.add(Video(url, format, url, null))
+                }
+            }
+            return videoList
+        }
+        val sourceTag = element.select("source").firstOrNull()!!
+        return listOf(Video(sourceTag.attr("src"), "Default", sourceTag.attr("src"), null))
+    }
+
+    override fun videoFromElement(element: Element) = throw Exception("not used")
+
+    override fun videoUrlParse(document: Document) = throw Exception("not used")
+
+    // search
+
+    override fun searchAnimeFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.select("a > strong.hasyear").text()
+        anime.thumbnail_url = element.select("a > span.BG--GridItem").attr("data-lazy-style").substringAfter("background-image:url(").substringBefore(");")
+        return anime
+    }
+
+    override fun searchAnimeNextPageSelector(): String = "ul.page-numbers li a.next"
+
+    override fun searchAnimeSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        val url = if (query.isNotBlank()) {
+            "$baseUrl/search/$query/page/$page"
+        } else {
+            (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
+                when (filter) {
+                    is CategoryList -> {
+                        if (filter.state > 0) {
+                            val catQ = getCategoryList()[filter.state].name
+                            val catUrl = "$baseUrl/$catQ/page/$page"
+                            return GET(catUrl, headers)
+                        }
+                    }
+                }
+            }
+            throw Exception("Choose a Filters")
+        }
+        return GET(url, headers)
+    }
+
+    // Details
+
+    override fun animeDetailsParse(document: Document): SAnime {
+        val anime = SAnime.create()
+        anime.title = document.select("div.Title--Content--Single-begin > h1").text()
+        anime.genre = document.select("li:contains(التصنيف) > p > a, li:contains(النوع) > p > a").joinToString(", ") { it.text() }
+        anime.description = document.select("div.AsideContext > div.StoryMovieContent, div.PostItemContent").text()
+        anime.author = document.select("li:contains(شركات الإنتاج) > p > a").joinToString(", ") { it.text() }
+        // add alternative name to anime description
+        document.select("li:contains( بالعربي) > p, li:contains(معروف) > p").text()?.let {
+            if (it.isEmpty().not()) {
+                anime.description += when {
+                    anime.description!!.isEmpty() -> "Alternative Name: $it"
+                    else -> "\n\nAlternativ Name: $it"
+                }
+            }
+        }
+        return anime
+    }
+
+    // Latest
+
+    override fun latestUpdatesNextPageSelector(): String = "ul.page-numbers li a.next"
+
+    override fun latestUpdatesFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.title = element.select("a > strong.hasyear").text()
+        anime.thumbnail_url = element.select("a > span").attr("data-lazy-style").substringAfter("background-image:url(").substringBefore(");")
+        return anime
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/movies/recent/page/$page/")
+
+    override fun latestUpdatesSelector(): String = "div.Grid--MycimaPosts div.GridItem div.Thumb--GridItem"
+
+    // Filters
+
+    override fun getFilterList() = AnimeFilterList(
+        CategoryList(categoriesName),
+    )
+
+    private class CategoryList(categories: Array<String>) : AnimeFilter.Select<String>("الأقسام", categories)
+    private data class CatUnit(val name: String)
+    private val categoriesName = getCategoryList().map {
+        it.name
+    }.toTypedArray()
+
+    private fun getCategoryList() = listOf(
+        CatUnit("اختر"),
+        CatUnit("category/افلام-كرتون"),
+        CatUnit("movies/best")
+    )
+}
