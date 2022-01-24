5b87b45b1d20aa925bad5cba06ac613141774af7
Ahmed gamal <12355.ahmedgamal.com@gmail.com>
2022-01-25 00:16:34 +0200

    fix WitAnime

--- src/ar/witanime/src/eu/kanade/tachiyomi/animeextension/ar/witanime/WitAnime.kt
+++ src/ar/witanime/src/eu/kanade/tachiyomi/animeextension/ar/witanime/WitAnime.kt
@@ -1,5 +1,13 @@
 package eu.kanade.tachiyomi.animeextension.ar.witanime
 
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animeextension.ar.witanime.extractors.FembedExtractor
+// import eu.kanade.tachiyomi.animeextension.ar.witanime.extractors.OkruExtractor
+import eu.kanade.tachiyomi.animeextension.ar.witanime.extractors.SoraPlayExtractor
+import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
@@ -7,15 +15,16 @@ import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Headers.Companion.toHeaders
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.lang.Exception
 
-class WitAnime : ParsedAnimeHttpSource() {
+class WitAnime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override val name = "WIT ANIME"
 
@@ -26,6 +35,10 @@ class WitAnime : ParsedAnimeHttpSource() {
     override val supportsLatest = false
 
     override val client: OkHttpClient = network.cloudflareClient
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
 
     override fun popularAnimeSelector(): String = "div.anime-list-content div:nth-child(1) div.col-lg-2 div.anime-card-container"
 
@@ -59,20 +72,85 @@ class WitAnime : ParsedAnimeHttpSource() {
 
     override fun videoListParse(response: Response): List<Video> {
         val document = response.asJsoup()
-        val iframe = document.selectFirst("ul.nav-tabs li a[id^=WitAnime-4shared]").attr("abs:data-ep-url")
-        val referer = response.request.url.encodedPath
-        val newHeaderList = mutableMapOf(Pair("referer", baseUrl + referer))
-        headers.forEach { newHeaderList[it.first] = it.second }
-        val iframeResponse = client.newCall(GET(iframe, newHeaderList.toHeaders()))
-            .execute().asJsoup()
-        return iframeResponse.select(videoListSelector()).map { videoFromElement(it) }
+        val videoList = mutableListOf<Video>()
+        document.select("ul#episode-servers li").forEach { it ->
+            val server = it.select("a").text()
+            val url = it.select("a").attr("data-ep-url")
+            when {
+                server.contains("fembed") -> {
+                    val videos = FembedExtractor().videosFromUrl(url)
+                    videoList.addAll(videos)
+                }
+                server.contains("soraplay") -> {
+                    val witAnime = "https://witanime.com/"
+                    val newHeaders = headers.newBuilder()
+                        .set("referer", "$witAnime")
+                        .build()
+                    val videos = SoraPlayExtractor(client).videosFromUrl(url, newHeaders)
+                    videoList.addAll(videos)
+                }
+                server.contains("yuistream") -> {
+                    val videos = FembedExtractor().videosFromUrl(url)
+                    videoList.addAll(videos)
+                }
+                /*server.contains("ok") -> {
+                    val videos = OkruExtractor(client).videosFromUrl(url)
+                    if (videos == null) {
+                        throw Exception("Not used")
+                    } else {
+                        videoList.addAll(videos)
+                    }
+                }*/
+            }
+
+            /*if (server == "fembed") {
+                val videos = FembedExtractor().videosFromUrl(url)
+                videoList.addAll(videos)
+            }
+            if (server == "soraplay") {
+                val witAnime = "https://witanime.com/"
+                val newHeaders = headers.newBuilder()
+                    .set("referer", "$witAnime")
+                    .build()
+                val videos = SoraPlayExtractor(client).videosFromUrl(url, newHeaders)
+                videoList.addAll(videos)
+            }
+            if (server == "yuistream") {
+                val videos = FembedExtractor().videosFromUrl(url)
+                videoList.addAll(videos)
+            }
+            if (server == "ok.ru") {
+                val videos = OkruExtractor(client).videosFromUrl(url)
+                videoList.addAll(videos)
+            }*/
+            /*if (server == "4shared") {
+                val videos = SharedExtractor(client).videosFromUrl(url)
+                videoList.addAll(videos)
+            }*/
+        }
+        return videoList
     }
 
-    override fun videoListSelector() = "source"
+    override fun videoListSelector() = throw Exception("not used")
 
-    override fun videoFromElement(element: Element): Video {
-        element.attr("src")
-        return Video(element.attr("src"), "Unknown quality", element.attr("src"), null)
+    override fun videoFromElement(element: Element) = throw Exception("not used")
+
+    override fun List<Video>.sort(): List<Video> {
+        val quality = preferences.getString("preferred_quality", "Fembed: 720p")
+        if (quality != null) {
+            val newList = mutableListOf<Video>()
+            var preferred = 0
+            for (video in this) {
+                if (video.quality == quality) {
+                    newList.add(preferred, video)
+                    preferred++
+                } else {
+                    newList.add(video)
+                }
+            }
+            return newList
+        }
+        return this
     }
 
     override fun videoUrlParse(document: Document) = throw Exception("not used")
@@ -115,4 +193,23 @@ class WitAnime : ParsedAnimeHttpSource() {
     override fun latestUpdatesRequest(page: Int): Request = throw Exception("Not used")
 
     override fun latestUpdatesSelector(): String = throw Exception("Not used")
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val videoQualityPref = ListPreference(screen.context).apply {
+            key = "preferred_quality"
+            title = "Preferred quality"
+            entries = arrayOf("Fembed:480p", "Fembed:720p", "Fembed:1080p", "Okru: full", "Okru: sd", "Okru: low", "Okru: lowest", "Okru: mobile", "4Shared", "soraplay: 360p", "soraplay: 480p", "soraplay: 720p", "soraplay: 1080p")
+            entryValues = arrayOf("Fembed:480p", "Fembed:720p", "Fembed:1080p", "Okru: full", "Okru: sd", "Okru: low", "Okru: lowest", "Okru: mobile", "4Shared", "soraplay: 360p", "soraplay: 480p", "soraplay: 720p", "soraplay: 1080p")
+            setDefaultValue("Fembed:720p")
+            summary = "%s"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val selected = newValue as String
+                val index = findIndexOfValue(selected)
+                val entry = entryValues[index] as String
+                preferences.edit().putString(key, entry).commit()
+            }
+        }
+        screen.addPreference(videoQualityPref)
+    }
 }
