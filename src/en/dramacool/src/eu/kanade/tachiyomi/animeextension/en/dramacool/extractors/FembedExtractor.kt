248186226b2327dfc930b6bbe465c7726f4a7ad0
Ahmed gamal <12355.ahmedgamal.com@gmail.com>
2022-02-10 15:46:59 +0200

    fix for FembedExtractor

--- src/en/dramacool/src/eu/kanade/tachiyomi/animeextension/en/dramacool/extractors/FembedExtractor.kt
+++ src/en/dramacool/src/eu/kanade/tachiyomi/animeextension/en/dramacool/extractors/FembedExtractor.kt
@@ -9,14 +9,20 @@ class FembedExtractor {
     fun videosFromUrl(url: String): List<Video> {
         val videoApi = url.replace("/v/", "/api/source/")
         val json = JSONObject(Jsoup.connect(videoApi).ignoreContentType(true).method(Connection.Method.POST).execute().body())
-        check(json.getBoolean("success")) { "Request was not succeeded" }
         val videoList = mutableListOf<Video>()
-
-        val jsonArray = json.getJSONArray("data")
-        for (i in 0 until jsonArray.length()) {
-            val `object` = jsonArray.getJSONObject(i)
-            val videoUrl = `object`.getString("file")
-            val quality = "Fembed:" + `object`.getString("label")
+        if (json.getBoolean("success")) {
+            val videoList = mutableListOf<Video>()
+            val jsonArray = json.getJSONArray("data")
+            for (i in 0 until jsonArray.length()) {
+                val `object` = jsonArray.getJSONObject(i)
+                val videoUrl = `object`.getString("file")
+                val quality = "Fembed:" + `object`.getString("label")
+                videoList.add(Video(videoUrl, quality, videoUrl, null))
+            }
+            return videoList
+        } else {
+            val videoUrl = "not used"
+            val quality = "Video taken down for dmca"
             videoList.add(Video(videoUrl, quality, videoUrl, null))
         }
         return videoList
