b171a1f3819ae79fc7705d7e048424810d290396
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-02-11 22:29:23 +0530

    Add new source : Animixplay (#324)
    * Add animixplay
    
    * completed streamSb Extractor
    
    * add Animixplay source
    
    * update StreamSbExtractor

---
+++ src/en/animixplay/src/eu/kanade/tachiyomi/animeextension/en/animixplay/Animixplay.kt
@@ -0,0 +1,293 @@
+package eu.kanade.tachiyomi.animeextension.en.animixplay
+
+import android.app.Application
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animeextension.en.animixplay.extractors.DoodExtractor
+import eu.kanade.tachiyomi.animeextension.en.animixplay.extractors.GogoCdnExtractor
+import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.FormBody
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import uy.kohesive.injekt.injectLazy
+import java.lang.Exception
+import java.util.Locale
+
+@ExperimentalSerializationApi
+class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
+
+    override val name = "Animixplay"
+
+    override val baseUrl = "https://animixplay.to"
+
+    override val lang = "en"
+
+    override val supportsLatest = false
+
+    override val client: OkHttpClient = network.cloudflareClient
+
+    private val json: Json by injectLazy()
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
+    override fun popularAnimeSelector(): String = throw Exception("not used")
+
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/?tab=popular")
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        val document = response.asJsoup()
+        val scriptData = document.select("script:containsData(var nowtime)").toString()
+        val envSeason = scriptData.substringAfter("var envSeason = '").substringBefore("';")
+        val envYear = scriptData.substringAfter("var envYear = ").substringBefore(";")
+        val animeJson = json.decodeFromString<JsonObject>(
+            client.newCall(
+                GET(
+                    url = "https://animixplay.to/assets/season/$envYear/${
+                    envSeason.toLowerCase(
+                        Locale.ROOT
+                    )
+                    }.json",
+                    headers = Headers.headersOf("Referer", document.location())
+                )
+            ).execute().body!!.string()
+        )
+        val animeList = animeJson["anime"]!!.jsonObject
+        val animes = animeList.map { element ->
+            popularAnimeFromElement(element.value.jsonObject)
+        }
+
+        return AnimesPage(animes, false)
+    }
+
+    override fun popularAnimeFromElement(element: Element) = throw Exception("not used")
+
+    private fun popularAnimeFromElement(animeJson: JsonObject): SAnime {
+        val anime = SAnime.create()
+        val url = "https://animixplay.to/assets/mal/${
+        animeJson["mal_id"]!!.jsonPrimitive.content
+        }.json"
+        anime.setUrlWithoutDomain(url)
+        anime.thumbnail_url = animeJson["image_url"]!!.jsonPrimitive.content
+        anime.title = animeJson["title"]!!.jsonPrimitive.content
+        return anime
+    }
+
+    override fun popularAnimeNextPageSelector(): String = throw Exception("not used")
+
+    override fun episodeListSelector() = throw Exception("not used")
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val document = getDocumentFromRequestUrl(response)
+        val animeJson = json.decodeFromString<JsonObject>(document.select("body").text())
+        val malId = animeJson["mal_id"]!!.jsonPrimitive.int
+
+        return episodesRequest(malId, document)
+    }
+
+    private fun episodesRequest(malId: Int, document: Document): List<SEpisode> {
+        // POST data
+        val body = FormBody.Builder()
+            .add("recomended", malId.toString())
+            .build()
+        val animeServersJson = json.decodeFromString<JsonObject>(
+            client.newCall(
+                POST(
+                    "https://animixplay.to/api/search",
+                    body = body,
+                    headers = Headers.headersOf("Referer", document.location())
+                )
+            ).execute().body!!.string()
+        )
+        val urlEndpoint = animeServersJson["data"]!!.jsonArray[0].jsonObject["items"]!!.jsonArray[0].jsonObject["url"]!!.jsonPrimitive.content
+        val episodesResponse = client.newCall(
+            GET(
+                baseUrl + urlEndpoint,
+            )
+        ).execute().asJsoup()
+        val episodeListJson = json.decodeFromString<JsonObject>(episodesResponse.select("div#epslistplace").text())
+        val episodeAvailable = episodeListJson["eptotal"]!!.jsonPrimitive.int
+        val episodeList = mutableListOf<SEpisode>()
+
+        for (i in 0 until episodeAvailable) {
+            episodeList.add(episodeFromJsonElement(baseUrl + urlEndpoint, i))
+        }
+        return episodeList
+    }
+
+    override fun episodeFromElement(element: Element): SEpisode = throw Exception("not used")
+
+    private fun episodeFromJsonElement(url: String, number: Int): SEpisode {
+        val episode = SEpisode.create()
+        episode.setUrlWithoutDomain("$url/ep$number")
+        episode.episode_number = number.toFloat()
+        episode.name = "Episode ${number + 1}"
+        episode.date_upload = System.currentTimeMillis()
+        return episode
+    }
+
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        val episodeListJson = json.decodeFromString<JsonObject>(document.select("div#epslistplace").text())
+        val epNo = response.request.url.toString().substringAfter("/ep")
+        val serverUrl = "https:" + episodeListJson[epNo]!!.jsonPrimitive.content
+        val gogoVideos = GogoCdnExtractor(client, json).videosFromUrl(serverUrl)
+        return gogoVideos.ifEmpty {
+            DoodExtractor(client).videosFromUrl(serverUrl)
+        }
+    }
+
+    override fun videoListSelector() = throw Exception("not used")
+
+    override fun videoFromElement(element: Element) = throw Exception("not used")
+
+    override fun videoUrlParse(document: Document) = throw Exception("not used")
+
+    override fun List<Video>.sort(): List<Video> {
+        val quality = preferences.getString("preferred_quality", "1080")
+        if (quality != null) {
+            val newList = mutableListOf<Video>()
+            var preferred = 0
+            for (video in this) {
+                if (video.quality.contains(quality)) {
+                    newList.add(preferred, video)
+                    preferred++
+                } else {
+                    newList.add(video)
+                }
+            }
+            return newList
+        }
+        return this
+    }
+
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        val responseJson = json.decodeFromString<JsonObject>(response.body!!.string())
+        val document = Jsoup.parse(responseJson["result"]!!.jsonPrimitive.content)
+        val animeList = document.select("li")
+        val animes = animeList.map {
+            searchAnimeFromElement(it)
+        }
+        return AnimesPage(animes, false)
+    }
+
+    override fun searchAnimeFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("div a").attr("href"))
+        anime.thumbnail_url = element.select("img").first().attr("src")
+        anime.title = element.select("p.name a").attr("title")
+        return anime
+    }
+
+    override fun searchAnimeNextPageSelector(): String = throw Exception("not used")
+
+    override fun searchAnimeSelector(): String = throw Exception("not used")
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        val formData = FormBody.Builder()
+            .addEncoded("q2", query)
+            .addEncoded("root", "animixplay.to")
+            .addEncoded("origin", "1")
+            .build()
+        return when {
+            query.isNotBlank() -> POST("https://v1.ic5qwh28vwloyjo28qtg.workers.dev/", headers, formData)
+            else -> GET("$baseUrl/?tab=popular")
+        }
+    }
+
+    override fun animeDetailsParse(response: Response): SAnime {
+        Log.d("detail", response.request.url.toString())
+        return animeDetailsParse(getDocumentFromRequestUrl(response))
+    }
+
+    private fun getDocumentFromRequestUrl(response: Response): Document {
+        return if (!response.request.url.toString().contains(".json")) {
+            val scriptData = response.asJsoup().select("script:containsData(var malid )").toString()
+            val malId = scriptData.substringAfter("var malid = '").substringBefore("';")
+            val url = "https://animixplay.to/assets/mal/$malId.json"
+            client.newCall(
+                GET(
+                    url,
+                    headers
+                )
+            ).execute().asJsoup()
+        } else {
+            response.asJsoup()
+        }
+    }
+
+    override fun animeDetailsParse(document: Document): SAnime {
+        val anime = SAnime.create()
+        val animeJson = json.decodeFromString<JsonObject>(document.select("body").text())
+        anime.title = animeJson["title"]!!.jsonPrimitive.content
+        anime.genre =
+            animeJson["genres"]!!.jsonArray.joinToString { it.jsonObject["name"]!!.jsonPrimitive.content }
+        anime.description = animeJson["synopsis"]!!.jsonPrimitive.content
+        anime.status = parseStatus(animeJson["status"]!!.jsonPrimitive.content)
+        anime.author =
+            animeJson["studios"]!!.jsonArray[0].jsonObject["name"]!!.jsonPrimitive.content
+        return anime
+    }
+
+    private fun parseStatus(statusString: String): Int {
+        return when (statusString) {
+            "Currently Airing" -> SAnime.ONGOING
+            "Finished Airing" -> SAnime.COMPLETED
+            else -> SAnime.UNKNOWN
+        }
+    }
+
+    override fun latestUpdatesNextPageSelector(): String = throw Exception("not used")
+
+    override fun latestUpdatesFromElement(element: Element): SAnime = throw Exception("not used")
+
+    override fun latestUpdatesRequest(page: Int): Request = throw Exception("not used")
+
+    override fun latestUpdatesSelector(): String = throw Exception("not used")
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val videoQualityPref = ListPreference(screen.context).apply {
+            key = "preferred_quality"
+            title = "Preferred quality"
+            entries = arrayOf("1080p", "720p", "480p", "360p")
+            entryValues = arrayOf("1080", "720", "480", "360")
+            setDefaultValue("1080")
+            summary = "%s"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val selected = newValue as String
+                val index = findIndexOfValue(selected)
+                val entry = entryValues[index] as String
+                preferences.edit().putString(key, entry).commit()
+            }
+        }
+        screen.addPreference(videoQualityPref)
+    }
+}
