ab147aef4de584d3cb9cc071fe5afb524e409f83
GouravShDev <gourav0sharma1@gmail.com>
2022-02-11 16:45:15 +0530

    add Animixplay source

--- src/en/animixplay/src/eu/kanade/tachiyomi/animeextension/en/animixplay/Animixplay.kt
+++ src/en/animixplay/src/eu/kanade/tachiyomi/animeextension/en/animixplay/Animixplay.kt
@@ -2,12 +2,12 @@ package eu.kanade.tachiyomi.animeextension.en.animixplay
 
 import android.app.Application
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animeextension.en.animixplay.extractors.DoodExtractor
-import eu.kanade.tachiyomi.animeextension.en.animixplay.extractors.StreamSbExtractor
+import eu.kanade.tachiyomi.animeextension.en.animixplay.extractors.GogoCdnExtractor
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
-import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.animesource.model.SAnime
@@ -30,6 +30,7 @@ import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import uy.kohesive.injekt.Injekt
@@ -47,7 +48,7 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override val lang = "en"
 
-    override val supportsLatest = true
+    override val supportsLatest = false
 
     override val client: OkHttpClient = network.cloudflareClient
 
@@ -57,7 +58,7 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
 
-    override fun popularAnimeSelector(): String = "div.img a"
+    override fun popularAnimeSelector(): String = throw Exception("not used")
 
     override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/?tab=popular")
 
@@ -99,13 +100,12 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         return anime
     }
 
-    override fun popularAnimeNextPageSelector(): String =
-        "ul.pagination-list li:last-child:not(.selected)"
+    override fun popularAnimeNextPageSelector(): String = throw Exception("not used")
 
-    override fun episodeListSelector() = "div#epslistplace"
+    override fun episodeListSelector() = throw Exception("not used")
 
     override fun episodeListParse(response: Response): List<SEpisode> {
-        val document = response.asJsoup()
+        val document = getDocumentFromRequestUrl(response)
         val animeJson = json.decodeFromString<JsonObject>(document.select("body").text())
         val malId = animeJson["mal_id"]!!.jsonPrimitive.int
 
@@ -157,11 +157,8 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         val document = response.asJsoup()
         val episodeListJson = json.decodeFromString<JsonObject>(document.select("div#epslistplace").text())
         val epNo = response.request.url.toString().substringAfter("/ep")
-//        val serverUrl = "https:" + episodeListJson[epNo]!!.jsonPrimitive.content
-        val serverUrl = "https://streamsb.net/e/g13hrgae001c.html"
-
-//        val gogoVideos = GogoCdnExtractor(client, json).videosFromUrl(serverUrl)
-        val gogoVideos = StreamSbExtractor(client).videosFromUrl(serverUrl)
+        val serverUrl = "https:" + episodeListJson[epNo]!!.jsonPrimitive.content
+        val gogoVideos = GogoCdnExtractor(client, json).videosFromUrl(serverUrl)
         return gogoVideos.ifEmpty {
             DoodExtractor(client).videosFromUrl(serverUrl)
         }
@@ -191,51 +188,71 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         return this
     }
 
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        val responseJson = json.decodeFromString<JsonObject>(response.body!!.string())
+        val document = Jsoup.parse(responseJson["result"]!!.jsonPrimitive.content)
+        val animeList = document.select("li")
+        val animes = animeList.map {
+            searchAnimeFromElement(it)
+        }
+        return AnimesPage(animes, false)
+    }
+
     override fun searchAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.setUrlWithoutDomain(element.attr("href"))
+        anime.setUrlWithoutDomain(element.select("div a").attr("href"))
         anime.thumbnail_url = element.select("img").first().attr("src")
-        anime.title = element.attr("title")
+        anime.title = element.select("p.name a").attr("title")
         return anime
     }
 
-    override fun searchAnimeNextPageSelector(): String =
-        "ul.pagination-list li:last-child:not(.selected)"
+    override fun searchAnimeNextPageSelector(): String = throw Exception("not used")
 
-    override fun searchAnimeSelector(): String = "div.img a"
+    override fun searchAnimeSelector(): String = throw Exception("not used")
 
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
-        val filterList = if (filters.isEmpty()) getFilterList() else filters
-        val genreFilter = filterList.find { it is GenreFilter } as GenreFilter
-
+        val formData = FormBody.Builder()
+            .addEncoded("q2", query)
+            .addEncoded("root", "animixplay.to")
+            .addEncoded("origin", "1")
+            .build()
         return when {
-            query.isNotBlank() -> GET("$baseUrl/search.html?keyword=$query&page=$page", headers)
-            genreFilter.state != 0 -> GET("$baseUrl/genre/${genreFilter.toUriPart()}?page=$page")
-            else -> GET("$baseUrl/popular.html?page=$page")
+            query.isNotBlank() -> POST("https://v1.ic5qwh28vwloyjo28qtg.workers.dev/", headers, formData)
+            else -> GET("$baseUrl/?tab=popular")
+        }
+    }
+
+    override fun animeDetailsParse(response: Response): SAnime {
+        Log.d("detail", response.request.url.toString())
+        return animeDetailsParse(getDocumentFromRequestUrl(response))
+    }
+
+    private fun getDocumentFromRequestUrl(response: Response): Document {
+        return if (!response.request.url.toString().contains(".json")) {
+            val scriptData = response.asJsoup().select("script:containsData(var malid )").toString()
+            val malId = scriptData.substringAfter("var malid = '").substringBefore("';")
+            val url = "https://animixplay.to/assets/mal/$malId.json"
+            client.newCall(
+                GET(
+                    url,
+                    headers
+                )
+            ).execute().asJsoup()
+        } else {
+            response.asJsoup()
         }
     }
 
     override fun animeDetailsParse(document: Document): SAnime {
         val anime = SAnime.create()
         val animeJson = json.decodeFromString<JsonObject>(document.select("body").text())
-        anime.title = animeJson["title_english"]!!.jsonPrimitive.content
+        anime.title = animeJson["title"]!!.jsonPrimitive.content
         anime.genre =
             animeJson["genres"]!!.jsonArray.joinToString { it.jsonObject["name"]!!.jsonPrimitive.content }
-//        anime.genre = document.select("p.type:eq(5) a").joinToString("") { it.text() }
         anime.description = animeJson["synopsis"]!!.jsonPrimitive.content
         anime.status = parseStatus(animeJson["status"]!!.jsonPrimitive.content)
         anime.author =
             animeJson["studios"]!!.jsonArray[0].jsonObject["name"]!!.jsonPrimitive.content
-        // add alternative name to anime description
-//        val altName = "Other name(s): "
-//        document.select("p.type:eq(8)").firstOrNull()?.ownText()?.let {
-//            if (it.isBlank().not()) {
-//                anime.description = when {
-//                    anime.description.isNullOrBlank() -> altName + it
-//                    else -> anime.description + "\n\n$altName" + it
-//                }
-//            }
-//        }
         return anime
     }
 
@@ -247,25 +264,13 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         }
     }
 
-    override fun latestUpdatesNextPageSelector(): String =
-        "ul.pagination-list li:last-child:not(.selected)"
+    override fun latestUpdatesNextPageSelector(): String = throw Exception("not used")
 
-    override fun latestUpdatesFromElement(element: Element): SAnime {
-        val anime = SAnime.create()
-        anime.setUrlWithoutDomain(baseUrl + element.attr("href"))
-        val style = element.select("div.thumbnail-popular").attr("style")
-        anime.thumbnail_url = style.substringAfter("background: url('").substringBefore("');")
-        anime.title = element.attr("title")
-        return anime
-    }
+    override fun latestUpdatesFromElement(element: Element): SAnime = throw Exception("not used")
 
-    override fun latestUpdatesRequest(page: Int): Request =
-        GET(
-            "https://ajax.gogo-load.com/ajax/page-recent-release-ongoing.html?page=$page&type=1",
-            headers
-        )
+    override fun latestUpdatesRequest(page: Int): Request = throw Exception("not used")
 
-    override fun latestUpdatesSelector(): String = "div.added_series_body.popular li a:has(div)"
+    override fun latestUpdatesSelector(): String = throw Exception("not used")
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
         val videoQualityPref = ListPreference(screen.context).apply {
@@ -284,69 +289,5 @@ class Animixplay : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
             }
         }
         screen.addPreference(videoQualityPref)
-    }
-
-    // Filters
-    override fun getFilterList(): AnimeFilterList = AnimeFilterList(
-        AnimeFilter.Header("Text search ignores filters"),
-        GenreFilter()
-    )
-
-    private class GenreFilter : UriPartFilter(
-        "Genres",
-        arrayOf(
-            Pair("<select>", ""),
-            Pair("Action", "action"),
-            Pair("Adventure", "adventure"),
-            Pair("Cars", "cars"),
-            Pair("Comedy", "comedy"),
-            Pair("Crime", "crime"),
-            Pair("Dementia", "dementia"),
-            Pair("Demons", "demons"),
-            Pair("Drama", "drama"),
-            Pair("Dub", "dub"),
-            Pair("Ecchi", "ecchi"),
-            Pair("Family", "family"),
-            Pair("Fantasy", "fantasy"),
-            Pair("Game", "game"),
-            Pair("Harem", "harem"),
-            Pair("Historical", "historical"),
-            Pair("Horror", "horror"),
-            Pair("Josei", "josei"),
-            Pair("Kids", "kids"),
-            Pair("Magic", "magic"),
-            Pair("Martial Arts", "martial-arts"),
-            Pair("Mature", "mature"),
-            Pair("Mecha", "mecha"),
-            Pair("Military", "military"),
-            Pair("Music", "music"),
-            Pair("Mystery", "mystery"),
-            Pair("Parody", "parody"),
-            Pair("Police", "police"),
-            Pair("Psychological", "psychological"),
-            Pair("Romance", "romance"),
-            Pair("Samurai", "samurai"),
-            Pair("School", "school"),
-            Pair("Sci-Fi", "sci-fi"),
-            Pair("Seinen", "seinen"),
-            Pair("Shoujo", "shoujo"),
-            Pair("Shoujo Ai", "shoujo-ai"),
-            Pair("Shounen", "shounen"),
-            Pair("Shounen Ai", "shounen-ai"),
-            Pair("Slice of Life", "slice-of-life"),
-            Pair("Space", "space"),
-            Pair("Sports", "sports"),
-            Pair("Super Power", "super-power"),
-            Pair("Supernatural", "supernatural"),
-            Pair("Thriller", "thriller"),
-            Pair("Vampire", "vampire"),
-            Pair("Yaoi", "yaoi"),
-            Pair("Yuri", "yuri")
-        )
-    )
-
-    private open class UriPartFilter(displayName: String, val vals: Array<Pair<String, String>>) :
-        AnimeFilter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
-        fun toUriPart() = vals[state].second
     }
 }
