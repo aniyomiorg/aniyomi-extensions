63d6acc5ab5f8e8b4e39ba36bd72bef2fcdc415d
Ahmed gamal <12355.ahmedgamal.com@gmail.com>
2022-02-24 17:28:47 +0200

    genoanime: add headers for streamSB extractor (#357)

--- src/en/genoanime/src/eu/kanade/tachiyomi/animeextension/en/genoanime/GenoAnime.kt
+++ src/en/genoanime/src/eu/kanade/tachiyomi/animeextension/en/genoanime/GenoAnime.kt
@@ -140,6 +140,9 @@ class GenoAnime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
                     val callMaster = client.newCall(GET(master, nheaders)).execute().asJsoup()
                     Log.i("testt", "$callMaster")*/
                     val headers = headers.newBuilder()
+                        .set("Referer", url)
+                        .set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0")
+                        .set("Accept-Language", "en-US,en;q=0.5")
                         .set("watchsb", "streamsb")
                         .build()
                     val videos = StreamSBExtractor(client).videosFromUrl(url, headers)
--- src/en/genoanime/src/eu/kanade/tachiyomi/animeextension/en/genoanime/extractors/StreamSBExtractor.kt
+++ src/en/genoanime/src/eu/kanade/tachiyomi/animeextension/en/genoanime/extractors/StreamSBExtractor.kt
@@ -9,7 +9,6 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import okhttp3.Headers
 import okhttp3.OkHttpClient
-import org.jsoup.Jsoup
 
 class StreamSBExtractor(private val client: OkHttpClient) {
 
@@ -35,16 +34,19 @@ class StreamSBExtractor(private val client: OkHttpClient) {
         Log.i("bytesToHex", bytesToHex)
         val master = "https://sbplay2.com/sources40/566d337678566f743674494a7c7c${bytesToHex}7c7c346b6767586d6934774855537c7c73747265616d7362/6565417268755339773461447c7c346133383438333436313335376136323337373433383634376337633465366534393338373136643732373736343735373237613763376334363733353737303533366236333463353333363534366137633763373337343732363536313664373336327c7c6b586c3163614468645a47617c7c73747265616d7362"
         Log.i("master", master)
-        val json = Json.decodeFromString<JsonObject>(Jsoup.connect(master).ignoreContentType(true).header("watchsb", "streamsb").execute().body())
+        val json = Json.decodeFromString<JsonObject>(
+            client.newCall(GET(master, headers))
+                .execute().body!!.string()
+        )
         Log.i("json", "$json")
         val masterUrl = json["stream_data"]!!.jsonObject["file"].toString().trim('"')
-        val masterPlaylist = client.newCall(GET(masterUrl)).execute().body!!.string()
+        val masterPlaylist = client.newCall(GET(masterUrl, headers)).execute().body!!.string()
         Log.i("masterplayl", masterPlaylist)
         val videoList = mutableListOf<Video>()
         masterPlaylist.substringAfter("#EXT-X-STREAM-INF:").split("#EXT-X-STREAM-INF:").forEach {
             val quality = "StreamSB:" + it.substringAfter("RESOLUTION=").substringAfter("x").substringBefore(",") + "p"
             val videoUrl = it.substringAfter("\n").substringBefore("\n")
-            videoList.add(Video(videoUrl, quality, videoUrl, null))
+            videoList.add(Video(videoUrl, quality, videoUrl, null, headers))
         }
         return videoList
     }
