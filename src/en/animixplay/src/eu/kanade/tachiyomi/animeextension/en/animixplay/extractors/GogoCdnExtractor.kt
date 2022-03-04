a0489bda0c7162cf6e7413247d8d84b323289943
GouravShDev <gourav0sharma1@gmail.com>
2022-03-04 21:09:48 +0530

    add fix for gogo server

--- src/en/animixplay/src/eu/kanade/tachiyomi/animeextension/en/animixplay/extractors/GogoCdnExtractor.kt
+++ src/en/animixplay/src/eu/kanade/tachiyomi/animeextension/en/animixplay/extractors/GogoCdnExtractor.kt
@@ -3,13 +3,13 @@ package eu.kanade.tachiyomi.animeextension.en.animixplay.extractors
 import android.util.Base64
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
@@ -23,37 +23,60 @@ import javax.crypto.spec.SecretKeySpec
 class GogoCdnExtractor(private val client: OkHttpClient, private val json: Json) {
     fun videosFromUrl(serverUrl: String): List<Video> {
         try {
-            val serverResponse = client.newCall(GET(serverUrl)).execute().asJsoup()
+            val id = serverUrl.toHttpUrl().queryParameter("id") ?: throw Exception("error getting id")
+            val iv = "31323835363732333833393339383532".decodeHex()
+            val secretKey = "3235373136353338353232393338333936313634363632323738383333323838".decodeHex()
 
-            val encrypted = serverResponse.select("script[data-name='crypto']").attr("data-value")
-            val iv = serverResponse.select("script[data-name='ts']").attr("data-value").toByteArray()
-            val id = serverUrl.toHttpUrl().queryParameter("id") ?: throw Exception("error decrypting")
-            val secretKey = cryptoHandler(encrypted, iv, iv + iv, false)
-
-            val encryptedId =
-                cryptoHandler(id, "0000000000000000".toByteArray(), secretKey.toByteArray())
+            val encryptedId = try { cryptoHandler(id, iv, secretKey) } catch (e: Exception) { e.message ?: "" }
 
             val jsonResponse = client.newCall(
                 GET(
-                    "http://gogoplay.io/encrypt-ajax.php?id=$encryptedId&time=00000000000000000000",
+                    "http://gogoplay.io/encrypt-ajax.php?id=$encryptedId",
                     Headers.headersOf("X-Requested-With", "XMLHttpRequest")
                 )
             ).execute().body!!.string()
+            val data = json.decodeFromString<JsonObject>(jsonResponse)["data"]!!.jsonPrimitive.content
+            val decryptedData = cryptoHandler(data, iv, secretKey, false)
             val videoList = mutableListOf<Video>()
             val autoList = mutableListOf<Video>()
-            json.decodeFromString<JsonObject>(jsonResponse)["source"]!!.jsonArray.forEach {
+            val array = json.decodeFromString<JsonObject>(decryptedData)["source"]!!.jsonArray
+            if (array.size == 1 && array[0].jsonObject["type"]!!.jsonPrimitive.content == "hls") {
+                val fileURL = array[0].jsonObject["file"].toString().trim('"')
+                val masterPlaylist = client.newCall(GET(fileURL)).execute().body!!.string()
+                masterPlaylist.substringAfter("#EXT-X-STREAM-INF:")
+                    .split("#EXT-X-STREAM-INF:").reversed().forEach {
+                        val quality = it.substringAfter("RESOLUTION=").substringAfter("x").substringBefore("\n") + "p"
+                        val videoUrl = fileURL.substringBeforeLast("/") + "/" + it.substringAfter("\n").substringBefore("\n")
+                        videoList.add(Video(videoUrl, quality, videoUrl, null))
+                    }
+            } else array.forEach {
                 val label = it.jsonObject["label"].toString().toLowerCase(Locale.ROOT)
                     .trim('"').replace(" ", "")
                 val fileURL = it.jsonObject["file"].toString().trim('"')
                 val videoHeaders = Headers.headersOf("Referer", serverUrl)
-                if (label == "auto") autoList.add(Video(fileURL, label, fileURL, null, videoHeaders))
+                if (label == "auto") autoList.add(
+                    Video(
+                        fileURL,
+                        label,
+                        fileURL,
+                        null,
+                        videoHeaders
+                    )
+                )
                 else videoList.add(Video(fileURL, label, fileURL, null, videoHeaders))
             }
             return videoList.reversed() + autoList
-        } catch (e: Exception) { return emptyList() }
+        } catch (e: Exception) {
+            return emptyList()
+        }
     }
 
-    private fun cryptoHandler(string: String, iv: ByteArray, secretKeyString: ByteArray, encrypt: Boolean = true): String {
+    private fun cryptoHandler(
+        string: String,
+        iv: ByteArray,
+        secretKeyString: ByteArray,
+        encrypt: Boolean = true
+    ): String {
         val ivParameterSpec = IvParameterSpec(iv)
         val secretKey = SecretKeySpec(secretKeyString, "AES")
         val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
@@ -64,5 +87,12 @@ class GogoCdnExtractor(private val client: OkHttpClient, private val json: Json)
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec)
             Base64.encodeToString(cipher.doFinal(string.toByteArray()), Base64.NO_WRAP)
         }
+    }
+
+    private fun String.decodeHex(): ByteArray {
+        check(length % 2 == 0) { "Must have an even length" }
+        return chunked(2)
+            .map { it.toInt(16).toByte() }
+            .toByteArray()
     }
 }
