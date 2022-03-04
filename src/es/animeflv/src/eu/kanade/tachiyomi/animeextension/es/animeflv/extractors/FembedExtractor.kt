ee02a35ac00ff60338c7104067c27d955e8792d0
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2022-03-04 09:58:25 -0600

    Add DUB option [AnimeFLV] (#386)

--- src/es/animeflv/src/eu/kanade/tachiyomi/animeextension/es/animeflv/extractors/FembedExtractor.kt
+++ src/es/animeflv/src/eu/kanade/tachiyomi/animeextension/es/animeflv/extractors/FembedExtractor.kt
@@ -6,17 +6,18 @@ import org.jsoup.Connection
 import org.jsoup.Jsoup
 
 class FembedExtractor {
-    fun videosFromUrl(url: String): List<Video> {
+    fun videosFromUrl(url: String, qualityPrefix: String = ""): List<Video> {
         val videoApi = url.replace("/v/", "/api/source/")
         val json = JSONObject(Jsoup.connect(videoApi).ignoreContentType(true).method(Connection.Method.POST).execute().body())
         val videoList = mutableListOf<Video>()
+        val prefix = qualityPrefix
         if (json.getBoolean("success")) {
             val videoList = mutableListOf<Video>()
             val jsonArray = json.getJSONArray("data")
             for (i in 0 until jsonArray.length()) {
                 val `object` = jsonArray.getJSONObject(i)
                 val videoUrl = `object`.getString("file")
-                val quality = "Fembed:" + `object`.getString("label")
+                val quality = prefix + "Fembed:" + `object`.getString("label")
                 videoList.add(Video(videoUrl, quality, videoUrl, null))
             }
             return videoList
