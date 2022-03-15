cf7e14ec4006c99a71f508e6a75fa96e41683509
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-03-15 11:40:38 -0300

    New source: AnimesVision (#399)

---
+++ src/pt/animesvision/src/eu/kanade/tachiyomi/animeextension/pt/animesvision/extractors/VisionFreeExtractor.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.animeextension.pt.animesvision.extractors
+
+import android.util.Log
+import eu.kanade.tachiyomi.animesource.model.Video
+
+class VisionFreeExtractor {
+
+    private val REGEX_VISION_PLAYER = Regex(""""file":"(\S+?)",.*?"label":"(.*?)"""")
+    private val TAG = "VisionFreeExtractor"
+
+    fun videoListFromHtml(html: String): List<Video> {
+        return REGEX_VISION_PLAYER.findAll(html).map {
+            val videoUrl = it.groupValues[1].replace("\\", "")
+            val quality = it.groupValues[2]
+            Log.i(TAG, "$videoUrl -> $quality")
+            Video(videoUrl, quality, videoUrl, null)
+        }.toList()
+    }
+}
