130a201f6e11456fd4fd4e410e69a0110ec4efa1
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-03-26 07:48:31 -0300

    New source: Goyabu (#429)

---
+++ src/pt/goyabu/src/eu/kanade/tachiyomi/animeextension/pt/goyabu/dto/SearchResultDto.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.animeextension.pt.goyabu
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.JsonTransformingSerializer
+
+@Serializable
+data class SearchResultDto(
+    val title: String,
+    val genre: String,
+    @Serializable(with = IntSerializer::class)
+    val videos: Int,
+    val cover: String,
+    val type: String,
+    val slug: String
+)
+
+object IntSerializer : JsonTransformingSerializer<Int>(Int.serializer()) {
+    override fun transformDeserialize(element: JsonElement): JsonElement {
+        return try {
+            JsonPrimitive(element.toString().toInt())
+        } catch (e: Exception) { JsonPrimitive(-1) }
+    }
+}
