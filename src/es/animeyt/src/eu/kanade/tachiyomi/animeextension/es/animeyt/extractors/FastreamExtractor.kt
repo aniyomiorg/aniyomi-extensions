ac3ae10864d3f524b29213aa82b3e2e0a46803e0
miguelantonioe <miguelantonio.e@hotmail.com>
2022-07-06 03:07:04 -0500

    extension finished (#621)

---
+++ src/es/animeyt/src/eu/kanade/tachiyomi/animeextension/es/animeyt/extractors/FastreamExtractor.kt
@@ -0,0 +1,18 @@
+package eu.kanade.tachiyomi.animeextension.es.animeyt.extractors
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+
+class FastreamExtractor(private val client: OkHttpClient) {
+    fun videoFromUrl(url: String, server: String): Video {
+        var url1 = ""
+        client.newCall(GET(url)).execute().asJsoup().select("script").forEach {
+            if (it.data().contains("jwplayer(\"vplayer\").setup({")) {
+                url1 = it.data().substringAfter("sources: [{file:\"").substringBefore("\"}],")
+            }
+        }
+        return Video(url1, server, url1, null)
+    }
+}
