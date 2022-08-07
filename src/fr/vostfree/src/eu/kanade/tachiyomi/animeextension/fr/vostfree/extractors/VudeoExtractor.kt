c3110f90da0eab1f4881b3318b43cb0fc3183fd6
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2022-08-06 20:44:41 -0400

    Fix Search and Video Extractor [VostFree]] (#738)

--- src/fr/vostfree/src/eu/kanade/tachiyomi/animeextension/fr/vostfree/extractors/VudeoExtractor.kt
+++ src/fr/vostfree/src/eu/kanade/tachiyomi/animeextension/fr/vostfree/extractors/VudeoExtractor.kt
@@ -3,17 +3,17 @@ package eu.kanade.tachiyomi.animeextension.fr.vostfree.extractors
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 
 class VudeoExtractor(private val client: OkHttpClient) {
-    fun videosFromUrl(url: String): List<Video> {
+    fun videosFromUrl(url: String, headers: Headers): List<Video> {
         val document = client.newCall(GET(url)).execute().asJsoup()
         val videoList = mutableListOf<Video>()
-        document.select("script").forEach { script ->
-            if (script.data().contains("sources: [\"")) {
-                val videoUrl = script.data().substringAfter("sources: [\"").substringBefore("\"],")
-                val quality = "Vudeo"
-                videoList.add(Video(videoUrl, quality, videoUrl))
+        document.select("script:containsData(sources: [)").forEach { script ->
+            val videoUrl = script.data().substringAfter("sources: [").substringBefore("]").replace("\"", "").split(",")
+            videoUrl.forEach {
+                videoList.add(Video(it, "Vudeo", it, headers))
             }
         }
         return videoList
