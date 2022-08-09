ce091e238ffbcb5c1907534e312e9294f94fb17a
Willy Nur Wahyudi <Willynurwahyudi@gmail.Com>
2022-08-09 20:37:13 +0700

    Update Kuronime.kt

--- src/id/kuronime/src/eu/kanade/tachiyomi/animeextension/id/kuronime/Kuronime.kt
+++ src/id/kuronime/src/eu/kanade/tachiyomi/animeextension/id/kuronime/Kuronime.kt
@@ -22,7 +22,7 @@ import java.lang.Exception
 import java.util.Locale
 
 class Kuronime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
-    override val baseUrl: String = "https://kuronime.org"
+    override val baseUrl: String = "https://45.12.2.2"
     override val lang: String = "id"
     override val name: String = "Kuronime"
     override val supportsLatest: Boolean = true
