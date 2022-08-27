86b2ed198c0391e4c82f6405e12da7d6149e8fcb
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2022-08-27 04:21:31 -0400

    Fixes [AnimeFenix] (#810)
    * Fixes [AnimeFenix]
    
    * add condition for blank urls [AnimeFenix]

--- src/es/animefenix/src/eu/kanade/tachiyomi/animeextension/es/animefenix/Animefenix.kt
+++ src/es/animefenix/src/eu/kanade/tachiyomi/animeextension/es/animefenix/Animefenix.kt
@@ -6,6 +6,7 @@ import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animeextension.es.animefenix.extractors.FembedExtractor
 import eu.kanade.tachiyomi.animeextension.es.animefenix.extractors.OkruExtractor
+import eu.kanade.tachiyomi.animeextension.es.animefenix.extractors.StreamTapeExtractor
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
@@ -18,12 +19,11 @@ import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
-import java.lang.Exception
+import java.net.URLDecoder
 
 class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
@@ -41,39 +41,35 @@ class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
 
-    override fun popularAnimeSelector(): String = "div.container div.container div.list-series article.serie-card"
+    override fun popularAnimeSelector(): String = "article.serie-card"
 
-    override fun popularAnimeRequest(page: Int): Request = GET("https://www.animefenix.com/animes?order=likes&page=$page")
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/animes?order=likes&page=$page")
 
     override fun popularAnimeFromElement(element: Element): SAnime {
-        val anime = SAnime.create()
-        anime.setUrlWithoutDomain(
-            element.select("figure.image a").attr("href")
-        )
-        anime.title = element.select("div.title h3 a").text()
-        anime.thumbnail_url = element.select("figure.image a img").attr("src")
-        anime.description = element.select("div.serie-card__information p").text()
+        val anime = SAnime.create().apply {
+            setUrlWithoutDomain(
+                element.select("figure.image a").attr("href")
+            )
+            title = element.select("div.title h3 a").text()
+            thumbnail_url = element.select("figure.image a img").attr("src")
+            description = element.select("div.serie-card__information p").text()
+        }
         return anime
     }
 
-    override fun popularAnimeNextPageSelector(): String = "div.pagination.is-centered ul.pagination-list li a.pagination-link"
+    override fun popularAnimeNextPageSelector(): String = "a.pagination-link"
 
     override fun episodeListParse(response: Response): List<SEpisode> {
-        val episodes = mutableListOf<SEpisode>()
-        val jsoup = response.asJsoup()
+        val document = response.asJsoup()
 
-        jsoup.select("ul.anime-page__episode-list.is-size-6 li").forEach { it ->
-
+        return document.select("ul.anime-page__episode-list.is-size-6 li").map { it ->
             val epNum = it.select("a span").text().replace("Episodio", "")
-            val episode = SEpisode.create().apply {
+            SEpisode.create().apply {
                 episode_number = epNum.toFloat()
                 name = "Episodio $epNum"
+                setUrlWithoutDomain(it.select("a").attr("href"))
             }
-            episode.setUrlWithoutDomain(it.select("a").attr("href"))
-            episodes.add(episode)
         }
-
-        return episodes
     }
 
     override fun episodeListSelector() = throw Exception("not used")
@@ -83,45 +79,45 @@ class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     override fun videoListParse(response: Response): List<Video> {
         val document = response.asJsoup()
         val videoList = mutableListOf<Video>()
-        document.select("ul.is-borderless.episode-page__servers-list li").forEach { it ->
-            val server = it.select("a").attr("title")
-            val serverId = it.select("a").attr("href").replace("#vid", "").toInt()
-            val serverCode = document.select("div.player-container script").toString()
-                .substringAfter("tabsArray['$serverId'] =")
-                .substringBefore("&amp;thumbnail")
-                .substringAfter("code=")
-                .substringBefore("&amp")
+        val servers = document.selectFirst("script:containsData(var tabsArray)").data()
+            .split("tabsArray").map { it.substringAfter("src='").substringBefore("'").replace("amp;", "") }
+            .filter { it.contains("https") }
 
-            if (server == "Fembed" || server == "fembed") {
-                val fembedUrl = "https://www.fembed.com/v/$serverCode"
-                val video = FembedExtractor().videosFromUrl(fembedUrl)
-                videoList.addAll(video)
-            }
-            if (server == "RU" || server == "ru") {
-                val okrUrl = "https://ok.ru/videoembed/$serverCode"
-                val video = OkruExtractor(client).videosFromUrl(okrUrl)
-                videoList.addAll(video)
-            }
-            if (server == "Amazon" || server == "AMAZON" || server == "amazon") {
-                val amazonUrl = "https://www.animefenix.com/stream/amz.php?v=$serverCode"
-                val video = amazonExtractor(amazonUrl)
-                videoList.add(Video(video, "Amazon", video))
-            }
-            if (server == "AmazonEs" || server == "AmazonES" || server == "amazones") {
-                val amazonUrl = "https://www.animefenix.com/stream/amz.php?v=$serverCode&ext=es"
-                val video = amazonExtractor(amazonUrl)
-                videoList.add(Video(video, "AmazonES", video))
+        servers.forEach { server ->
+            val decodedUrl = URLDecoder.decode(server, "UTF-8")
+            val realUrl = client.newCall(GET(decodedUrl)).execute().asJsoup().selectFirst("script").data()
+                .substringAfter("src=\"").substringBefore("\"")
+
+            when {
+                realUrl.contains("ok.ru") -> {
+                    val video = OkruExtractor(client).videosFromUrl(realUrl)
+                    videoList.addAll(video)
+                }
+                realUrl.contains("fembed") -> {
+                    val video = FembedExtractor().videosFromUrl(realUrl)
+                    videoList.addAll(video)
+                }
+                realUrl.contains("/stream/amz.php?") -> {
+                    val video = amazonExtractor(baseUrl + realUrl.substringAfter(".."))
+                    if (video.isNotBlank()) {
+                        if (realUrl.contains("&ext=es")) {
+
+                            videoList.add(Video(video, "Amazon ES", video))
+                        } else {
+                            videoList.add(Video(video, "Amazon", video))
+                        }
+                    }
+                }
+                realUrl.contains("streamtape") -> {
+                    val video = StreamTapeExtractor(client).videoFromUrl(realUrl, "Streamtape")
+                    if (video != null) {
+                        videoList.add(video)
+                    }
+                }
             }
         }
-        return videoList
-    }
 
-    private fun amazonExtractor(url: String): String {
-        val jsoup = Jsoup.connect(url).get()
-        val videoUrl = jsoup.select("body script").toString()
-            .substringAfter("[{\"file\":\"")
-            .substringBefore("\",").replace("\\", "")
-        return videoUrl
+        return videoList
     }
 
     override fun videoListSelector() = throw Exception("not used")
@@ -155,22 +151,21 @@ class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         return when {
             query.isNotBlank() -> GET("$baseUrl/animes?q=$query&page=$page", headers)
             genreFilter.state != 0 -> GET("$baseUrl/animes?genero[]=${genreFilter.toUriPart()}&order=default&page=$page")
-            else -> GET("https://www.animefenix.com/animes?order=likes&page=$page ")
+            else -> GET("$baseUrl/animes?order=likes&page=$page ")
         }
     }
-    override fun searchAnimeFromElement(element: Element): SAnime {
-        return popularAnimeFromElement(element)
-    }
+    override fun searchAnimeFromElement(element: Element): SAnime = popularAnimeFromElement(element)
 
     override fun searchAnimeNextPageSelector(): String = popularAnimeNextPageSelector()
 
     override fun searchAnimeSelector(): String = popularAnimeSelector()
 
     override fun animeDetailsParse(document: Document): SAnime {
-        val anime = SAnime.create()
-        anime.title = document.select("div.column.is-12-mobile.is-8-tablet.is-10-desktop h1.title.has-text-orange").text()
-        anime.genre = document.select("p.genres.buttons a.button.is-small.is-orange.is-outlined.is-roundedX").joinToString { it.text() }
-        anime.status = parseStatus(document.select(" div.column.is-12-mobile.xis-3-tablet.xis-3-desktop.xhas-background-danger.is-narrow-tablet.is-narrow-desktop a").text())
+        val anime = SAnime.create().apply {
+            title = document.select("h1.title.has-text-orange").text()
+            genre = document.select("a.button.is-small.is-orange.is-outlined.is-roundedX").joinToString { it.text() }
+            status = parseStatus(document.select("div.column.is-12-mobile.xis-3-tablet.xis-3-desktop.xhas-background-danger.is-narrow-tablet.is-narrow-desktop a").text())
+        }
         return anime
     }
 
@@ -186,9 +181,16 @@ class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override fun latestUpdatesFromElement(element: Element) = popularAnimeFromElement(element)
 
-    override fun latestUpdatesRequest(page: Int) = GET("https://www.animefenix.com/animes?order=added&page=$page")
+    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/animes?order=added&page=$page")
 
     override fun latestUpdatesSelector() = popularAnimeSelector()
+
+    private fun amazonExtractor(url: String): String {
+        val document = client.newCall(GET(url)).execute().asJsoup()
+        return document.selectFirst("script:containsData(sources: [)").data()
+            .substringAfter("[{\"file\":\"")
+            .substringBefore("\",").replace("\\", "")
+    }
 
     override fun getFilterList(): AnimeFilterList = AnimeFilterList(
         AnimeFilter.Header("La busqueda por texto ignora el filtro"),
@@ -254,6 +256,7 @@ class Animefenix : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
+
         val videoQualityPref = ListPreference(screen.context).apply {
             key = "preferred_quality"
             title = "Preferred quality"
