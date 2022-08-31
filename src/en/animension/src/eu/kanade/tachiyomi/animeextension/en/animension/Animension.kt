6393ae9b4cecc85b670b17771b228ab84d50a0d5
notLaww <rorensuuu@gmail.com>
2022-08-31 15:50:01 +0800

    add new source animension (#822)
    Co-authored-by: Lawwwwwwww <97511605+Lawwwwwwww@users.noreply.github.com>

---
+++ src/en/animension/src/eu/kanade/tachiyomi/animeextension/en/animension/Animension.kt
@@ -0,0 +1,243 @@
+package eu.kanade.tachiyomi.animeextension.en.animension
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animeextension.en.animension.extractors.DoodExtractor
+import eu.kanade.tachiyomi.animeextension.en.animension.extractors.FembedExtractor
+import eu.kanade.tachiyomi.animeextension.en.animension.extractors.StreamSBExtractor
+import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.float
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Request
+import okhttp3.Response
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+class Animension() : ConfigurableAnimeSource, AnimeHttpSource() {
+    override val lang = "en"
+
+    override val name = "Animension"
+
+    override val baseUrl = "https://animension.to/"
+
+    override val supportsLatest = false
+
+    private val apiUrl = "https://animension.to/public-api"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
+    // Popular
+    override fun popularAnimeRequest(page: Int): Request =
+        GET("$apiUrl/search.php?dub=0&sort=popular-week&page=$page")
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        val responseJson = json.decodeFromString<JsonArray>(response.body!!.string())
+        val animes = responseJson.map { anime ->
+            val data = anime.jsonArray
+
+            SAnime.create().apply {
+                title = data[0].jsonPrimitive.content
+                url = data[1].jsonPrimitive.content
+                thumbnail_url = data[2].jsonPrimitive.content
+            }
+        }
+        val hasNextPage = responseJson.size >= 25
+
+        return AnimesPage(animes, hasNextPage)
+    }
+
+    // Episode
+    override fun episodeListRequest(anime: SAnime): Request {
+        return (GET("$apiUrl/episodes.php?id=${anime.url}", headers))
+    }
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val responseJson = json.decodeFromString<JsonArray>(response.body!!.string())
+        val episodes = responseJson.map { episode ->
+            val data = episode.jsonArray
+
+            SEpisode.create().apply {
+                name = "Episode ${data[2]}"
+                url = data[1].jsonPrimitive.content
+                episode_number = data[2].jsonPrimitive.float
+            }
+        }
+        return episodes
+    }
+
+    // Vide urls
+    override fun videoListRequest(episode: SEpisode): Request =
+        GET("$apiUrl/episode.php?id=${episode.url}", headers)
+
+    override fun videoListParse(response: Response): List<Video> {
+        val responseJson = json.decodeFromString<JsonArray>(response.body!!.string())
+        val videos = json.decodeFromString<JsonObject>(responseJson[3].jsonPrimitive.content)
+        val videoList = mutableListOf<Video>()
+
+        for (key in videos.keys.toList()) {
+            val url = videos[key]!!.jsonPrimitive.content
+            when {
+                url.contains("sbembed.com") || url.contains("sbembed1.com") || url.contains("sbplay.org") ||
+                    url.contains("sbvideo.net") || url.contains("streamsb.net") || url.contains("sbplay.one") ||
+                    url.contains("cloudemb.com") || url.contains("playersb.com") || url.contains("tubesb.com") ||
+                    url.contains("sbplay1.com") || url.contains("embedsb.com") || url.contains("watchsb.com") ||
+                    url.contains("sbplay2.com") || url.contains("japopav.tv") || url.contains("viewsb.com") ||
+                    url.contains("sbfast") || url.contains("sbfull.com") || url.contains("javplaya.com") ||
+                    url.contains("ssbstream.net") || url.contains("p1ayerjavseen.com") || url.contains("sbthe.com") ||
+                    url.contains("streamsss.net") // || url.contains("") // || url.contains("")
+                -> {
+                    val headers = headers.newBuilder()
+                        .set("Referer", url)
+                        .set(
+                            "User-Agent",
+                            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0"
+                        )
+                        .set("Accept-Language", "en-US,en;q=0.5")
+                        .set("watchsb", "streamsb")
+                        .build()
+                    videoList.addAll(StreamSBExtractor(client).videosFromUrl(url, headers))
+                }
+
+                url.contains("dood") -> {
+                    val video = DoodExtractor(client).videoFromUrl(url)
+                    if (video != null) {
+                        videoList.add(video)
+                    }
+                }
+
+                url.contains("fembed.com") || url.contains("anime789.com") || url.contains("24hd.club") ||
+                    url.contains("fembad.org") || url.contains("vcdn.io") || url.contains("sharinglink.club") ||
+                    url.contains("moviemaniac.org") || url.contains("votrefiles.club") || url.contains("femoload.xyz") ||
+                    url.contains("albavido.xyz") || url.contains("feurl.com") || url.contains("dailyplanet.pw") ||
+                    url.contains("ncdnstm.com") || url.contains("jplayer.net") || url.contains("xstreamcdn.com") ||
+                    url.contains("fembed-hd.com") || url.contains("gcloud.live") || url.contains("vcdnplay.com") ||
+                    url.contains("superplayxyz.club") || url.contains("vidohd.com") || url.contains("vidsource.me") ||
+                    url.contains("cinegrabber.com") || url.contains("votrefile.xyz") || url.contains("zidiplay.com") ||
+                    url.contains("ndrama.xyz") || url.contains("fcdn.stream") || url.contains("mediashore.org") ||
+                    url.contains("suzihaza.com") || url.contains("there.to") || url.contains("femax20.com") ||
+                    url.contains("javstream.top") || url.contains("viplayer.cc") || url.contains("sexhd.co") ||
+                    url.contains("fembed.net") || url.contains("mrdhan.com") || url.contains("votrefilms.xyz") ||
+                    url.contains("fembed9hd.com") || url.contains("embedsito.com") || url.contains("dutrag.com") ||
+                    url.contains("youvideos.ru") || url.contains("streamm4u.club") || // url.contains("") ||
+                    url.contains("moviepl.xyz") || url.contains("asianclub.tv") || // url.contains("") ||
+                    url.contains("vidcloud.fun") || url.contains("fplayer.info") || // url.contains("") ||
+                    url.contains("diasfem.com") || url.contains("javpoll.com") // url.contains("")
+                -> {
+                    FembedExtractor().videosFromUrl(url)?.let { videoList.addAll(it) }
+                }
+            }
+        }
+        return videoList
+    }
+
+    override fun List<Video>.sort(): List<Video> {
+        val quality = preferences.getString("preferred_quality", null)
+        if (quality != null) {
+            val newList = mutableListOf<Video>()
+            var preferred = 0
+            for (video in this) {
+                if (video.quality.contains(quality)) {
+                    newList.add(preferred, video)
+                    preferred++
+                } else {
+                    newList.add(video)
+                }
+            }
+            return newList
+        }
+        return this
+    }
+
+    override fun videoUrlParse(response: Response) = throw Exception("not used")
+
+    // Anime details
+    override fun animeDetailsParse(response: Response): SAnime {
+        val document = response.asJsoup()
+        val anime = SAnime.create()
+        anime.thumbnail_url = document.select("div.thumb img").attr("src")
+        anime.title = document.select("h1.entry-title").text()
+        anime.description = document.select("div.desc").text()
+        anime.genre = document.select("div.genxed span a").joinToString { it.text() }
+        anime.status = parseStatus(document.select("div.spe span:contains(Status)").text().substringAfter("Status: "))
+        return anime
+    }
+
+    // Latest
+    override fun latestUpdatesParse(response: Response): AnimesPage {
+        throw Exception("Not used")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        throw Exception("Not used")
+    }
+
+    // Search
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        val responseJson = json.decodeFromString<JsonArray>(response.body!!.string())
+        val animes = responseJson.map { anime ->
+            val data = anime.jsonArray
+
+            SAnime.create().apply {
+                title = data[0].jsonPrimitive.content
+                url = data[1].jsonPrimitive.content
+                thumbnail_url = data[2].jsonPrimitive.content
+            }
+        }
+        val hasNextPage = responseJson.size >= 25
+
+        return AnimesPage(animes, hasNextPage)
+    }
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request =
+        GET("$apiUrl/search.php?search_text=$query&page=$page", headers)
+
+    // Utilities
+    private fun parseStatus(statusString: String): Int {
+        return when (statusString) {
+            "Ongoing" -> SAnime.ONGOING
+            "Completed" -> SAnime.COMPLETED
+            else -> SAnime.UNKNOWN
+        }
+    }
+
+    // Preferences
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val videoQualityPref = ListPreference(screen.context).apply {
+            key = "preferred_quality"
+            title = "Preferred quality"
+            entries = arrayOf("1080p", "720p", "480p", "360p", "Doodstream")
+            entryValues = arrayOf("1080", "720", "480", "360", "Doodstream")
+            setDefaultValue("1080")
+            summary = "%s"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val selected = newValue as String
+                val index = findIndexOfValue(selected)
+                val entry = entryValues[index] as String
+                preferences.edit().putString(key, entry).commit()
+            }
+        }
+        screen.addPreference(videoQualityPref)
+    }
+}
