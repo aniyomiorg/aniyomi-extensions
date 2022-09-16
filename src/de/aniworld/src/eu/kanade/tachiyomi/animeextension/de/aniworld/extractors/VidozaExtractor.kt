dceb71c7c8bd59ffee87d7f0569868ca5cd2b4fd
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2022-09-16 09:09:39 +0200

    AniWorld: add vidoza extractor & probably improvments (#868)
    * AniWorld: add vidoza extractor & probably improvments
    
    * add experimental to name
    
    * Update build.gradle

---
+++ src/de/aniworld/src/eu/kanade/tachiyomi/animeextension/de/aniworld/extractors/VidozaExtractor.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.animeextension.de.aniworld.extractors
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+
+class VidozaExtractor(private val client: OkHttpClient) {
+
+    fun videoFromUrl(url: String, quality: String): Video? {
+        val document = client.newCall(GET(url)).execute().asJsoup()
+        val script = document.select("script:containsData(window.pData = {)")
+            .firstOrNull()?.data()?.substringAfter("sourcesCode: [{ src: \"") ?: return null
+        val videoUrl = script.substringAfter("sourcesCode: [{ src: \"").substringBefore("\", type:")
+        return Video(url, quality, videoUrl)
+    }
+}
