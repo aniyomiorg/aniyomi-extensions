774dc97f0ee12b15aeb0c31f9c4dc28b19528f26
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-09-19 06:25:14 -0300

    New source: AnimesOnlinex (#873)
    * AnimesOnlineX: Create extension base
    
    It needs more extractors, i'll make them soon.
    
    * AnimesOnlineX: add some extractors
    
    * AnimesOnlineX: Fix problems at animeDetails / episodeList*
    
    * AnimesOnlineX: Fix anicdn video extraction
    
    * AnimesOnlineX: get all urls/qualities from .m3u8's
    
    * AnimesOnlineX: Add more qualities and normalize them
    
    * AnimesOnlineX: Fix anime description (again)

---
+++ src/pt/animesonlinex/src/eu/kanade/tachiyomi/animeextension/pt/animesonlinex/AOXConstants.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.animeextension.pt.animesonlinex
+
+object AOXConstants {
+    const val ACCEPT_LANGUAGE = "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7"
+    const val USER_AGENT = "Mozilla/5.0 (Android 11; Mobile; rv:105.0) Gecko/105.0 Firefox/105.0"
+    const val PREFIX_SEARCH = "slug:"
+    const val PREFERRED_QUALITY = "preferred_quality"
+    const val DEFAULT_QUALITY = "720p"
+    val QUALITY_LIST = arrayOf("480p", "720p", "1080p")
+}
