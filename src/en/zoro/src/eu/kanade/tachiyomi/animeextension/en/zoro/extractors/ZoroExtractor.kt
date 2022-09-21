e69af2a4ac76e9e3e26eb097c4f580a688942de9
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-09-21 13:06:37 -0300

    Prevent caching an important file
    The e6.js content change everytime, so it should not be cached, or else the decryption algorithm will fail miserably.

--- src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/extractors/ZoroExtractor.kt
+++ src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/extractors/ZoroExtractor.kt
@@ -2,9 +2,16 @@ package eu.kanade.tachiyomi.animeextension.en.zoro.extractors
 
 import eu.kanade.tachiyomi.animeextension.en.zoro.utils.Decryptor
 import eu.kanade.tachiyomi.network.GET
+import okhttp3.CacheControl
 import okhttp3.OkHttpClient
 
 class ZoroExtractor(private val client: OkHttpClient) {
+
+    // Prevent caching the .JS file
+    private val cacheControl = CacheControl.Builder().noStore().build()
+    private val newClient = client.newBuilder()
+        .cache(null)
+        .build()
 
     companion object {
         private const val SERVER_URL = "https://rapid-cloud.co"
@@ -13,11 +20,15 @@ class ZoroExtractor(private val client: OkHttpClient) {
     }
 
     fun getSourcesJson(url: String): String? {
-        val js = client.newCall(GET(JS_URL)).execute().body!!.string()
+        val js = newClient.newCall(GET(JS_URL, cache = cacheControl)).execute()
+            .body!!.string()
         val id = url.substringAfter("/embed-6/", "")
             .substringBefore("?", "").ifEmpty { return null }
-        val srcRes = client.newCall(GET(SOURCES_URL + id)).execute().body!!.string()
+        val srcRes = newClient.newCall(GET(SOURCES_URL + id, cache = cacheControl))
+            .execute()
+            .body!!.string()
         if ("\"encrypted\":false" in srcRes) return srcRes
+        if (!srcRes.contains("{\"sources\":")) return null
         val encrypted = srcRes.substringAfter("sources\":\"").substringBefore("\"")
         val decrypted = Decryptor.decrypt(encrypted, js) ?: return null
         val end = srcRes.replace("\"$encrypted\"", decrypted)
