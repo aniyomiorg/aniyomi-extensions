d7530fb2a2d754e0635a23df1154f729385ea987
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-10-21 17:17:00 -0300

    Anitube: fix extractor

--- src/pt/anitube/src/eu/kanade/tachiyomi/animeextension/pt/anitube/extractors/AnitubeExtractor.kt
+++ src/pt/anitube/src/eu/kanade/tachiyomi/animeextension/pt/anitube/extractors/AnitubeExtractor.kt
@@ -4,17 +4,16 @@ import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
 import okhttp3.Response
-import org.jsoup.nodes.Document
 
 object AnitubeExtractor {
 
     private val headers = Headers.headersOf("User-Agent", "VLC/3.0.16 LibVLC/3.0.16")
 
     fun getVideoList(response: Response): List<Video> {
-        val doc: Document = response.asJsoup()
-        val hasFHD: Boolean = doc.selectFirst("div.abaItem:contains(FULLHD)") != null
-        val serverUrl: String = doc.selectFirst("meta[itemprop=contentURL]").attr("content")
-        val type: String = serverUrl.substringAfter("cz/").substringBefore("/")
+        val doc = response.asJsoup()
+        val hasFHD = doc.selectFirst("div.abaItem:contains(FULLHD)") != null
+        val serverUrl = doc.selectFirst("meta[itemprop=contentURL]").attr("content")
+        val type = serverUrl.split("/").get(3)
         val qualities = listOfNotNull("SD", "HD", if (hasFHD) "FULLHD" else null)
         val paths = when (type) {
             "appsd" -> mutableListOf("mobilesd", "mobilehd")
