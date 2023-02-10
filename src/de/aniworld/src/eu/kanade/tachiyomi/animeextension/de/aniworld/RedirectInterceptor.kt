be05549ebc9474b8a3102e194f90ca6748a5d29d
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-02-10 14:06:59 +0100

    AniWorld: add reCapBypass v0.1
    First version of my recaptcha bypasser. Total early stage version.

--- src/de/aniworld/src/eu/kanade/tachiyomi/animeextension/de/aniworld/RedirectInterceptor.kt
+++ src/de/aniworld/src/eu/kanade/tachiyomi/animeextension/de/aniworld/RedirectInterceptor.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
@@ -26,7 +27,7 @@ class RedirectInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
 
-        val newRequest = resolveWithWebView(originalRequest) ?: throw Exception("Bitte Captcha in WebView lösen")
+        val newRequest = resolveWithWebView(originalRequest) ?: throw Exception("Versuche es später nochmal")
 
         return chain.proceed(newRequest)
     }
@@ -40,6 +41,7 @@ class RedirectInterceptor : Interceptor {
         var webView: WebView? = null
 
         val origRequestUrl = request.url.toString()
+
         val headers = request.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }.toMutableMap()
 
         var newRequest: Request? = null
@@ -53,9 +55,7 @@ class RedirectInterceptor : Interceptor {
                 databaseEnabled = true
                 useWideViewPort = false
                 loadWithOverviewMode = false
-                userAgentString = // request.header("User-Agent")
-                    // ?: "\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63\""
-                    "Mozilla/5.0 (Linux; Android 12; Pixel 5 Build/SP2A.220405.004; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.127 Safari/537.36"
+                userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0"
             }
 
             webview.webViewClient = object : WebViewClient() {
@@ -63,22 +63,14 @@ class RedirectInterceptor : Interceptor {
                     view: WebView,
                     request: WebResourceRequest,
                 ): WebResourceResponse? {
-                    if (request.url.toString().contains("token") || request.url.toString().contains("https://dood") || request.url.toString().contains("https://streamtape") ||
-                        request.url.toString().contains("https://voe") || request.url.toString().contains("https://realfinanceblogcenter") ||
-                        request.url.toString().contains("https://launchreliantcleaverriver") ||
-                        request.url.toString().contains("https://fraudclatterflyingcar") ||
-                        request.url.toString().contains("https://uptodatefinishconferenceroom")
-                    ) {
-                        if (!request.url.toString().contains("ad.js")) {
-                            newRequest = GET(request.url.toString(), request.requestHeaders.toHeaders())
-                            latch.countDown()
-                        }
+                    Log.i("shouldInterceptRequest", request.url.toString())
+                    if (request.url.toString().contains("payload")) {
+                        newRequest = GET(request.url.toString(), request.requestHeaders.toHeaders())
+                        latch.countDown()
                     }
-                    if (request.url.toString().contains("token") || request.url.toString().contains("https://vidoza.net/embed-")) {
-                        if (request.url.toString().contains(".html")) {
-                            newRequest = GET(request.url.toString(), request.requestHeaders.toHeaders())
-                            latch.countDown()
-                        }
+                    if (request.url.toString().contains("https://aniworld.to/redirect/") && request.url.toString().contains("token")) {
+                        newRequest = GET(request.url.toString(), request.requestHeaders.toHeaders())
+                        latch.countDown()
                     }
                     return super.shouldInterceptRequest(view, request)
                 }
