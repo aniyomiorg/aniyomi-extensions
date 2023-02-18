692f4a214599ee9881e50ed174af1b1a28300048
enimax-anime <107899019+enimax-anime@users.noreply.github.com>
2023-02-17 16:46:52 -0800

    Update SFlixExtractor.kt

--- src/en/sflix/src/eu/kanade/tachiyomi/animeextension/en/sflix/extractors/SFlixExtractor.kt
+++ src/en/sflix/src/eu/kanade/tachiyomi/animeextension/en/sflix/extractors/SFlixExtractor.kt
@@ -48,7 +48,7 @@ class SFlixExtractor(private val client: OkHttpClient) {
             .execute()
             .body!!.string()
 
-        val key = newClient.newCall(GET("https://raw.githubusercontent.com/consumet/rapidclown/rabbitstream/key.txt"))
+        val key = newClient.newCall(GET("https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt"))
             .execute()
             .body!!.string()
 
