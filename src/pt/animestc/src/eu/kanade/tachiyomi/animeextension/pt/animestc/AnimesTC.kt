467ce4119c5601a8b88d34fa05d56069fb5ca94a
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-02-22 19:11:50 -0300

    feat: Create AnimesTC base

---
+++ src/pt/animestc/src/eu/kanade/tachiyomi/animeextension/pt/animestc/AnimesTC.kt
@@ -0,0 +1,98 @@
+package eu.kanade.tachiyomi.animeextension.pt.animestc
+
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import kotlinx.serialization.json.Json
+import okhttp3.Request
+import okhttp3.Response
+import rx.Observable
+
+class AnimesTC : AnimeHttpSource() {
+
+    override val name = "AnimesTC"
+
+    override val baseUrl = "https://wwww.animestc.net"
+
+    override val lang = "pt-BR"
+
+    override val supportsLatest = false
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    // ============================== Popular ===============================
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularAnimeRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    // ============================== Episodes ==============================
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        TODO("Not yet implemented")
+    }
+
+    // ============================ Video Links =============================
+    override fun videoListRequest(episode: SEpisode): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun videoListParse(response: Response): List<Video> {
+        TODO("Not yet implemented")
+    }
+
+    // =========================== Anime Details ============================
+    override fun animeDetailsParse(response: Response): SAnime {
+        TODO("Not yet implemented")
+    }
+
+    // =============================== Search ===============================
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun fetchSearchAnime(page: Int, query: String, filters: AnimeFilterList): Observable<AnimesPage> {
+        return if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
+            val id = query.removePrefix(PREFIX_SEARCH)
+            client.newCall(GET("$baseUrl/animes/$id"))
+                .asObservableSuccess()
+                .map { response ->
+                    searchAnimeByIdParse(response, id)
+                }
+        } else {
+            super.fetchSearchAnime(page, query, filters)
+        }
+    }
+
+    private fun searchAnimeByIdParse(response: Response, id: String): AnimesPage {
+        val details = animeDetailsParse(response)
+        details.url = "/animes/$id"
+        return AnimesPage(listOf(details), false)
+    }
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val PREFIX_SEARCH = "id:"
+    }
+}
