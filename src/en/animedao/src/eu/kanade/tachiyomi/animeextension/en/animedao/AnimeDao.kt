17eb927cf08030f63120b7162a87bed86f0647c9
Abhishek890 <46078948+Abhishek890@users.noreply.github.com>
2023-02-22 18:41:59 +0530

    update

--- src/en/animedao/src/eu/kanade/tachiyomi/animeextension/en/animedao/AnimeDao.kt
+++ src/en/animedao/src/eu/kanade/tachiyomi/animeextension/en/animedao/AnimeDao.kt
@@ -374,9 +374,9 @@ class AnimeDao : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         val videoServerPref = ListPreference(screen.context).apply {
             key = "preferred_server"
             title = "Preferred server"
-            entries = arrayOf("vstream", "src2", "src", "mixdrop", "vcdn", "streamsb", "streamtape", "vplayer", "doodstream")
-            entryValues = arrayOf("vstream", "src2", "src", "mixdrop", "vcdn", "streamsb", "streamtape", "vplayer", "doodstream")
-            setDefaultValue("vstream")
+            entries = arrayOf("Vidstreaming", "Doodstream", "Mixdrop", "StreamSB", "streamtape", "Fembed")
+            entryValues = arrayOf("Vidstreaming", "Doodstream", "Mixdrop", "StreamSB", "streamtape", "Fembed")
+            setDefaultValue("Vidstreaming")
             summary = "%s"
 
             setOnPreferenceChangeListener { _, newValue ->
