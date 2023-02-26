136eca47c4a77422c2aac33e543947b41213dd9f
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-02-26 05:58:04 -0300

    Convert YourUploadExtractor to lib (#1333)

---
+++ lib/yourupload-extractor/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+    namespace = "eu.kanade.tachiyomi.lib.youruploadextractor"
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+        targetSdk = AndroidConfig.targetSdk
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(libs.kotlin.stdlib)
+    compileOnly(libs.okhttp)
+    compileOnly(libs.aniyomi.lib)
+    compileOnly(libs.jsoup)
+}
+// BUMPS: 0
