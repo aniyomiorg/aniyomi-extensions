ee3a7f7bd94fdba5a3faae91fd1077b7dbfb998d
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-03-13 18:47:37 -0300

    refactor: Remove/replace some unnecessary code

--- lib/streamsb-extractor/src/main/java/eu/kanade/tachiyomi/lib/streamsbextractor/StreamSBExtractor.kt
+++ lib/streamsb-extractor/src/main/java/eu/kanade/tachiyomi/lib/streamsbextractor/StreamSBExtractor.kt
@@ -25,32 +25,34 @@ class StreamSBExtractor(private val client: OkHttpClient) {
 
     // animension, asianload and dramacool uses "common = false"
     private fun fixUrl(url: String, common: Boolean): String {
-        val sbUrl = "https://${url.toHttpUrl().host}"
-        val id = url.substringAfter("${url.toHttpUrl().host}")
+        val host = url.toHttpUrl().host
+        val sbUrl = "https://$host/sources15"
+        val id = url.substringAfter(host)
             .substringAfter("/e/")
             .substringAfter("/embed-")
             .substringBefore("?")
             .substringBefore(".html")
             .substringAfter("/")
-        return if (common) {
+        return sbUrl + if (common) {
             val hexBytes = bytesToHex(id.toByteArray())
-            "$sbUrl/sources15/625a364258615242766475327c7c${hexBytes}7c7c4761574550654f7461566d347c7c73747265616d7362"
+            "/625a364258615242766475327c7c${hexBytes}7c7c4761574550654f7461566d347c7c73747265616d7362"
         } else {
-            "$sbUrl/sources15/${bytesToHex("||$id||||streamsb".toByteArray())}/"
+            "/${bytesToHex("||$id||||streamsb".toByteArray())}/"
         }
     }
 
-    fun videosFromUrl(url: String, headers: Headers, prefix: String = "", suffix: String = "", common: Boolean = true): List<Video> {
-        val newHeaders = headers.newBuilder()
+    fun videosFromUrl(url: String, headers: Headers, prefix: String = "", suffix: String = "", common: Boolean = true, manualData: Boolean = false): List<Video> {
+        val newHeaders = if(manualData) headers else headers.newBuilder()
             .set("referer", url)
             .set("watchsb", "sbstream")
             .set("authority", "embedsb.com")
             .build()
         return try {
-            val master = fixUrl(url, common)
+            val master = if(manualData) url else fixUrl(url, common)
             val json = Json { ignoreUnknownKeys = true }.decodeFromString<Response>(
                 client.newCall(GET(master, newHeaders))
-                    .execute().body.string()
+                    .execute()
+                    .use { it.body.string() }
             )
             val masterUrl = json.stream_data.file.trim('"')
             val subtitleList = json.stream_data.subs?.let {
@@ -59,18 +61,16 @@ class StreamSBExtractor(private val client: OkHttpClient) {
 
             val masterPlaylist = client.newCall(GET(masterUrl, newHeaders))
                 .execute()
-                .body.string()
+                .use { it.body.string() }
 
-            val audioList = mutableListOf<Track>()
             val audioRegex = Regex("""#EXT-X-MEDIA:TYPE=AUDIO.*?NAME="(.*?)".*?URI="(.*?)"""")
-            audioList.addAll(
-                audioRegex.findAll(masterPlaylist).map {
+            val audioList: List<Track> = audioRegex.findAll(masterPlaylist)
+                .map {
                     Track(
-                        it.groupValues[2],
-                        it.groupValues[1]
+                        it.groupValues[2], // Url
+                        it.groupValues[1] // Name
                     )
-                }
-            )
+                }.toList()
 
             val separator = "#EXT-X-STREAM-INF"
             masterPlaylist.substringAfter(separator).split(separator).map {
@@ -98,48 +98,6 @@ class StreamSBExtractor(private val client: OkHttpClient) {
     }
 
     fun videosFromDecryptedUrl(realUrl: String, headers: Headers, prefix: String = "", suffix: String = ""): List<Video> {
-        return try {
-            val json = Json { ignoreUnknownKeys = true }.decodeFromString<Response>(client.newCall(GET(realUrl, headers)).execute().body.string())
-            val masterUrl = json.stream_data.file.trim('"')
-            val subtitleList = json.stream_data.subs?.let {
-                it.map { s -> Track(s.file, s.label) }
-            } ?: emptyList()
-
-            val masterPlaylist = client.newCall(GET(masterUrl, headers)).execute().body.string()
-
-            val audioList = mutableListOf<Track>()
-            val audioRegex = Regex("""#EXT-X-MEDIA:TYPE=AUDIO.*?NAME="(.*?)".*?URI="(.*?)"""")
-            audioList.addAll(
-                audioRegex.findAll(masterPlaylist).map {
-                    Track(
-                        it.groupValues[2],
-                        it.groupValues[1]
-                    )
-                }
-            )
-
-            val separator = "#EXT-X-STREAM-INF"
-            masterPlaylist.substringAfter(separator).split(separator).map {
-                val resolution = it.substringAfter("RESOLUTION=")
-                    .substringBefore("\n")
-                    .substringAfter("x")
-                    .substringBefore(",") + "p"
-                val quality = ("StreamSB:$resolution").let {
-                    if(prefix.isNotBlank()) "$prefix $it"
-                    else it
-                }.let {
-                    if(suffix.isNotBlank()) "$it $suffix"
-                    else it
-                }
-                val videoUrl = it.substringAfter("\n").substringBefore("\n")
-                if (audioList.isEmpty()) {
-                    Video(videoUrl, quality, videoUrl, headers = headers, subtitleTracks = subtitleList)
-                } else {
-                    Video(videoUrl, quality, videoUrl, headers = headers, subtitleTracks = subtitleList, audioTracks = audioList)
-                }
-            }
-        } catch (e: Exception) {
-            emptyList()
-        }
+        return videosFromUrl(realUrl, headers, prefix, suffix, manualData = true)
     }
 }
