7a5fc9fa2b8b4502a6a884be43db637c80022d76
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-06 11:03:32 -0300

    feat: Convert VidbomExtractor to shared lib (#1471)

---
+++ lib/vidbom-extractor/src/main/java/eu/kanade/tachiyomi/lib/vidbomextractor/VidBomExtractor.kt
@@ -0,0 +1,23 @@
+package eu.kanade.tachiyomi.lib.vidbomextractor
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+
+class VidBomExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String): List<Video> {
+        val doc = client.newCall(GET(url)).execute().asJsoup()
+        val script = doc.selectFirst("script:containsData(sources)")!!
+        val data = script.data().substringAfter("sources: [").substringBefore("],")
+
+        return data.split("file:\"").drop(1).map { source ->
+            val src = source.substringBefore("\"")
+            var quality = "Vidbom: " + source.substringAfter("label:\"").substringBefore("\"")
+            if (quality.length > 15) {
+                quality = "Vidshare: 480p"
+            }
+            Video(src, quality, src)
+        }
+    }
+}
