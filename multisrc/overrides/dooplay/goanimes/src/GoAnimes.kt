ad013676d262714d6ed215a0d40b6b042089f5e8
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-09 16:04:46 -0300

    fix: Fix episodes list

--- multisrc/overrides/dooplay/goanimes/src/GoAnimes.kt
+++ multisrc/overrides/dooplay/goanimes/src/GoAnimes.kt
@@ -1,6 +1,10 @@
 package eu.kanade.tachiyomi.animeextension.pt.goanimes
 
+import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import org.jsoup.nodes.Element
 
 class GoAnimes : DooPlay(
     "pt-BR",
@@ -9,6 +13,23 @@ class GoAnimes : DooPlay(
 ) {
     // ============================== Popular ===============================
     override fun popularAnimeSelector() = "div#featured-titles article.item.tvshows > div.poster"
+
+    // ============================== Episodes ==============================
+    override val seasonListSelector = "div#seasons > *"
+
+    override fun getSeasonEpisodes(season: Element): List<SEpisode> {
+        // All episodes are listed under a single page
+        season.selectFirst(episodeListSelector())?.let {
+            return super.getSeasonEpisodes(season)
+        }
+
+        // Episodes are listed at another page
+        val url = season.attr("href")
+        return client.newCall(GET(url))
+            .execute()
+            .asJsoup()
+            .let { super.getSeasonEpisodes(it) }
+    }
 
     // =============================== Latest ===============================
     override val latestUpdatesPath = "lancamentos"
