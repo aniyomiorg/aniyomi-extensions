720f25c6e744a9646bb1aab6387775e2adb94037
Folke <folke.steen85@gmail.com>
2023-04-21 18:50:11 +0200

    New extension

---
+++ src/all/googledrive/src/eu/kanade/tachiyomi/animeextension/all/googledrive/DataModel.kt
@@ -0,0 +1,40 @@
+package eu.kanade.tachiyomi.animeextension.all.googledrive
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostResponse(
+    val nextPageToken: String? = null,
+    val items: List<ResponseItem>? = null,
+) {
+    @Serializable
+    data class ResponseItem(
+        val id: String,
+        val title: String,
+        val mimeType: String,
+        val fileSize: String? = null,
+    )
+}
+
+@Serializable
+data class Details(
+    val title: String? = null,
+    val author: String? = null,
+    val artist: String? = null,
+    val description: String? = null,
+    val genre: List<String>? = null,
+    val status: Int? = null,
+)
+
+@Serializable
+data class LinkData(
+    val url: String,
+    val type: String,
+    val info: LinkDataInfo? = null,
+)
+
+@Serializable
+data class LinkDataInfo(
+    val title: String,
+    val size: String,
+)
