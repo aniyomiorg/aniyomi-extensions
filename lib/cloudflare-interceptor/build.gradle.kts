c5999483bf43ff733170c1d5aa6ac49c6e550ffe
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-25 04:00:02 -0300

    feat(lib): Convert CloudflareInterceptor into a shared lib (#1537)

---
+++ lib/cloudflare-interceptor/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+    namespace = "eu.kanade.tachiyomi.lib.cloudflareinterceptor"
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+        targetSdk = AndroidConfig.targetSdk
+    }
+}
+
+dependencies {
+    compileOnly(libs.kotlin.stdlib)
+    compileOnly(libs.okhttp)
+    compileOnly(libs.aniyomi.lib)
+    compileOnly(libs.injekt.core)
+}
