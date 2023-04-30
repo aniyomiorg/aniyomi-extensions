5c896df54ff049c948947e19a1bf84284edb6b5c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-30 16:07:43 -0300

    fix: Fix popular animes page

--- multisrc/overrides/dooplay/animeplayer/src/AnimePlayer.kt
+++ multisrc/overrides/dooplay/animeplayer/src/AnimePlayer.kt
@@ -1,9 +1,15 @@
 package eu.kanade.tachiyomi.animeextension.pt.animeplayer
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
 
 class AnimePlayer : DooPlay(
     "pt-BR",
     "AnimePlayer",
     "https://animeplayer.com.br",
-)
+) {
+    // ============================== Popular ===============================
+    override fun popularAnimeSelector() = "div#featured-titles article div.poster"
+
+    override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes/")
+}
