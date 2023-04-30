a5826a179a268047b50c37dec349bcdb967e3117
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-30 16:07:02 -0300

    feat(multisrc/dooplay): Add animeplayer override base

---
+++ multisrc/overrides/dooplay/animeplayer/src/AnimePlayer.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.animeextension.pt.animeplayer
+
+import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+
+class AnimePlayer : DooPlay(
+    "pt-BR",
+    "AnimePlayer",
+    "https://animeplayer.com.br",
+)
