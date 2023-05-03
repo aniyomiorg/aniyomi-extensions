61a3e50ce9e3e3a83b0bf4dbe9976b0365d3b256
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-05-03 13:36:42 -0300

    fix: Fix popular animes page

--- multisrc/overrides/dooplay/animesfoxbr/src/AnimesFoxBR.kt
+++ multisrc/overrides/dooplay/animesfoxbr/src/AnimesFoxBR.kt
@@ -1,9 +1,19 @@
 package eu.kanade.tachiyomi.animeextension.pt.animesfoxbr
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
 
 class AnimesFoxBR : DooPlay(
     "pt-BR",
     "AnimesFox BR",
     "https://animesfoxbr.com",
-)
+) {
+    // ============================== Popular ===============================
+    // The site doesn't have a true popular anime tab,
+    // so we use the latest added anime page instead.
+    override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes/page/$page")
+
+    override fun popularAnimeSelector() = "div.clw div.b_flex > div > a"
+
+    override fun popularAnimeNextPageSelector() = "div.pagination i#nextpagination"
+}
