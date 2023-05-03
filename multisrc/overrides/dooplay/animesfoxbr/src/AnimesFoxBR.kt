ae20064f97c8e40e425b7b67a7bf1e218ab1d360
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-05-03 13:26:38 -0300

    feat(multisrc/dooplay): New source: AnimesFox BR

---
+++ multisrc/overrides/dooplay/animesfoxbr/src/AnimesFoxBR.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.animeextension.pt.animesfoxbr
+
+import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+
+class AnimesFoxBR : DooPlay(
+    "pt-BR",
+    "AnimesFox BR",
+    "https://animesfoxbr.com",
+)
