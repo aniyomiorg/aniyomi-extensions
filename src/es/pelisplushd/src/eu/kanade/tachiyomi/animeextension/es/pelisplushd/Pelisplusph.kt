e3f3c3f6f9d20ab01f122da27d187e940d1d53a6
imper1aldev <imperialdev@outlook.com>
2023-05-09 19:38:20 -0600

    Pelisplushd: New source added to Factory

--- src/es/pelisplushd/src/eu/kanade/tachiyomi/animeextension/es/pelisplushd/Pelisplusph.kt
+++ src/es/pelisplushd/src/eu/kanade/tachiyomi/animeextension/es/pelisplushd/Pelisplusph.kt
@@ -19,8 +19,6 @@ import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Request
 import okhttp3.Response
@@ -28,32 +26,42 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import uy.kohesive.injekt.injectLazy
 
-class Pelisplusto(override val name: String, override val baseUrl: String) : Pelisplushd(name, baseUrl) {
+class Pelisplusph(override val name: String, override val baseUrl: String) : Pelisplushd(name, baseUrl) {
 
     private val json: Json by injectLazy()
 
     override val supportsLatest = false
 
-    override fun popularAnimeSelector(): String = "article.item"
+    override fun popularAnimeSelector(): String = ".items-peliculas .item-pelicula"
 
-    override fun popularAnimeNextPageSelector(): String = "a[rel=\"next\"]"
+    override fun popularAnimeNextPageSelector(): String = ".items-peliculas > a"
 
     override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/peliculas?page=$page")
 
     override fun popularAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.setUrlWithoutDomain(element.select("a").attr("href"))
-        anime.title = element.select("a h2").text()
-        anime.thumbnail_url = element.select("a .item__image picture img").attr("data-src")
+        anime.setUrlWithoutDomain(baseUrl + element.selectFirst("a")?.attr("href"))
+        anime.title = element.select("a .item-detail > p").text()
+        anime.thumbnail_url = baseUrl + element.select("a .item-picture img").attr("src")
         return anime
     }
 
     override fun animeDetailsParse(document: Document): SAnime {
         val anime = SAnime.create()
-        anime.title = document.selectFirst(".home__slider_content div h1.slugh1")!!.text()
-        anime.description = document.selectFirst(".home__slider_content .description")!!.text()
-        anime.genre = document.select(".home__slider_content div:nth-child(5) > a").joinToString { it.text() }
-        anime.status = SAnime.COMPLETED
+        anime.title = document.selectFirst(".info-content h1")!!.text()
+        document.select(".info-content p").map { p ->
+            if (p.select(".content-type").text().contains("Sinópsis:")) {
+                anime.description = p.select(".sinopsis")!!.text()
+            }
+            if (p.select(".content-type").text().contains("Géneros:")) {
+                anime.genre = p.select(".content-type-a a").joinToString { it.text() }
+            }
+            if (p.select(".content-type").text().contains("Reparto:")) {
+                anime.artist = p.select(".content-type ~ span").text().substringBefore(",")
+            }
+        }
+        anime.status = if (document.location().contains("/serie/")) SAnime.UNKNOWN else SAnime.COMPLETED
+
         return anime
     }
 
@@ -68,26 +76,21 @@ class Pelisplusto(override val name: String, override val baseUrl: String) : Pel
             episode.setUrlWithoutDomain(response.request.url.toString())
             episodes.add(episode)
         } else {
-            var jsonscript = ""
-            jsoup.select("script[type=text/javascript]").mapNotNull { script ->
-                val ssRegex = Regex("(?i)seasons")
-                val ss = if (script.data().contains(ssRegex)) script.data() else ""
-                val swaa = ss.substringAfter("seasonsJson = ").substringBefore(";")
-                jsonscript = swaa
-            }
-            val jsonParse = json.decodeFromString<JsonObject>(jsonscript)
             var index = 0
-            jsonParse.entries.map {
-                it.value.jsonArray.reversed().map { element ->
+            jsoup.select(".item-season").reversed().mapIndexed { idxSeas, season ->
+                val seasonNumber = try {
+                    getNumberFromString(season.selectFirst(".item-season-title")!!.ownText())
+                } catch (_: Exception) { idxSeas + 1 }
+                season.select(".item-season-episodes a").reversed().mapIndexed { idx, ep ->
                     index += 1
-                    val jsonElement = element!!.jsonObject
-                    val season = jsonElement["season"]!!.jsonPrimitive!!.content
-                    val title = jsonElement["title"]!!.jsonPrimitive!!.content
-                    val ep = jsonElement["episode"]!!.jsonPrimitive!!.content
+                    val noEp = try {
+                        getNumberFromString(ep.ownText())
+                    } catch (_: Exception) { idx + 1 }
+
                     val episode = SEpisode.create()
                     episode.episode_number = index.toFloat()
-                    episode.name = "T$season - E$ep - $title"
-                    episode.setUrlWithoutDomain("${response.request.url}/season/$season/episode/$ep")
+                    episode.name = "T$seasonNumber - E$noEp - ${ep.ownText()}"
+                    episode.setUrlWithoutDomain(baseUrl + ep.attr("href"))
                     episodes.add(episode)
                 }
             }
@@ -99,7 +102,7 @@ class Pelisplusto(override val name: String, override val baseUrl: String) : Pel
         val filterList = if (filters.isEmpty()) getFilterList() else filters
         val genreFilter = filterList.find { it is GenreFilter } as GenreFilter
         return when {
-            query.isNotBlank() -> GET("$baseUrl/api/search?search=$query", headers)
+            query.isNotBlank() -> GET("$baseUrl/search/$query", headers)
             genreFilter.state != 0 -> GET("$baseUrl/${genreFilter.toUriPart()}?page=$page")
             else -> popularAnimeRequest(page)
         }
@@ -108,14 +111,15 @@ class Pelisplusto(override val name: String, override val baseUrl: String) : Pel
     override fun videoListParse(response: Response): List<Video> {
         val document = response.asJsoup()
         val videoList = mutableListOf<Video>()
-        document.select(".bg-tabs li").map { it ->
-            val link = it.attr("data-server")
-                .replace("https://owodeuwu.xyz", "https://fembed.com")
-                .replace("https://sblanh.com", "https://watchsb.com")
-                .replace(Regex("([a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)=https:\\/\\/ww3.pelisplus.to.*"), "")
-
-            loadExtractor(link).let { videos ->
-                videoList.addAll(videos)
+        document.select("[class*=server-item-]").map {
+            val langIdx = getNumberFromString(it.attr("class").substringAfter("server-item-"))
+            val langItem = document.select("li[data-id=\"$langIdx\"] a").text()
+            val lang = if (langItem.contains("Subtitulado")) "[Sub]" else if (langItem.contains("Latino")) "[Lat]" else "[Cast]"
+            it.select("li.tab-video").map { servers ->
+                val url = servers.attr("data-video")
+                loadExtractor(url, lang).let { videos ->
+                    videoList.addAll(videos)
+                }
             }
         }
         return videoList
@@ -177,7 +181,7 @@ class Pelisplusto(override val name: String, override val baseUrl: String) : Pel
             )
         }
         if (embedUrl.contains("voe")) {
-            VoeExtractor(client).videoFromUrl(url)?.let { videoList.add(it) }
+            VoeExtractor(client).videoFromUrl(url, "$prefix VoeCDN")?.let { videoList.add(it) }
         }
         if (embedUrl.contains("filemoon") || embedUrl.contains("moonplayer")) {
             FilemoonExtractor(client).videoFromUrl(url, prefix)?.let {
@@ -201,37 +205,109 @@ class Pelisplusto(override val name: String, override val baseUrl: String) : Pel
             Pair("<selecionar>", ""),
             Pair("Peliculas", "peliculas"),
             Pair("Series", "series"),
-            Pair("Doramas", "doramas"),
-            Pair("Animes", "animes"),
-            Pair("Acción", "genres/accion"),
-            Pair("Action & Adventure", "genres/action-adventure"),
-            Pair("Animación", "genres/animacion"),
-            Pair("Aventura", "genres/aventura"),
-            Pair("Bélica", "genres/belica"),
-            Pair("Ciencia ficción", "genres/ciencia-ficcion"),
-            Pair("Comedia", "genres/comedia"),
-            Pair("Crimen", "genres/crimen"),
-            Pair("Documental", "genres/documental"),
-            Pair("Dorama", "genres/dorama"),
-            Pair("Drama", "genres/drama"),
-            Pair("Familia", "genres/familia"),
-            Pair("Fantasía", "genres/fantasia"),
-            Pair("Guerra", "genres/guerra"),
-            Pair("Historia", "genres/historia"),
-            Pair("Horror", "genres/horror"),
-            Pair("Kids", "genres/kids"),
-            Pair("Misterio", "genres/misterio"),
-            Pair("Música", "genres/musica"),
-            Pair("Musical", "genres/musical"),
-            Pair("Película de TV", "genres/pelicula-de-tv"),
-            Pair("Reality", "genres/reality"),
-            Pair("Romance", "genres/romance"),
-            Pair("Sci-Fi & Fantasy", "genres/sci-fi-fantasy"),
-            Pair("Soap", "genres/soap"),
-            Pair("Suspense", "genres/suspense"),
-            Pair("Terror", "genres/terror"),
-            Pair("War & Politics", "genres/war-politics"),
-            Pair("Western", "genres/western"),
+            Pair("Estrenos", "estrenos"),
+            Pair("Acción", "genero/accion"),
+            Pair("Artes marciales", "genero/artes-marciales"),
+            Pair("Asesinos en serie", "genero/asesinos-en-serie"),
+            Pair("Aventura", "genero/aventura"),
+            Pair("Baile", "genero/baile"),
+            Pair("Bélico", "genero/belico"),
+            Pair("Biografico", "genero/biografico"),
+            Pair("Catástrofe", "genero/catastrofe"),
+            Pair("Ciencia Ficción", "genero/ciencia-ficcion"),
+            Pair("Cine Adolescente", "genero/cine-adolescente"),
+            Pair("Cine LGBT", "genero/cine-lgbt"),
+            Pair("Cine Negro", "genero/cine-negro"),
+            Pair("Cine Policiaco", "genero/cine-policiaco"),
+            Pair("Clásicas", "genero/clasicas"),
+            Pair("Comedia", "genero/comedia"),
+            Pair("Comedia Negra", "genero/comedia-negra"),
+            Pair("Crimen", "genero/crimen"),
+            Pair("DC Comics", "genero/dc-comics"),
+            Pair("Deportes", "genero/deportes"),
+            Pair("Desapariciones", "genero/desapariciones"),
+            Pair("Disney", "genero/disney"),
+            Pair("Documental", "genero/documental"),
+            Pair("Drama", "genero/drama"),
+            Pair("Familiar", "genero/familiar"),
+            Pair("Fantasía", "genero/fantasia"),
+            Pair("Historia", "genero/historia"),
+            Pair("Horror", "genero/horror"),
+            Pair("Infantil", "genero/infantil"),
+            Pair("Intriga", "genero/intriga"),
+            Pair("live action", "genero/live-action"),
+            Pair("Marvel Comics", "genero/marvel-comics"),
+            Pair("Misterio", "genero/misterio"),
+            Pair("Música", "genero/musica"),
+            Pair("Musical", "genero/musical"),
+            Pair("Policial", "genero/policial"),
+            Pair("Político", "genero/politico"),
+            Pair("Psicológico", "genero/psicologico"),
+            Pair("Reality Tv", "genero/reality-tv"),
+            Pair("Romance", "genero/romance"),
+            Pair("Secuestro", "genero/secuestro"),
+            Pair("Slasher", "genero/slasher"),
+            Pair("Sobrenatural", "genero/sobrenatural"),
+            Pair("Stand Up", "genero/stand-up"),
+            Pair("Superhéroes", "genero/superheroes"),
+            Pair("Suspenso", "genero/suspenso"),
+            Pair("Terror", "genero/terror"),
+            Pair("Thriller", "genero/thriller"),
+            Pair("Tokusatsu", "genero/tokusatsu"),
+            Pair("TV Series", "genero/tv-series"),
+            Pair("Western", "genero/western"),
+            Pair("Zombie", "genero/zombie"),
+            Pair("Acción", "genero/accion"),
+            Pair("Artes marciales", "genero/artes-marciales"),
+            Pair("Asesinos en serie", "genero/asesinos-en-serie"),
+            Pair("Aventura", "genero/aventura"),
+            Pair("Baile", "genero/baile"),
+            Pair("Bélico", "genero/belico"),
+            Pair("Biografico", "genero/biografico"),
+            Pair("Catástrofe", "genero/catastrofe"),
+            Pair("Ciencia Ficción", "genero/ciencia-ficcion"),
+            Pair("Cine Adolescente", "genero/cine-adolescente"),
+            Pair("Cine LGBT", "genero/cine-lgbt"),
+            Pair("Cine Negro", "genero/cine-negro"),
+            Pair("Cine Policiaco", "genero/cine-policiaco"),
+            Pair("Clásicas", "genero/clasicas"),
+            Pair("Comedia", "genero/comedia"),
+            Pair("Comedia Negra", "genero/comedia-negra"),
+            Pair("Crimen", "genero/crimen"),
+            Pair("DC Comics", "genero/dc-comics"),
+            Pair("Deportes", "genero/deportes"),
+            Pair("Desapariciones", "genero/desapariciones"),
+            Pair("Disney", "genero/disney"),
+            Pair("Documental", "genero/documental"),
+            Pair("Drama", "genero/drama"),
+            Pair("Familiar", "genero/familiar"),
+            Pair("Fantasía", "genero/fantasia"),
+            Pair("Historia", "genero/historia"),
+            Pair("Horror", "genero/horror"),
+            Pair("Infantil", "genero/infantil"),
+            Pair("Intriga", "genero/intriga"),
+            Pair("live action", "genero/live-action"),
+            Pair("Marvel Comics", "genero/marvel-comics"),
+            Pair("Misterio", "genero/misterio"),
+            Pair("Música", "genero/musica"),
+            Pair("Musical", "genero/musical"),
+            Pair("Policial", "genero/policial"),
+            Pair("Político", "genero/politico"),
+            Pair("Psicológico", "genero/psicologico"),
+            Pair("Reality Tv", "genero/reality-tv"),
+            Pair("Romance", "genero/romance"),
+            Pair("Secuestro", "genero/secuestro"),
+            Pair("Slasher", "genero/slasher"),
+            Pair("Sobrenatural", "genero/sobrenatural"),
+            Pair("Stand Up", "genero/stand-up"),
+            Pair("Superhéroes", "genero/superheroes"),
+            Pair("Suspenso", "genero/suspenso"),
+            Pair("Terror", "genero/terror"),
+            Pair("Thriller", "genero/thriller"),
+            Pair("Tokusatsu", "genero/tokusatsu"),
+            Pair("TV Series", "genero/tv-series"),
+            Pair("Western", "genero/western"),
+            Pair("Zombie", "genero/zombie"),
         ),
     )
 
