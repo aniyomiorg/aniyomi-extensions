c44a096c47364f7596e44433b49a8d98ec0eca12
Quickdesh <devesh.ratra@gmail.com>
2023-05-19 23:50:32 +0900

    Add Tokuzilla (#1617)
    Co-authored-by: jmir1 <jhmiramon@gmail.com>

---
+++ lib/chillx-extractor/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+        targetSdk = AndroidConfig.targetSdk
+    }
+}
+
+dependencies {
+    compileOnly(libs.bundles.common)
+}
+// BUMPS: 0
