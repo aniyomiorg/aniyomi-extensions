80426140176bfd298777f8c8751f990e5e19d1e4
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-05-20 04:53:47 -0300

    chore: Bump version

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/dooplay/DooPlayGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/dooplay/DooPlayGenerator.kt
@@ -12,16 +12,16 @@ class DooPlayGenerator : ThemeSourceGenerator {
 
     override val sources = listOf(
         SingleLang("Animes House", "https://animeshouse.net", "pt-BR", isNsfw = false, overrideVersionCode = 4),
-        SingleLang("AnimesFox BR", "https://animesfoxbr.com", "pt-BR", isNsfw = false),
         SingleLang("AnimeOnline.Ninja", "https://www1.animeonline.ninja", "es", className = "AnimeOnlineNinja", isNsfw = false, overrideVersionCode = 26),
-        SingleLang("AnimeOnline360", "https://animeonline360.me", "en", isNsfw = false),
+        SingleLang("AnimesFox BR", "https://animesfoxbr.com", "pt-BR", isNsfw = false),
         SingleLang("AnimePlayer", "https://animeplayer.com.br", "pt-BR", isNsfw = true),
         SingleLang("Cinemathek", "https://cinemathek.net", "de", isNsfw = true, overrideVersionCode = 11),
         SingleLang("CineVision", "https://cinevision.vc", "pt-BR", isNsfw = true, overrideVersionCode = 4),
-        SingleLang("DonghuaX", "https://donghuax.com", "pt-BR", isNsfw = false),
         SingleLang("GoAnimes", "https://goanimes.net", "pt-BR", isNsfw = true),
         SingleLang("pactedanime", "https://pactedanime.com", "en", isNsfw = false, overrideVersionCode = 4),
-        SingleLang("Pi Fansubs", "https://pifansubs.club", "pt-BR", isNsfw = true, overrideVersionCode = 14),
+        SingleLang("AnimeOnline360", "https://animeonline360.me", "en", isNsfw = false),
+        SingleLang("Pi Fansubs", "https://pifansubs.org", "pt-BR", isNsfw = true, overrideVersionCode = 15),
+        SingleLang("DonghuaX", "https://donghuax.com", "pt-BR", isNsfw = false),
     )
 
     companion object {
