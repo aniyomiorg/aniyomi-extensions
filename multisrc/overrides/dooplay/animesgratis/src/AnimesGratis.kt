a182f11cc131468242a81f997d9db102c3083f24
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-09 11:06:02 -0300

    feat(multisrc/DooPlay): Create Animes Grátis(pt) base

---
+++ multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.animeextension.pt.animesgratis
+
+import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+
+class AnimesGratis : DooPlay(
+    "pt-BR",
+    "Animes Grátis",
+    "https://animesgratis.org",
+)
