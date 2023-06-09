be9bfde9de808e0847f60508180a25b097f7258d
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-09 11:12:08 -0300

    fix: Fix popular animes page

--- multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
+++ multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
@@ -1,9 +1,16 @@
 package eu.kanade.tachiyomi.animeextension.pt.animesgratis
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
 
 class AnimesGratis : DooPlay(
     "pt-BR",
     "Animes Grátis",
     "https://animesgratis.org",
-)
+) {
+
+    // ============================== Popular ===============================
+    override fun popularAnimeSelector() = "div.imdbRating > article > a"
+
+    override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes/")
+}
