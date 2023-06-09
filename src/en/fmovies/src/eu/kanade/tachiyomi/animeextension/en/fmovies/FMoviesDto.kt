a44c0b845ae3c949da260857566bfa53a812be00
Secozzi <49240133+Secozzi@users.noreply.github.com>
2023-06-09 14:25:56 +0200

    feat: New extension: FMovies (#1692)

---
+++ src/en/fmovies/src/eu/kanade/tachiyomi/animeextension/en/fmovies/FMoviesDto.kt
@@ -0,0 +1,36 @@
+package eu.kanade.tachiyomi.animeextension.en.fmovies
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class VrfResponse(
+    val url: String,
+    val vrfQuery: String? = null,
+)
+
+@Serializable
+data class AjaxResponse(
+    val result: String,
+)
+
+@Serializable
+data class AjaxServerResponse(
+    val result: UrlObject,
+) {
+    @Serializable
+    data class UrlObject(
+        val url: String,
+    )
+}
+
+@Serializable
+data class EpisodeInfo(
+    val id: String,
+    val url: String,
+)
+
+@Serializable
+data class FMoviesSubs(
+    val file: String,
+    val label: String,
+)
