b41ed8ef6270cded37e77fbfeb72b4095a97c183
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-09 15:04:26 -0300

    feat: Implement video extractors

--- multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
+++ multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
@@ -1,7 +1,16 @@
 package eu.kanade.tachiyomi.animeextension.pt.animesgratis
 
+import eu.kanade.tachiyomi.animeextension.pt.animesgratis.extractors.AnimesGratisPlayerExtractor
+import eu.kanade.tachiyomi.animeextension.pt.animesgratis.extractors.BloggerExtractor
+import eu.kanade.tachiyomi.animeextension.pt.animesgratis.extractors.RuplayExtractor
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.streamsbextractor.StreamSBExtractor
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.FormBody
+import okhttp3.Response
 import org.jsoup.nodes.Element
 
 class AnimesGratis : DooPlay(
@@ -16,6 +25,50 @@ class AnimesGratis : DooPlay(
     // =============================== Search ===============================
     override fun searchAnimeSelector() = latestUpdatesSelector()
     override fun searchAnimeFromElement(element: Element) = popularAnimeFromElement(element)
+
+    // ============================ Video Links =============================
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        val players = document.select("ul#playeroptionsul li")
+        return players.flatMap(::getPlayerVideos)
+    }
+
+    override val prefQualityValues = arrayOf("360p", "480p", "720p", "1080p")
+    override val prefQualityEntries = prefQualityValues
+
+    private fun getPlayerVideos(player: Element): List<Video> {
+        val name = player.selectFirst("span.title")!!.text().lowercase()
+        val url = getPlayerUrl(player)
+        return when {
+            "streamsb" in name ->
+                StreamSBExtractor(client).videosFromUrl(url, headers)
+            "ruplay" in name ->
+                RuplayExtractor(client).videosFromUrl(url)
+            "/player2/" in url ->
+                AnimesGratisPlayerExtractor(client).videosFromUrl(url)
+            "/player/" in url ->
+                BloggerExtractor(client).videosFromUrl(url, headers)
+            else -> emptyList<Video>()
+        }
+    }
+
+    private fun getPlayerUrl(player: Element): String {
+        val body = FormBody.Builder()
+            .add("action", "doo_player_ajax")
+            .add("post", player.attr("data-post"))
+            .add("nume", player.attr("data-nume"))
+            .add("type", player.attr("data-type"))
+            .build()
+
+        return client.newCall(POST("$baseUrl/wp-admin/admin-ajax.php", headers, body))
+            .execute()
+            .use { response ->
+                response.body.string()
+                    .substringAfter("\"embed_url\":\"")
+                    .substringBefore("\",")
+                    .replace("\\", "")
+            }
+    }
 
     // ============================== Filters ===============================
     override fun genresListRequest() = GET("$baseUrl/generos")
