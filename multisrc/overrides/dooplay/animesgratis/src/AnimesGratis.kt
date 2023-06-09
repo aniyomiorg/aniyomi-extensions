701dcebd876fbde9c745e393e05a29079a68c5d5
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-09 11:23:23 -0300

    fix: Fix search animes page

--- multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
+++ multisrc/overrides/dooplay/animesgratis/src/AnimesGratis.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.animeextension.pt.animesgratis
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
+import org.jsoup.nodes.Element
 
 class AnimesGratis : DooPlay(
     "pt-BR",
@@ -11,6 +12,9 @@ class AnimesGratis : DooPlay(
 
     // ============================== Popular ===============================
     override fun popularAnimeSelector() = "div.imdbRating > article > a"
-
     override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes/")
+
+    // =============================== Search ===============================
+    override fun searchAnimeSelector() = latestUpdatesSelector()
+    override fun searchAnimeFromElement(element: Element) = popularAnimeFromElement(element)
 }
