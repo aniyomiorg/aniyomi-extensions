760cd630ffc7d75d024b95a67a3315bd6e170669
jmir1 <jhmiramon@gmail.com>
2023-06-17 20:29:12 +0200

    fix: Use bot to commit repo branch

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,12 +2,12 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore ../master/repo/ .
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
+git config --global user.email "aniyomi-bot@aniyomi.org"
+git config --global user.name "aniyomi-bot[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
-    git commit -m "Update extensions repo"
+    git commit -S -m "Update extensions repo"
     git push
 
     # Purge cached index on jsDelivr
