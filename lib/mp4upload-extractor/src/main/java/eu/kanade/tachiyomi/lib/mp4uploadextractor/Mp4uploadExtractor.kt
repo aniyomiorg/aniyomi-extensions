ee6aed3db3c78e2f592d13bc417ee28637ca86ee
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-17 18:48:12 +0000

    feat(lib): New lib: Mp4uploadExtractor (#1753)

---
+++ lib/mp4upload-extractor/src/main/java/eu/kanade/tachiyomi/lib/mp4uploadextractor/Mp4uploadExtractor.kt
@@ -0,0 +1,36 @@
+package eu.kanade.tachiyomi.lib.mp4uploadextractor
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import dev.datlag.jsunpacker.JsUnpacker
+
+class Mp4uploadExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String, headers: Headers, prefix: String = "", suffix: String = ""): List<Video> {
+        val newHeaders = headers.newBuilder()
+            .add("referer", REFERER)
+            .build()
+
+        val doc = client.newCall(GET(url, newHeaders)).execute().use { it.asJsoup() }
+
+        val script = doc.selectFirst("script:containsData(eval):containsData(p,a,c,k,e,d)")?.data()
+            ?.let(JsUnpacker::unpackAndCombine)
+            ?: doc.selectFirst("script:containsData(player.src)")?.data()
+            ?: return emptyList()
+
+        val videoUrl = script.substringAfter(".src(").substringBefore(")")
+            .substringAfter("src:").substringAfter('"').substringBefore('"')
+
+        val resolution = QUALITY_REGEX.find(script)?.groupValues?.let { "${it[1]}p" } ?: "Unknown resolution"
+        val quality = "${prefix}Mp4Upload - $resolution$suffix"
+
+        return listOf(Video(videoUrl, quality, videoUrl, newHeaders))
+    }
+
+    companion object {
+        private val QUALITY_REGEX by lazy { """\WHEIGHT=(\d+)""".toRegex() }
+        private const val REFERER = "https://mp4upload.com/"
+    }
+}
