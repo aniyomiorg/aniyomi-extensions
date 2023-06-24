3616a37ecc6853b8081cef68a9f43d47a0a0b562
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-24 06:29:50 -0300

    chore: Bump version

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/animestream/AnimeStreamGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/animestream/AnimeStreamGenerator.kt
@@ -15,7 +15,7 @@ class AnimeStreamGenerator : ThemeSourceGenerator {
         SingleLang("LMAnime", "https://lmanime.com", "all", isNsfw = false, overrideVersionCode = 2),
         SingleLang("RineCloud", "https://rine.cloud", "pt-BR", isNsfw = false),
         SingleLang("Animenosub", "https://animenosub.com", "en", isNsfw = true),
-        SingleLang("MiniOppai", "https://minioppai.org", "id", isNsfw = true, overrideVersionCode = 1),
+        SingleLang("MiniOppai", "https://minioppai.org", "id", isNsfw = true, overrideVersionCode = 2),
         SingleLang("AnimeKhor", "https://animekhor.xyz", "en", isNsfw = false),
     )
 
