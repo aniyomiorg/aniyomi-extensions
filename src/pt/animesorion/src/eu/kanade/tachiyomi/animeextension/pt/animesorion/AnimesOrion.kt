d75ec0a3245cfdfdae8bdb15b69e42dd93e2dc89
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-30 19:15:43 -0300

    feat: Implement popular animes page

--- src/pt/animesorion/src/eu/kanade/tachiyomi/animeextension/pt/animesorion/AnimesOrion.kt
+++ src/pt/animesorion/src/eu/kanade/tachiyomi/animeextension/pt/animesorion/AnimesOrion.kt
@@ -26,21 +26,17 @@ class AnimesOrion : ParsedAnimeHttpSource() {
     override val supportsLatest = false
 
     // ============================== Popular ===============================
-    override fun popularAnimeRequest(page: Int): Request {
-        throw UnsupportedOperationException("Not used.")
+    override fun popularAnimeRequest(page: Int) = GET(baseUrl, headers)
+
+    override fun popularAnimeSelector() = "div.tab-content-block article > a"
+
+    override fun popularAnimeFromElement(element: Element) = SAnime.create().apply {
+        setUrlWithoutDomain(element.attr("href"))
+        title = element.selectFirst("h2.ttl")!!.text()
+        thumbnail_url = element.selectFirst("img")!!.attr("src")
     }
 
-    override fun popularAnimeSelector(): String {
-        throw UnsupportedOperationException("Not used.")
-    }
-
-    override fun popularAnimeFromElement(element: Element): SAnime {
-        throw UnsupportedOperationException("Not used.")
-    }
-
-    override fun popularAnimeNextPageSelector(): String? {
-        throw UnsupportedOperationException("Not used.")
-    }
+    override fun popularAnimeNextPageSelector() = null
 
     // =============================== Latest ===============================
     override fun latestUpdatesRequest(page: Int): Request {
