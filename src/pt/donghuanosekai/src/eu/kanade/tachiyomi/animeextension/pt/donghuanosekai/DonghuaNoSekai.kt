0a9a1da969975eee9fca44dd92fd5ac2cb8aa02c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-03 01:47:02 -0300

    feat: Implement search page

--- src/pt/donghuanosekai/src/eu/kanade/tachiyomi/animeextension/pt/donghuanosekai/DonghuaNoSekai.kt
+++ src/pt/donghuanosekai/src/eu/kanade/tachiyomi/animeextension/pt/donghuanosekai/DonghuaNoSekai.kt
@@ -7,13 +7,22 @@ import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import okhttp3.FormBody
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import rx.Observable
+import uy.kohesive.injekt.api.get
+import uy.kohesive.injekt.injectLazy
 
 class DonghuaNoSekai : ParsedAnimeHttpSource() {
 
@@ -24,6 +33,12 @@ class DonghuaNoSekai : ParsedAnimeHttpSource() {
     override val lang = "pt-BR"
 
     override val supportsLatest = true
+
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", baseUrl)
+        .add("Origin", baseUrl)
+
+    private val json: Json by injectLazy()
 
     // ============================== Popular ===============================
     override fun popularAnimeRequest(page: Int) = GET(baseUrl, headers)
@@ -68,17 +83,63 @@ class DonghuaNoSekai : ParsedAnimeHttpSource() {
         return AnimesPage(listOf(details), false)
     }
 
+    @Serializable
+    data class SearchResponseDto(
+        val results: List<String>,
+        val page: Int,
+        val total_page: Int = 1,
+    )
+
+    private val searchToken by lazy {
+        client.newCall(GET("$baseUrl/donghuas", headers)).execute()
+            .use { it.asJsoup() }
+            .selectFirst("div.menu_filter_box")!!
+            .attr("data-secury")
+    }
+
+    override fun getFilterList() = DonghuaNoSekaiFilters.FILTER_LIST
+
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
-        throw UnsupportedOperationException("Not used.")
+        val params = DonghuaNoSekaiFilters.getSearchParameters(filters)
+        val body = FormBody.Builder().apply {
+            add("type", "lista")
+            add("action", "getListFilter")
+            add("limit", "30")
+            add("token", searchToken)
+            add("search", query.ifBlank { "0" })
+            add("pagina", "$page")
+            val filterData = baseUrl.toHttpUrl().newBuilder().apply {
+                addQueryParameter("filter_animation", params.animation)
+                addQueryParameter("filter_audio", "undefined")
+                addQueryParameter("filter_letter", params.letter)
+                addQueryParameter("filter_order", params.orderBy)
+                addQueryParameter("filter_status", params.status)
+                addQueryParameter("type_url", "ONA")
+            }.build().encodedQuery
+
+            val genres = params.genres.joinToString { "\"$it\"" }
+            val delgenres = params.deleted_genres.joinToString { "\"$it\"" }
+
+            add("filters", """{"filter_data": "$filterData", "filter_genre_add": [$genres], "filter_genre_del": [$delgenres]}""")
+        }.build()
+
+        return POST("$baseUrl/wp-admin/admin-ajax.php", body = body, headers = headers)
     }
 
-    override fun searchAnimeSelector(): String {
-        throw UnsupportedOperationException("Not used.")
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        return runCatching {
+            val data = response.parseAs<SearchResponseDto>()
+            val animes = data.results.map(Jsoup::parse)
+                .mapNotNull { it.selectFirst(searchAnimeSelector()) }
+                .map(::searchAnimeFromElement)
+            val hasNext = data.total_page > data.page
+            AnimesPage(animes, hasNext)
+        }.getOrElse { AnimesPage(emptyList(), false) }
     }
 
-    override fun searchAnimeFromElement(element: Element): SAnime {
-        throw UnsupportedOperationException("Not used.")
-    }
+    override fun searchAnimeSelector() = "div.itemE > a"
+
+    override fun searchAnimeFromElement(element: Element) = latestUpdatesFromElement(element)
 
     override fun searchAnimeNextPageSelector(): String? {
         throw UnsupportedOperationException("Not used.")
@@ -113,6 +174,11 @@ class DonghuaNoSekai : ParsedAnimeHttpSource() {
 
     override fun videoUrlParse(document: Document): String {
         throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================= Utilities ==============================
+    private inline fun <reified T> Response.parseAs(): T {
+        return use { it.body.string() }.let(json::decodeFromString)
     }
 
     companion object {
