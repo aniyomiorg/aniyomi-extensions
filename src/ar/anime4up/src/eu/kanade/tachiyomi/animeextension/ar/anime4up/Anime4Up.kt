56cb9d3f868cb2f6d445a32484a7e5b2d5d7cc4a
adly98 <medofared5@gmail.com>
2023-07-06 07:37:39 +0200

    adding more video sources

--- src/ar/anime4up/src/eu/kanade/tachiyomi/animeextension/ar/anime4up/Anime4Up.kt
+++ src/ar/anime4up/src/eu/kanade/tachiyomi/animeextension/ar/anime4up/Anime4Up.kt
@@ -9,6 +9,7 @@ import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animeextension.ar.anime4up.extractors.GdrivePlayerExtractor
 import eu.kanade.tachiyomi.animeextension.ar.anime4up.extractors.SharedExtractor
 import eu.kanade.tachiyomi.animeextension.ar.anime4up.extractors.VidYardExtractor
+import eu.kanade.tachiyomi.animeextension.ar.anime4up.extractors.StreamWishExtractor
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
@@ -154,20 +155,24 @@ class Anime4Up : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
             url.contains("voe") -> {
                 VoeExtractor(client).videoFromUrl(url)?.let(::listOf)
             }
-            Regex("(do*d(?:stream)?\\.(?:com?|watch|to|s[ho]|cx|la|w[sf]|pm|re|yt|stream))").containsMatchIn(url) -> {
-                DoodExtractor(client).videoFromUrl(url, "Dood mirror")?.let(::listOf)
+            DOOD_REGEX.containsMatchIn(url) -> {
+                val finalUrl = DOOD_REGEX.find(url)!!.groupValues[0]
+                DoodExtractor(client).videoFromUrl("https://www.$finalUrl", "Dood mirror")?.let(::listOf)
             }
-            Regex("(?:v[aie]d[bp][aoe]?m|myvii?d|v[aei]{1,2}dshar[er]?)\\.(?:com|net|org|xyz)").containsMatchIn(url) -> {
-                VidBomExtractor(client).videosFromUrl(url)
+            VIDBOM_REGEX.containsMatchIn(url) -> {
+                val finalUrl = VIDBOM_REGEX.find(url)!!.groupValues[0]
+                VidBomExtractor(client).videosFromUrl("https://www.$finalUrl.html")
             }
-            Regex("(?:view|watch|embed(?:tv)?|tube|player|cloudemb|japopav|javplaya|p1ayerjavseen|gomovizplay|stream(?:ovies)?|vidmovie|javside|aintahalu|finaltayibin|yahlusubh|taeyabathuna|)?s{0,2}b?(?:embed\\d?|play\\d?|video|fast|full|streams{0,3}|the|speed|l?anh|tvmshow|longvu|arslanrocky|chill|rity|hight|brisk|face|lvturbo|net|one|asian|ani|rapid|sonic|lona)?\\.(?:com|net|org|one|tv|xyz|fun|pro|sbs)").containsMatchIn(url)
-            -> {
+            STREAMSB_REGEX.containsMatchIn(url) -> {
                 StreamSBExtractor(client).videosFromUrl(url, headers)
+            }
+            STREAMWISH_REGEX.containsMatchIn(url)  -> {
+                val finalUrl = STREAMWISH_REGEX.find(url)!!.groupValues[0]
+                StreamWishExtractor(client).videosFromUrl("https://www.$finalUrl")
             }
             else -> null
         } ?: emptyList()
     }
-
     // override fun videoListSelector() = "script:containsData(m3u8)"
     override fun videoListSelector() = "li[data-i] a"
 
@@ -250,7 +255,7 @@ class Anime4Up : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         anime.title = document.select("h1.anime-details-title").text()
         anime.genre = document.select("ul.anime-genres > li > a, div.anime-info > a").joinToString(", ") { it.text() }
         anime.description = document.select("p.anime-story").text()
-        document.select("div.anime-info a").text()?.also { statusText ->
+        document.select("div.anime-info a").text().also { statusText ->
             when {
                 statusText.contains("يعرض الان", true) -> anime.status = SAnime.ONGOING
                 statusText.contains("مكتمل", true) -> anime.status = SAnime.COMPLETED
@@ -263,7 +268,7 @@ class Anime4Up : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     // Latest
 
-    override fun latestUpdatesNextPageSelector(): String? = throw Exception("Not used")
+    override fun latestUpdatesNextPageSelector(): String = throw Exception("Not used")
 
     override fun latestUpdatesFromElement(element: Element): SAnime = throw Exception("Not used")
 
@@ -379,4 +384,10 @@ class Anime4Up : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         Status("يعرض الان", "%d9%8a%d8%b9%d8%b1%d8%b6-%d8%a7%d9%84%d8%a7%d9%86-1"),
 
     )
+    companion object {
+        private val VIDBOM_REGEX = Regex("(?:v[aie]d[bp][aoe]?m|myvii?d|segavid|v[aei]{1,2}dshar[er]?)\\.(?:com|net|org|xyz)(?::\\d+)?/(?:embed[/-])?([A-Za-z0-9]+)")
+        private val STREAMSB_REGEX = Regex("(?:view|watch|embed(?:tv)?|tube|player|cloudemb|japopav|javplaya|p1ayerjavseen|gomovizplay|stream(?:ovies)?|vidmovie|javside|aintahalu|finaltayibin|yahlusubh|taeyabathuna|)?s{0,2}b?(?:embed\\d?|play\\d?|video|fast|full|streams{0,3}|the|speed|l?anh|tvmshow|longvu|arslanrocky|chill|rity|hight|brisk|face|lvturbo|net|one|asian|ani|rapid|sonic|lona)?\\.(?:com|net|org|one|tv|xyz|fun|pro|sbs)")
+        private val DOOD_REGEX = Regex("(do*d(?:stream)?\\.(?:com?|watch|to|s[ho]|cx|la|w[sf]|pm|re|yt|stream))/(?:d|e)/([0-9a-zA-Z]+)")
+        private val STREAMWISH_REGEX = Regex("((?:streamwish|anime7u|animezd|ajmidyad|khadhnayad|yadmalik|hayaatieadhab)\\.(?:com|to|sbs))/(?:e/|v/|f/)?([0-9a-zA-Z]+)")
+    }
 }
