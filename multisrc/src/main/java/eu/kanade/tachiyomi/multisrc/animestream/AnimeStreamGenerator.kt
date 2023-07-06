5adc861ce149ebc49e12c1e79ea9e5040a6f0308
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-06 03:25:50 -0300

    chore: Bump version

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/animestream/AnimeStreamGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/animestream/AnimeStreamGenerator.kt
@@ -11,14 +11,14 @@ class AnimeStreamGenerator : ThemeSourceGenerator {
     override val baseVersionCode = 2
 
     override val sources = listOf(
-        SingleLang("AnimeXin", "https://animexin.vip", "all", isNsfw = false, overrideVersionCode = 4),
-        SingleLang("LMAnime", "https://lmanime.com", "all", isNsfw = false, overrideVersionCode = 2),
-        SingleLang("Hstream", "https://hstream.moe", "en", isNsfw = true, overrideVersionCode = 2),
-        SingleLang("RineCloud", "https://rine.cloud", "pt-BR", isNsfw = false),
-        SingleLang("Animenosub", "https://animenosub.com", "en", isNsfw = true),
-        SingleLang("MiniOppai", "https://minioppai.org", "id", isNsfw = true, overrideVersionCode = 2),
         SingleLang("AnimeKhor", "https://animekhor.xyz", "en", isNsfw = false),
+        SingleLang("Animenosub", "https://animenosub.com", "en", isNsfw = true),
         SingleLang("AnimeTitans", "https://animetitans.com", "ar", isNsfw = false, overrideVersionCode = 11),
+        SingleLang("AnimeXin", "https://animexin.vip", "all", isNsfw = false, overrideVersionCode = 4),
+        SingleLang("Hstream", "https://hstream.moe", "en", isNsfw = true, overrideVersionCode = 3),
+        SingleLang("LMAnime", "https://lmanime.com", "all", isNsfw = false, overrideVersionCode = 2),
+        SingleLang("MiniOppai", "https://minioppai.org", "id", isNsfw = true, overrideVersionCode = 2),
+        SingleLang("RineCloud", "https://rine.cloud", "pt-BR", isNsfw = false),
     )
 
     companion object {
