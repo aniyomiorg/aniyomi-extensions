ba50a7b604bc74617dd6d9bacb8533ea4f89b4de
adly98 <111687237+adly98@users.noreply.github.com>
2023-07-06 18:17:50 +0300

    fix(ar/Anime4Up): adding more streaming servers (#1843)
    Co-authored-by: jmir1 <jhmiramon@gmail.com>

---
+++ src/ar/anime4up/src/eu/kanade/tachiyomi/animeextension/ar/anime4up/extractors/StreamWishExtractor.kt
@@ -0,0 +1,25 @@
+package eu.kanade.tachiyomi.animeextension.ar.anime4up.extractors
+
+import dev.datlag.jsunpacker.JsUnpacker
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+
+class StreamWishExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String): List<Video> {
+        val doc = client.newCall(GET(url)).execute().asJsoup()
+        val script = doc.selectFirst("script:containsData(sources)")!!.data()
+        val scriptData = if("eval" in script) JsUnpacker.unpackAndCombine(script)!! else script
+        val m3u8 = Regex("sources:\\s*\\[\\{\\s*\\t*file:\\s*[\"']([^\"']+)").find(scriptData)!!.groupValues[1]
+        val streamLink = Regex("(.*)_,(.*),\\.urlset/master(.*)").find(m3u8)!!
+        val streamQuality = streamLink.groupValues[2].split(",").reversed()
+        val qualities = scriptData.substringAfter("'qualityLabels':").substringBefore("},")
+        val qRegex = Regex("\".*?\":\\s*\"(.*?)\"").findAll(qualities)
+        return qRegex.mapIndexed { index, matchResult ->
+            val src = streamLink.groupValues[1] + "_" + streamQuality[index] + "/index-v1-a1" + streamLink.groupValues[3]
+            val quality = "Mirror: " +  matchResult.groupValues[1]
+            Video(src, quality, src)
+        }.toList()
+    }
+}
