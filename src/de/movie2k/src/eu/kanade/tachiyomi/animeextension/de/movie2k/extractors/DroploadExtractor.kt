283c807baabf20e0a15e7b2c4d5e2c038e9e7370
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-07-07 19:57:59 +0200

    add german ext: Movie2k (#1857)

---
+++ src/de/movie2k/src/eu/kanade/tachiyomi/animeextension/de/movie2k/extractors/DroploadExtractor.kt
@@ -0,0 +1,31 @@
+package eu.kanade.tachiyomi.animeextension.de.movie2k.extractors
+
+import dev.datlag.jsunpacker.JsUnpacker
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+
+class DroploadExtractor(private val client: OkHttpClient) {
+
+    fun videoFromUrl(url: String): MutableList<Video>? {
+        try {
+            val jsE = client.newCall(GET(url)).execute().asJsoup().selectFirst("script:containsData(eval)")!!.data()
+            val masterUrl = JsUnpacker.unpackAndCombine(jsE).toString()
+                .substringAfter("{file:\"").substringBefore("\"}")
+            val masterBase = masterUrl.substringBefore("master")
+            val masterPlaylist = client.newCall(GET(masterUrl)).execute().body.string()
+            val videoList = mutableListOf<Video>()
+            masterPlaylist.substringAfter("#EXT-X-STREAM-INF:").split("#EXT-X-STREAM-INF:")
+                .forEach {
+                    val quality = "Dropload:" + it.substringAfter("RESOLUTION=").substringAfter("x").substringBefore(",") + "p "
+                    val videoUrl = masterBase + it.substringAfter("\n").substringBefore("\n")
+                    videoList.add(Video(videoUrl, quality, videoUrl, headers = Headers.headersOf("origin", "https://dropload.io", "referer", "https://dropload.io/")))
+                }
+            return videoList
+        } catch (e: Exception) {
+            return null
+        }
+    }
+}
