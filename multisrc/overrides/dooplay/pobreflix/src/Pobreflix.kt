a45d08b5f91cd17f6dc93cf27d762ced80aabe62
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-15 04:25:44 -0300

    fix: Fix latest updates page

--- multisrc/overrides/dooplay/pobreflix/src/Pobreflix.kt
+++ multisrc/overrides/dooplay/pobreflix/src/Pobreflix.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.pt.pobreflix
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
 
 class Pobreflix : DooPlay(
     "pt-BR",
@@ -10,4 +11,7 @@ class Pobreflix : DooPlay(
 
     // ============================== Popular ===============================
     override fun popularAnimeSelector() = "div.featured div.poster"
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/series/page/$page/", headers)
 }
