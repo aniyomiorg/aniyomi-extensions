572e1dfba65ee287fa8d66a812163d8718ff2135
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-16 22:01:03 -0300

    refactor(multisrc): Merge remote refactorations

--- multisrc/src/main/java/generator/ThemeSourceGenerator.kt
+++ multisrc/src/main/java/generator/ThemeSourceGenerator.kt
@@ -132,9 +132,9 @@ interface ThemeSourceGenerator {
             File(projectRootPath).let { projectRootFile ->
                 println("Generating $source")
 
-                projectRootFile.mkdirs()
                 // remove everything from past runs
-                cleanDirectory(projectRootFile)
+                projectRootFile.deleteRecursively()
+                projectRootFile.mkdirs()
 
                 writeGradle(projectGradleFile, source, themePkg, baseVersionCode, defaultAdditionalGradlePath, additionalGradleOverridePath)
                 writeAndroidManifest(projectAndroidManifestFile, manifestOverridePath, defaultAndroidManifestPath)
@@ -238,15 +238,6 @@ interface ThemeSourceGenerator {
                 |${factoryClassText()}
                 """.trimMargin(),
             )
-        }
-
-        private fun cleanDirectory(dir: File) {
-            dir.listFiles()?.forEach {
-                if (it.isDirectory) {
-                    cleanDirectory(it)
-                }
-                it.delete()
-            }
         }
     }
 }
