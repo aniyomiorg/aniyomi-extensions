25726b3ee1ba4b890fd3bd78d19a5c3883260175
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-28 12:58:55 -0300

    fix: Fix search animes page

--- multisrc/overrides/dooplay/animesync/src/AnimeSync.kt
+++ multisrc/overrides/dooplay/animesync/src/AnimeSync.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.animeextension.pt.animesync
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
+import org.jsoup.nodes.Element
 
 class AnimeSync : DooPlay(
     "pt-BR",
@@ -10,8 +11,11 @@ class AnimeSync : DooPlay(
 ) {
     // ============================== Popular ===============================
     override fun popularAnimeSelector() = "div.imdbRating > article > a"
-
     override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes")
+
+    // =============================== Search ===============================
+    override fun searchAnimeSelector() = "div.items > article.item"
+    override fun searchAnimeFromElement(element: Element) = popularAnimeFromElement(element)
 
     // ============================== Filters ===============================
     override fun genresListRequest() = GET("$baseUrl/generos/")
