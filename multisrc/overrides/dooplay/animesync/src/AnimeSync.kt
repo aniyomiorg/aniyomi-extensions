a014f00de02fbbb82fba2e9b71343a820652831c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-28 12:51:33 -0300

    fix: Fix popular animes page

--- multisrc/overrides/dooplay/animesync/src/AnimeSync.kt
+++ multisrc/overrides/dooplay/animesync/src/AnimeSync.kt
@@ -8,6 +8,11 @@ class AnimeSync : DooPlay(
     "AnimeSync",
     "https://animesync.org",
 ) {
+    // ============================== Popular ===============================
+    override fun popularAnimeSelector() = "div.imdbRating > article > a"
+
+    override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes")
+
     // ============================== Filters ===============================
     override fun genresListRequest() = GET("$baseUrl/generos/")
     override fun genresListSelector() = "ul.generos li > a"
