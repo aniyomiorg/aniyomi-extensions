0ef5bf258080fe2a7f59eaac66283364ad616343
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-30 14:59:48 -0300

    fix: Fix search filters

--- multisrc/overrides/animestream/animeindo/src/AnimeIndo.kt
+++ multisrc/overrides/animestream/animeindo/src/AnimeIndo.kt
@@ -1,11 +1,19 @@
 package eu.kanade.tachiyomi.animeextension.id.animeindo
 
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.GenresFilter
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.OrderFilter
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.SeasonFilter
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.StatusFilter
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.StudioFilter
+import eu.kanade.tachiyomi.animeextension.id.animeindo.AnimeIndoFilters.TypeFilter
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
+import eu.kanade.tachiyomi.multisrc.animestream.AnimeStreamFilters
 import eu.kanade.tachiyomi.network.GET
+import okhttp3.Request
 import org.jsoup.nodes.Element
-import uy.kohesive.injekt.api.get
 
 class AnimeIndo : AnimeStream(
     "id",
@@ -21,8 +29,16 @@ class AnimeIndo : AnimeStream(
     override fun latestUpdatesRequest(page: Int) = GET("$animeListUrl/page/$page/?order=update")
 
     // =============================== Search ===============================
-    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList) =
-        GET("$animeListUrl/page/$page/?title=$query")
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        val params = AnimeIndoFilters.getSearchParameters(filters)
+        val multiString = buildString {
+            if (params.genres.isNotEmpty()) append(params.genres + "&")
+            if (params.seasons.isNotEmpty()) append(params.seasons + "&")
+            if (params.studios.isNotEmpty()) append(params.studios + "&")
+        }
+
+        return GET("$animeListUrl/page/$page/?title=$query&$multiString&status=${params.status}&type=${params.type}&order=${params.order}")
+    }
 
     override fun searchAnimeSelector() = "div.animepost > div > a"
 
@@ -35,5 +51,21 @@ class AnimeIndo : AnimeStream(
     override fun searchAnimeNextPageSelector() = "div.pagination a:contains(i#nextpagination)"
 
     // ============================== Filters ===============================
-    override val fetchFilters = false
+    override val filtersSelector = "div.filtersearch tbody > tr:not(:has(td.filter_title:contains(Search))) > td.filter_act"
+
+    override fun getFilterList(): AnimeFilterList {
+        return if (AnimeStreamFilters.filterInitialized()) {
+            AnimeFilterList(
+                OrderFilter(orderFilterText),
+                StatusFilter(statusFilterText),
+                TypeFilter(typeFilterText),
+                AnimeFilter.Separator(),
+                GenresFilter(genresFilterText),
+                SeasonFilter(seasonsFilterText),
+                StudioFilter(studioFilterText),
+            )
+        } else {
+            AnimeFilterList(AnimeFilter.Header(filtersMissingWarning))
+        }
+    }
 }
