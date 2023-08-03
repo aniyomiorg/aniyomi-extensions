792448f127f0f15d3d819736b9a02649d47c6bdb
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-08-03 16:08:58 -0300

    feat: Implement search animes page

--- src/pt/animesgames/src/eu/kanade/tachiyomi/animeextension/pt/animesgames/AnimesGames.kt
+++ src/pt/animesgames/src/eu/kanade/tachiyomi/animeextension/pt/animesgames/AnimesGames.kt
@@ -7,13 +7,20 @@ import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import okhttp3.FormBody
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import rx.Observable
+import uy.kohesive.injekt.injectLazy
 
 class AnimesGames : ParsedAnimeHttpSource() {
 
@@ -24,6 +31,8 @@ class AnimesGames : ParsedAnimeHttpSource() {
     override val lang = "pt-BR"
 
     override val supportsLatest = true
+
+    private val json: Json by injectLazy()
 
     // ============================== Popular ===============================
     override fun popularAnimeRequest(page: Int) = GET(baseUrl)
@@ -67,16 +76,64 @@ class AnimesGames : ParsedAnimeHttpSource() {
         return AnimesPage(listOf(details), false)
     }
 
+    @Serializable
+    data class SearchResponseDto(
+        val results: List<String>,
+        val page: Int,
+        val total_page: Int = 1,
+    )
+
+    private val searchToken by lazy {
+        client.newCall(GET("$baseUrl/lista-de-animes", headers)).execute()
+            .use { it.asJsoup() }
+            .selectFirst("div.menu_filter_box")!!
+            .attr("data-secury")
+    }
+
+    override fun getFilterList() = AnimesGamesFilters.FILTER_LIST
+
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
-        throw UnsupportedOperationException("Not used.")
+        val params = AnimesGamesFilters.getSearchParameters(filters)
+        val body = FormBody.Builder().apply {
+            add("pagina", "$page")
+            add("type", "lista")
+            add("type_url", "anime")
+            add("limit", "30")
+            add("token", searchToken)
+            add("search", query.ifBlank { "0" })
+            val filterData = baseUrl.toHttpUrl().newBuilder().apply {
+                addQueryParameter("filter_audio", params.audio)
+                addQueryParameter("filter_letter", params.letter)
+                addQueryParameter("filter_order", params.orderBy)
+                addQueryParameter("filter_sort", "abc")
+            }.build().encodedQuery
+
+            val genres = params.genres.joinToString { "\"$it\"" }
+            val delgenres = params.deleted_genres.joinToString { "\"$it\"" }
+
+            add("filters", """{"filter_data": "$filterData", "filter_genre_add": [$genres], "filter_genre_del": [$delgenres]}""")
+        }.build()
+
+        return POST("$baseUrl/func/listanime", body = body, headers = headers)
     }
 
-    override fun searchAnimeSelector(): String {
-        throw UnsupportedOperationException("Not used.")
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        return runCatching {
+            val data = response.parseAs<SearchResponseDto>()
+            val animes = data.results.map(Jsoup::parse)
+                .mapNotNull { it.selectFirst(searchAnimeSelector()) }
+                .map(::searchAnimeFromElement)
+            val hasNext = data.total_page > data.page
+            AnimesPage(animes, hasNext)
+        }.getOrElse { AnimesPage(emptyList(), false) }
     }
 
-    override fun searchAnimeFromElement(element: Element): SAnime {
-        throw UnsupportedOperationException("Not used.")
+    override fun searchAnimeSelector() = "section.animeItem > a"
+
+    override fun searchAnimeFromElement(element: Element) = SAnime.create().apply {
+        setUrlWithoutDomain(element.attr("href"))
+        title = element.selectFirst("div.tituloAnime")!!.text()
+        thumbnail_url = element.selectFirst("img")!!.attr("src")
     }
 
     override fun searchAnimeNextPageSelector(): String? {
@@ -112,6 +169,11 @@ class AnimesGames : ParsedAnimeHttpSource() {
 
     override fun videoUrlParse(document: Document): String {
         throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================= Utilities ==============================
+    private inline fun <reified T> Response.parseAs(): T {
+        return use { it.body.string() }.let(json::decodeFromString)
     }
 
     companion object {
