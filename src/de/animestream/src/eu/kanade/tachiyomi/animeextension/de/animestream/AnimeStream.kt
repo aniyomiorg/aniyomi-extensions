28e36a7e5ae28859199c525ed4c8124a0d8d23b2
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-08-11 22:41:26 +0200

    feat(de/animestream): Rework
    Used the old ext structure so will turn the removal into a rework

---
+++ src/de/animestream/src/eu/kanade/tachiyomi/animeextension/de/animestream/AnimeStream.kt
@@ -0,0 +1,175 @@
+package eu.kanade.tachiyomi.animeextension.de.animestream
+
+import android.app.Application
+import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animeextension.de.animestream.extractors.MetaExtractor
+import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import kotlin.Exception
+
+class AnimeStream : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
+
+    override val name = "Anime-Stream"
+
+    override val baseUrl = "https://anime-stream.to"
+
+    override val lang = "de"
+
+    override val supportsLatest = false
+
+    override val client: OkHttpClient = network.client
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
+    override fun popularAnimeSelector(): String = "div.movies-list div.ml-item"
+
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/series/page/$page/")
+
+    override fun popularAnimeFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.thumbnail_url = element.select("a img").attr("data-original")
+        anime.title = element.select("a img").attr("alt")
+        return anime
+    }
+
+    override fun popularAnimeNextPageSelector(): String = "li.active ~ li"
+
+    // episodes
+
+    override fun episodeListSelector() = throw Exception("not used")
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val document = response.asJsoup()
+        val episodeList = mutableListOf<SEpisode>()
+        val episodeElement = document.select("div.les-content a")
+        episodeElement.forEach {
+            var num = 0
+            val episode = nEpisodeFromElement(it, num)
+            episodeList.add(episode)
+        }
+
+        return episodeList.reversed()
+    }
+
+    private fun nEpisodeFromElement(element: Element, num: Int): SEpisode {
+        num + 1
+        val episode = SEpisode.create()
+        episode.episode_number = num.toFloat()
+        episode.name = element.text()
+        episode.setUrlWithoutDomain(element.attr("href"))
+        return episode
+    }
+
+    override fun episodeFromElement(element: Element): SEpisode = throw Exception("not used")
+
+    // Video Extractor
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        return videosFromElement(document)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun videosFromElement(document: Document): List<Video> {
+        val videoList = mutableListOf<Video>()
+        val url = document.select("div a.lnk-lnk").attr("href")
+        val quality = "Metastream"
+        val video = MetaExtractor(client).videoFromUrl(url, quality)
+        if (video != null) {
+            videoList.add(video)
+        }
+        return videoList.reversed()
+    }
+
+    override fun List<Video>.sort(): List<Video> {
+        val hoster = preferences.getString("preferred_hoster", null)
+        if (hoster != null) {
+            val newList = mutableListOf<Video>()
+            var preferred = 0
+            for (video in this) {
+                if (video.quality.contains(hoster)) {
+                    newList.add(preferred, video)
+                    preferred++
+                } else {
+                    newList.add(video)
+                }
+            }
+            return newList
+        }
+        return this
+    }
+
+    override fun videoListSelector() = throw Exception("not used")
+
+    override fun videoFromElement(element: Element) = throw Exception("not used")
+
+    override fun videoUrlParse(document: Document) = throw Exception("not used")
+
+    // Search
+
+    override fun searchAnimeFromElement(element: Element): SAnime {
+        val anime = SAnime.create()
+        anime.setUrlWithoutDomain(element.select("a").attr("href"))
+        anime.thumbnail_url = element.select("a img").attr("data-original")
+        anime.title = element.select("a img").attr("alt")
+        return anime
+    }
+
+    override fun searchAnimeNextPageSelector(): String = "li.active ~ li"
+
+    override fun searchAnimeSelector(): String = "div.movies-list div.ml-item"
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request = GET("$baseUrl/page/$page/?s=$query")
+
+    // Details
+
+    override fun animeDetailsParse(document: Document): SAnime {
+        val anime = SAnime.create()
+        anime.thumbnail_url = document.select("div.thumb img").attr("src")
+        anime.title = document.select("div.thumb img").attr("alt")
+        anime.description = document.select("div.desc p.f-desc").text()
+        anime.status = parseStatus(document.select("div.mvici-right span[itemprop=\"duration\"]").text())
+        anime.genre = document.select("div.mvici-left p a[rel=\"category tag\"]").joinToString(", ") { it.text() }
+        return anime
+    }
+
+    private fun parseStatus(status: String?) = when {
+        status == null -> SAnime.UNKNOWN
+        status.contains("Abgeschlossen", ignoreCase = true) -> SAnime.ONGOING
+        else -> SAnime.COMPLETED
+    }
+
+    // Latest
+
+    override fun latestUpdatesNextPageSelector(): String = throw Exception("not used")
+
+    override fun latestUpdatesFromElement(element: Element): SAnime = throw Exception("not used")
+
+    override fun latestUpdatesRequest(page: Int): Request = throw Exception("not used")
+
+    override fun latestUpdatesSelector(): String = throw Exception("not used")
+
+    // Preferences
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) = throw Exception("not used")
+}
