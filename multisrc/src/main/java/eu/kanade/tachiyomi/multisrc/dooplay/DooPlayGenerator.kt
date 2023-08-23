46d682417b2f01c2b4a6a7b4e7e12b71cd7066aa
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-08-23 13:48:48 -0300

    chore: Bump version

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/dooplay/DooPlayGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/dooplay/DooPlayGenerator.kt
@@ -13,7 +13,7 @@ class DooPlayGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         SingleLang("AnimeOnline360", "https://animeonline360.me", "en", isNsfw = false),
         SingleLang("AnimeOnline.Ninja", "https://ww3.animeonline.ninja", "es", className = "AnimeOnlineNinja", isNsfw = false, overrideVersionCode = 31),
-        SingleLang("AnimesOnline", "https://animesonline.nz", "pt-BR", isNsfw = false, overrideVersionCode = 2, pkgName = "animesgratis"),
+        SingleLang("AnimesOnline", "https://animesonline.nz", "pt-BR", isNsfw = false, overrideVersionCode = 3, pkgName = "animesgratis"),
         SingleLang("AnimePlayer", "https://animeplayer.com.br", "pt-BR", isNsfw = true, overrideVersionCode = 1),
         SingleLang("AnimePlayer", "https://animeplayer.com.br", "pt-BR", isNsfw = true),
         SingleLang("AnimeSync", "https://animesync.org", "pt-BR", isNsfw = true),
