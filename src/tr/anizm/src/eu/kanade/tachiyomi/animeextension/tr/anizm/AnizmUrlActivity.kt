1e06cf8a386b268d92c85bba2ef7a0f779974d06
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-08-31 04:26:42 -0300

    chore: Fix comment

--- src/tr/anizm/src/eu/kanade/tachiyomi/animeextension/tr/anizm/AnizmUrlActivity.kt
+++ src/tr/anizm/src/eu/kanade/tachiyomi/animeextension/tr/anizm/AnizmUrlActivity.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import kotlin.system.exitProcess
 
 /**
- * Springboard that accepts https://anizm.net/anime/<item> intents
+ * Springboard that accepts https://anizm.net/<item> intents
  * and redirects them to the main Aniyomi process.
  */
 class AnizmUrlActivity : Activity() {
