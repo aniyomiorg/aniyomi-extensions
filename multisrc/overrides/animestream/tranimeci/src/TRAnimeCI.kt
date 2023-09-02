ab60b8ede471eee0c953bd7555bef9419bafe3ea
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-09-02 00:00:11 -0300

    feat: Implement popular anime page

--- multisrc/overrides/animestream/tranimeci/src/TRAnimeCI.kt
+++ multisrc/overrides/animestream/tranimeci/src/TRAnimeCI.kt
@@ -1,9 +1,26 @@
 package eu.kanade.tachiyomi.animeextension.tr.tranimeci
 
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
+import eu.kanade.tachiyomi.network.GET
 
 class TRAnimeCI : AnimeStream(
     "tr",
     "TRAnimeCI",
     "https://tranimeci.com",
-)
+) {
+    override val client by lazy {
+        network.client.newBuilder()
+            .addInterceptor(ShittyProtectionInterceptor(network.client))
+            .build()
+    }
+
+    // ============================== Popular ===============================
+    override fun popularAnimeRequest(page: Int) = GET(baseUrl)
+
+    override fun popularAnimeSelector() = "div.releases:contains(Populer) + div.listupd a.tip"
+
+    override fun popularAnimeNextPageSelector() = null
+
+    // ============================== Filters ===============================
+    override val fetchFilters = false
+}
