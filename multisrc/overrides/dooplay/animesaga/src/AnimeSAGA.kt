729070e45819c12075c346628c1fb4111cceb3a6
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-09-04 00:22:27 -0300

    feat[WIP]: Initial convertion of AnimeSAGA to dooplay multisrc

---
+++ multisrc/overrides/dooplay/animesaga/src/AnimeSAGA.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.animeextension.hi.animesaga
+
+import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+
+class AnimeSAGA : DooPlay(
+    "hi",
+    "AnimeSAGA",
+    "https://www.animesaga.in",
+)
