c1e02ab2b7aa7e66582e8c2f9ac6b6a98d470fd2
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-09-10 00:56:31 -0300

    feat(lib): Convert VudeoExtractor to a shared lib

---
+++ lib/vudeo-extractor/src/main/java/eu/kanade/tachiyomi/lib/vudeoextractor/VudeoExtractor.kt
@@ -0,0 +1,30 @@
+package eu.kanade.tachiyomi.lib.vudeoextractor
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+
+class VudeoExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String, prefix: String = ""): List<Video> {
+        val doc = client.newCall(GET(url)).execute()
+            .use { it.asJsoup() }
+
+        val sources = doc.selectFirst("script:containsData(sources: [)")?.data()
+            ?: return emptyList()
+
+        val referer = "https://" + url.toHttpUrl().host + "/"
+
+        val headers = Headers.headersOf("referer", referer)
+
+        return sources.substringAfter("sources: [").substringBefore("]")
+            .replace("\"", "")
+            .split(',')
+            .filter { it.startsWith("https") } // remove invalid links
+            .map { videoUrl ->
+                Video(videoUrl, "${prefix}Vudeo", videoUrl, headers)
+            }
+    }
+}
