7f7891c9cefd345fee8699fde4cb9d361c54f8ac
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-09-10 17:48:19 +0200

    feat(src/de): New Source: CineClix (#2170)

---
+++ src/de/cineclix/src/eu/kanade/tachiyomi/animeextension/de/cineclix/extractors/SuperVideoExtractor.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.animeextension.de.cineclix.extractors
+
+import dev.datlag.jsunpacker.JsUnpacker
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+
+class SuperVideoExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String, prefix: String = ""): List<Video> {
+        return runCatching {
+            val doc = client.newCall(GET(url)).execute().asJsoup()
+
+            val script = doc.selectFirst("script:containsData(eval):containsData(p,a,c,k,e,d)")?.data()
+                ?.let(JsUnpacker::unpackAndCombine)
+                ?: return emptyList()
+            val masterUrl = script.substringAfter("sources:[{file:\"").substringBefore("\"}]")
+            PlaylistUtils(client).extractFromHls(masterUrl, videoNameGen = { "${prefix}SuperVideo - $it" })
+        }.getOrElse { emptyList() }
+    }
+}
