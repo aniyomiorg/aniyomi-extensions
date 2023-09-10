3b3fc047a161cef67ef7d7a44783bc40414226d5
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-09-10 15:08:51 +0200

    use playlist utils

--- src/de/cineclix/src/eu/kanade/tachiyomi/animeextension/de/cineclix/extractors/SuperVideoExtractor.kt
+++ src/de/cineclix/src/eu/kanade/tachiyomi/animeextension/de/cineclix/extractors/SuperVideoExtractor.kt
@@ -2,32 +2,21 @@ package eu.kanade.tachiyomi.animeextension.de.cineclix.extractors
 
 import dev.datlag.jsunpacker.JsUnpacker
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.OkHttpClient
 
 class SuperVideoExtractor(private val client: OkHttpClient) {
-    fun videosFromUrl(url: String, prefix: String = "", suffix: String = ""): List<Video> {
+    fun videosFromUrl(url: String, prefix: String = ""): List<Video> {
         return runCatching {
             val doc = client.newCall(GET(url)).execute().asJsoup()
 
             val script = doc.selectFirst("script:containsData(eval):containsData(p,a,c,k,e,d)")?.data()
                 ?.let(JsUnpacker::unpackAndCombine)
-            val masterUrl = script?.substringAfter("sources:[{file:\"")?.substringBefore("\"}]")
-            val masterPlaylist = client.newCall(GET(masterUrl!!)).execute().body.string()
-            val separator = "#EXT-X-STREAM-INF:"
-            masterPlaylist.substringAfter(separator).split(separator).map {
-                val resolution = it.substringAfter("RESOLUTION=")
-                    .substringAfter("x")
-                    .substringBefore(",") + "p"
-                val videoUrl = it.substringAfter("\n").substringBefore("\n")
-                val quality = "${prefix}SuperVideo - $resolution$suffix"
-                Video(
-                    videoUrl,
-                    quality,
-                    videoUrl,
-                )
-            }
+                ?: return emptyList()
+            val masterUrl = script.substringAfter("sources:[{file:\"").substringBefore("\"}]")
+            PlaylistUtils(client).extractFromHls(masterUrl, videoNameGen = { "${prefix}SuperVideo - $it" })
         }.getOrElse { emptyList() }
     }
 }
