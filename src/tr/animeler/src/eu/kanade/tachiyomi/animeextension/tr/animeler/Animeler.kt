7eb82fff2e77c33c1efac1d525d5747e83eb4cb7
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-09-12 13:24:57 -0300

    feat: Create Animeler base

---
+++ src/tr/animeler/src/eu/kanade/tachiyomi/animeextension/tr/animeler/Animeler.kt
@@ -0,0 +1,100 @@
+package eu.kanade.tachiyomi.animeextension.tr.animeler
+
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import okhttp3.Request
+import okhttp3.Response
+import rx.Observable
+import uy.kohesive.injekt.injectLazy
+
+class Animeler : AnimeHttpSource() {
+
+    override val name = "Animeler"
+
+    override val baseUrl = "https://animeler.me"
+
+    override val lang = "tr"
+
+    override val supportsLatest = false
+
+    private val json: Json by injectLazy()
+
+    // ============================== Popular ===============================
+    override fun popularAnimeRequest(page: Int): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesRequest(page: Int): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun latestUpdatesParse(response: Response): AnimesPage {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =============================== Search ===============================
+    override fun fetchSearchAnime(page: Int, query: String, filters: AnimeFilterList): Observable<AnimesPage> {
+        return if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
+            val id = query.removePrefix(PREFIX_SEARCH)
+            client.newCall(GET("$baseUrl/anime/$id"))
+                .asObservableSuccess()
+                .map(::searchAnimeByIdParse)
+        } else {
+            super.fetchSearchAnime(page, query, filters)
+        }
+    }
+
+    private fun searchAnimeByIdParse(response: Response): AnimesPage {
+        val details = animeDetailsParse(response)
+        return AnimesPage(listOf(details), false)
+    }
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =========================== Anime Details ============================
+    override fun animeDetailsParse(response: Response): SAnime {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================== Episodes ==============================
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================ Video Links =============================
+    override fun videoListRequest(episode: SEpisode): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun videoListParse(response: Response): List<Video> {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================= Utilities ==============================
+    private inline fun <reified T> Response.parseAs(): T {
+        return body.string().let(json::decodeFromString)
+    }
+
+    companion object {
+        const val PREFIX_SEARCH = "id:"
+    }
+}
