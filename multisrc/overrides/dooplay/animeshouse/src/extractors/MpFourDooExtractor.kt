aae16cdb6dd2fecf900bed2e0ff9031f6351bdbc
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-09-13 20:48:33 -0300

    refactor(pt/animeshouse): PlaylistUtils go BRRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- multisrc/overrides/dooplay/animeshouse/src/extractors/MpFourDooExtractor.kt
+++ multisrc/overrides/dooplay/animeshouse/src/extractors/MpFourDooExtractor.kt
@@ -1,7 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.pt.animeshouse.extractors
 
 import eu.kanade.tachiyomi.animesource.model.Video
-import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 
@@ -11,32 +11,14 @@ class MpFourDooExtractor(
 ) {
     private val regexMpdoo = Regex("file\":\"(.*?)\"")
     private val playerName = "Mp4Doo"
+    private val playlistUtils by lazy { PlaylistUtils(client, headers) }
 
     fun getVideoList(js: String): List<Video> {
         val videoUrl = regexMpdoo.find(js)!!.groupValues
             .get(1)
             .replace("fy..", "fy.v.")
         return if (videoUrl.endsWith("playlist.m3u8")) {
-            val playlistBody = client.newCall(GET(videoUrl, headers)).execute()
-                .use { it.body.string() }
-
-            val separator = "#EXT-X-STREAM-INF:"
-            playlistBody.substringAfter(separator).split(separator).map {
-                val quality = playerName + " - " + it.substringAfter("RESOLUTION=")
-                    .substringAfter("x")
-                    .substringBefore("\n")
-                    .substringBefore(",") + "p"
-
-                val path = it.substringAfter("\n").substringBefore("\n")
-
-                val playlistUrl = if (!path.startsWith("https:")) {
-                    videoUrl.replace("playlist.m3u8", path)
-                } else {
-                    path
-                }
-
-                Video(playlistUrl, quality, playlistUrl, headers)
-            }
+            playlistUtils.extractFromHls(videoUrl, videoNameGen = { "$playerName: $it" })
         } else {
             listOf(Video(videoUrl, playerName, videoUrl, headers))
         }
