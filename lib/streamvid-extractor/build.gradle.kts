b3f11442aa89ebd2fb3c64573300ba9143b4a7d0
hollow <35398794+hollowshiroyuki@users.noreply.github.com>
2023-09-23 14:15:24 +0100

    Add: StreamVid extractor

---
+++ lib/streamvid-extractor/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+    namespace = "eu.kanade.tachiyomi.lib.streamvidextractor"
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+    }
+}
+
+dependencies {
+    implementation(project(":lib-playlist-utils"))
+    implementation("dev.datlag.jsunpacker:jsunpacker:1.0.1") {
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    }
+    compileOnly(libs.bundles.common)
+}
