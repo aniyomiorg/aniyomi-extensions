c5c07a17d8eaf6e4da4e182e2d55c150bc97b6e4
hollow <35398794+hollowshiroyuki@users.noreply.github.com>
2023-09-23 14:15:47 +0100

    Add: Upstream extractor

---
+++ lib/upstream-extractor/src/main/java/eu/kanade/tachiyomi/lib/upstreamextractor/UpstreamExtractor.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.lib.upstreamextractor
+
+import dev.datlag.jsunpacker.JsUnpacker
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.internal.commonEmptyHeaders
+
+class UpstreamExtractor(private val client: OkHttpClient) {
+    fun videosFromUrl(url: String, prefix: String = ""): List<Video> =
+        runCatching {
+            val jsE = client.newCall(GET(url)).execute().asJsoup().selectFirst("script:containsData(eval)")!!.data()
+            val masterUrl = JsUnpacker.unpackAndCombine(jsE)!!.substringAfter("{file:\"").substringBefore("\"}")
+            PlaylistUtils(client).extractFromHls(masterUrl, videoNameGen = { "${prefix}Upstream - $it" } )
+        }.getOrDefault(emptyList())
+}
