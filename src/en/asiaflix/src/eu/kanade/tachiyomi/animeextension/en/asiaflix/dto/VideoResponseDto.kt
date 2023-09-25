71323a29f410cc4c062a103643db2685a5a1ab36
AwkwardPeak7 <mubashirmusab@gmail.com>
2023-09-25 22:43:51 +0500

    cleanup

--- src/en/asiaflix/src/eu/kanade/tachiyomi/animeextension/en/asiaflix/dto/VideoResponseDto.kt
+++ src/en/asiaflix/src/eu/kanade/tachiyomi/animeextension/en/asiaflix/dto/VideoResponseDto.kt
@@ -1,6 +1,12 @@
 package eu.kanade.tachiyomi.animeextension.en.asiaflix.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+@Serializable
+data class StreamHeadDto(
+    @SerialName("stream_source") val source: String,
+)
 
 @Serializable
 data class EncryptedResponseDto(val data: String)
