6660336161470a6370b09eb531483bed801f83a9
folke <folke.steen85@gmail.com>
2023-10-14 11:21:50 +0200

    remove log part 2

--- src/en/fmovies/src/eu/kanade/tachiyomi/animeextension/en/fmovies/FMovies.kt
+++ src/en/fmovies/src/eu/kanade/tachiyomi/animeextension/en/fmovies/FMovies.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.animeextension.en.fmovies
 
 import android.app.Application
 import android.content.SharedPreferences
-import android.util.Log
 import androidx.preference.ListPreference
 import androidx.preference.MultiSelectListPreference
 import androidx.preference.PreferenceScreen
@@ -237,8 +236,6 @@ class FMovies : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
                 ).execute().parseAs<AjaxServerResponse>().result.url
 
                 val decrypted = vrfHelper.decrypt(encrypted)
-
-                Log.i("SOMETHING", "$name - $decrypted")
 
                 when (name) {
                     "Vidplay", "MyCloud" -> vidsrcExtractor.videosFromUrl(decrypted, name)
