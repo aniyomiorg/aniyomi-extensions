e08c6db3001ec1aca0f8f076d6474d6d8d315915
Salman Ali <ali.salman.sa.01@gmail.com>
2023-10-25 23:41:24 +0545

    fix(en/animetake): Fix episodes and filters + migrate to GogoStreamExtractor (#2412)

--- src/en/animetake/src/eu/kanade/tachiyomi/animeextension/en/animetake/AnimeTake.kt
+++ src/en/animetake/src/eu/kanade/tachiyomi/animeextension/en/animetake/AnimeTake.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.SharedPreferences
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
-import eu.kanade.tachiyomi.animeextension.en.animetake.extractors.VidstreamingExtractor
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
@@ -13,6 +12,7 @@ import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.lib.doodextractor.DoodExtractor
 import eu.kanade.tachiyomi.lib.filemoonextractor.FilemoonExtractor
+import eu.kanade.tachiyomi.lib.gogostreamextractor.GogoStreamExtractor
 import eu.kanade.tachiyomi.lib.mp4uploadextractor.Mp4uploadExtractor
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
@@ -119,29 +119,41 @@ class AnimeTake : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     // ============================== Episodes ==============================
-    override fun episodeListSelector() = "div#eps > div > a[href]"
+    override fun episodeListSelector() = "div.tab-content"
 
     override fun episodeListParse(response: Response): List<SEpisode> {
         val document = response.asJsoup()
         val episodesLink = document.select(episodeListSelector())
-        return episodesLink.map(::episodeFromElement).reversed()
+        val episodes = mutableListOf<SEpisode>()
+
+        val specialsDiv = episodesLink.select("div#specials")
+        if (specialsDiv.isNotEmpty()) {
+            episodes.addAll(specialsDiv.select("a[href]").map(::episodeFromElement).reversed())
+        }
+        episodes.addAll(
+            episodesLink.select("div#eps").select("a[href]")
+                .map(::episodeFromElement).reversed(),
+        )
+
+        return episodes.toList()
     }
 
     override fun episodeFromElement(element: Element): SEpisode {
         return SEpisode.create().apply {
             setUrlWithoutDomain(element.attr("href"))
-            val upDate = element.select("div.col-xs-12 > span.label").text()
+            val upDate = element.select("div.col-xs-12 > span.front_time").text().trim()
             date_upload = parseDate(upDate)
-            val epName = element.select("div.col-xs-12 > div.anime-title > b").text()
+            val epName = element.select("div.col-xs-12 > div.anime-title > b").text().trim()
+            val epNum = epName.split(" ").last()
             name = epName
-            episode_number = 0F
+            episode_number = epNum.toFloatOrNull() ?: 0F
         }
     }
 
     // ============================ Video Links =============================
     private val doodExtractor by lazy { DoodExtractor(client) }
     private val mp4uploadExtractor by lazy { Mp4uploadExtractor(client) }
-    private val vidstreamingExtractor by lazy { VidstreamingExtractor(client) }
+    private val gogoStreamExtractor by lazy { GogoStreamExtractor(client) }
     private val filemoonExtractor by lazy { FilemoonExtractor(client) }
 
     override fun videoListParse(response: Response): List<Video> {
@@ -169,7 +181,7 @@ class AnimeTake : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     private fun extractVideo(url: String): List<Video> {
-        val videos = vidstreamingExtractor.videosFromUrl(url, "Vidstreaming - ")
+        val videos = gogoStreamExtractor.videosFromUrl(url)
 
         val request = GET(url)
         val response = client.newCall(request).execute()
