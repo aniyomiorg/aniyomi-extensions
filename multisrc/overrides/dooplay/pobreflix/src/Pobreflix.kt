eedd7e60ec21b623b11e082277923927d339c3e9
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-10-29 12:58:49 -0300

    fix: Fix video extractor + add more extractors

--- multisrc/overrides/dooplay/pobreflix/src/Pobreflix.kt
+++ multisrc/overrides/dooplay/pobreflix/src/Pobreflix.kt
@@ -1,8 +1,11 @@
 package eu.kanade.tachiyomi.animeextension.pt.pobreflix
 
 import android.util.Base64
+import eu.kanade.tachiyomi.animeextension.pt.pobreflix.extractors.EplayerExtractor
+import eu.kanade.tachiyomi.animeextension.pt.pobreflix.extractors.MyStreamExtractor
 import eu.kanade.tachiyomi.animeextension.pt.pobreflix.extractors.PainelfxExtractor
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.filemoonextractor.FilemoonExtractor
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
@@ -21,19 +24,36 @@ class Pobreflix : DooPlay(
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/series/page/$page/", headers)
 
     // ============================ Video Links =============================
+    private val painelfxExtractor by lazy { PainelfxExtractor(client, headers, ::genericExtractor) }
+    private val eplayerExtractor by lazy { EplayerExtractor(client) }
+    private val filemoonExtractor by lazy { FilemoonExtractor(client) }
+    private val mystreamExtractor by lazy { MyStreamExtractor(client, headers) }
+
     override fun videoListParse(response: Response): List<Video> {
         val doc = response.use { it.asJsoup() }
         return doc.select("div.source-box > a").flatMap {
-            val data = it.attr("href").toHttpUrl().queryParameter("auth")
-                ?.let { Base64.decode(it, Base64.DEFAULT) }
-                ?.let(::String)
-                ?: return@flatMap emptyList()
-            val url = data.replace("\\", "").substringAfter("url\":\"").substringBefore('"')
-            when {
-                url.contains("painelfx") ->
-                    PainelfxExtractor(client).videosFromUrl(url, headers)
-                else -> emptyList()
-            }
+            runCatching {
+                val data = it.attr("href").toHttpUrl().queryParameter("auth")
+                    ?.let { Base64.decode(it, Base64.DEFAULT) }
+                    ?.let(::String)
+                    ?: return@flatMap emptyList()
+                val url = data.replace("\\", "").substringAfter("url\":\"").substringBefore('"')
+                when {
+                    url.contains("painelfx") ->
+                        painelfxExtractor.videosFromUrl(url)
+                    else -> genericExtractor(url)
+                }
+            }.getOrElse { emptyList() }
+        }
+    }
+
+    private fun genericExtractor(url: String, language: String = ""): List<Video> {
+        return when {
+            url.contains("filemoon") -> filemoonExtractor.videosFromUrl(url, headers = headers)
+            url.contains("watch.brplayer") || url.contains("/watch?v=") ->
+                mystreamExtractor.videosFromUrl(url, language)
+            url.contains("embedplayer") -> eplayerExtractor.videosFromUrl(url, language)
+            else -> emptyList()
         }
     }
 }
