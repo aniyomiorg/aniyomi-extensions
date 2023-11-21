c13c3afec4a9a24db67a5e57c062449bca0f253e
Secozzi <folke.steen85@gmail.com>
2023-11-21 10:38:46 +0100

    fix(en/aniwave): fix vidsrcextractor

--- src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/AniwaveUtils.kt
+++ src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/AniwaveUtils.kt
@@ -1,56 +1,18 @@
 package eu.kanade.tachiyomi.animeextension.en.nineanime
 
 import android.util.Base64
-import eu.kanade.tachiyomi.AppInfo
-import eu.kanade.tachiyomi.animeextension.BuildConfig
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.POST
-import kotlinx.serialization.json.Json
-import okhttp3.FormBody
-import okhttp3.Headers
-import okhttp3.OkHttpClient
-import okhttp3.Response
-import uy.kohesive.injekt.injectLazy
 import java.net.URLDecoder
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 
-class AniwaveUtils(private val client: OkHttpClient, private val headers: Headers) {
+class AniwaveUtils {
 
-    val json: Json by injectLazy()
-
-    private val userAgent = Headers.headersOf(
-        "User-Agent",
-        "Aniyomi/${AppInfo.getVersionName()} (AniWave; ${BuildConfig.VERSION_CODE})",
-    )
-
-    fun callEnimax(query: String, action: String): String {
-        return if (action in listOf("rawVizcloud", "rawMcloud")) {
-            val referer = if (action == "rawVizcloud") "https://vidstream.pro/" else "https://mcloud.to/"
-            val futoken = client.newCall(
-                GET(referer + "futoken", headers),
-            ).execute().use { it.body.string() }
-            val formBody = FormBody.Builder()
-                .add("query", query)
-                .add("futoken", futoken)
-                .build()
-            client.newCall(
-                POST(
-                    url = "https://9anime.eltik.net/$action?apikey=aniyomi",
-                    body = formBody,
-                    headers = userAgent,
-                ),
-            ).execute().parseAs<RawResponse>().rawURL
-        } else if (action == "decrypt") {
+    fun getVrf(query: String, action: String): String {
+        return if (action == "decrypt") {
             vrfDecrypt(query)
         } else {
             "vrf=${java.net.URLEncoder.encode(vrfEncrypt(query), "utf-8")}"
         }
-    }
-
-    private inline fun <reified T> Response.parseAs(): T {
-        val responseBody = use { it.body.string() }
-        return json.decodeFromString(responseBody)
     }
 
     private fun vrfEncrypt(input: String): String {
--- src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/extractors/VidsrcExtractor.kt
+++ src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/extractors/VidsrcExtractor.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.en.nineanime.extractors
 
-import eu.kanade.tachiyomi.animeextension.en.nineanime.AniwaveUtils
+import android.util.Base64
+import app.cash.quickjs.QuickJs
 import eu.kanade.tachiyomi.animeextension.en.nineanime.MediaResponseBody
 import eu.kanade.tachiyomi.animesource.model.Track
 import eu.kanade.tachiyomi.animesource.model.Video
@@ -12,39 +13,100 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
+import java.net.URLDecoder
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 class VidsrcExtractor(private val client: OkHttpClient, private val headers: Headers) {
 
     private val json: Json by injectLazy()
 
-    private val utils by lazy { AniwaveUtils(client, headers) }
-
     private val playlistUtils by lazy { PlaylistUtils(client, headers) }
 
     fun videosFromUrl(embedLink: String, name: String, type: String): List<Video> {
-        val vidId = embedLink.substringAfterLast("/").substringBefore("?")
-        val (serverName, action) = when (name) {
-            "vidplay" -> Pair("VidPlay", "rawVizcloud")
-            "mycloud" -> Pair("MyCloud", "rawMcloud")
-            else -> return emptyList()
+        val hosterName = when (name) {
+            "vidplay" -> "VidPlay"
+            else -> "MyCloud"
         }
-        val rawURL = utils.callEnimax(vidId, action) + "?${embedLink.substringAfter("?")}"
-        val rawReferer = Headers.headersOf(
-            "referer",
-            "$embedLink&autostart=true",
-            "x-requested-with",
-            "XMLHttpRequest",
-        )
-        val rawResponse = client.newCall(GET(rawURL, rawReferer)).execute().parseAs<MediaResponseBody>()
-        val playlistUrl = rawResponse.result.sources.first().file
-            .replace("#.mp4", "")
+
+        val host = embedLink.toHttpUrl().host
+        val params = embedLink.toHttpUrl().let { url ->
+            url.queryParameterNames.map {
+                Pair(it, url.queryParameter(it) ?: "")
+            }
+        }
+        val vidId = embedLink.substringAfterLast("/").substringBefore("?")
+        val encodedID = encodeID(vidId)
+        val apiSlug = callFromFuToken(host, encodedID)
+
+        val apiUrl = buildString {
+            append("https://")
+            append(host)
+            append("/")
+            append(apiSlug)
+            if (params.isNotEmpty()) {
+                append("?")
+                append(
+                    params.joinToString("&") {
+                        "${it.first}=${it.second}"
+                    },
+                )
+            }
+        }
+
+        val apiHeaders = headers.newBuilder().apply {
+            add("Accept", "application/json, text/javascript, */*; q=0.01")
+            add("Host", host)
+            add("Referer", URLDecoder.decode(embedLink, "UTF-8"))
+            add("X-Requested-With", "XMLHttpRequest")
+        }.build()
+
+        val response = client.newCall(
+            GET(apiUrl, apiHeaders),
+        ).execute().parseAs<MediaResponseBody>()
 
         return playlistUtils.extractFromHls(
-            playlistUrl,
-            referer = "https://${embedLink.toHttpUrl().host}/",
-            videoNameGen = { q -> "$serverName - $type - $q" },
-            subtitleList = rawResponse.result.tracks.toTracks(),
+            response.result.sources.first().file,
+            referer = "https://$host/",
+            videoNameGen = { q -> "$hosterName - $type - $q" },
+            subtitleList = response.result.tracks.toTracks(),
         )
+    }
+
+    private fun encodeID(videoID: String): String {
+        val rc4Key1 = SecretKeySpec("mfofdrbsTyNVOFwp".toByteArray(), "RC4")
+        val rc4Key2 = SecretKeySpec("Jl9tRew2GnsCaR0I".toByteArray(), "RC4")
+        val cipher1 = Cipher.getInstance("RC4")
+        val cipher2 = Cipher.getInstance("RC4")
+        cipher1.init(Cipher.DECRYPT_MODE, rc4Key1, cipher1.parameters)
+        cipher2.init(Cipher.DECRYPT_MODE, rc4Key2, cipher2.parameters)
+        var encoded = videoID.toByteArray()
+
+        encoded = cipher1.doFinal(encoded)
+        encoded = cipher2.doFinal(encoded)
+        encoded = Base64.encode(encoded, Base64.DEFAULT)
+        return encoded.toString(Charsets.UTF_8).replace("/", "_").trim()
+    }
+
+    private fun callFromFuToken(host: String, data: String): String {
+        val fuTokenScript = client.newCall(
+            GET("https://$host/futoken"),
+        ).execute().use { it.body.string() }
+
+        val js = buildString {
+            append("(function")
+            append(
+                fuTokenScript.substringAfter("requestInfo")
+                    .substringAfter("function")
+                    .replace("jQuery.ajax(", "")
+                    .substringBefore("+location.search"),
+            )
+            append("}(\"$data\"))")
+        }
+
+        return QuickJs.create().use {
+            it.evaluate(js)?.toString()!!
+        }
     }
 
     private inline fun <reified T> Response.parseAs(): T {
