d60ff529e23633b69080ba913acc24b91dd5a46a
Secozzi <49240133+Secozzi@users.noreply.github.com>
2023-11-29 15:38:28 +0000

    feat(en/allanimechi): Add option to use hoster names & fix gogo (#2575)

--- src/en/allanimechi/src/eu/kanade/tachiyomi/animeextension/en/allanimechi/extractors/InternalExtractor.kt
+++ src/en/allanimechi/src/eu/kanade/tachiyomi/animeextension/en/allanimechi/extractors/InternalExtractor.kt
@@ -9,11 +9,13 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
+import java.util.Locale
 
-class InternalExtractor(private val client: OkHttpClient, private val apiHeaders: Headers) {
+class InternalExtractor(private val client: OkHttpClient, private val apiHeaders: Headers, private val headers: Headers) {
 
     private val blogUrl = "aHR0cHM6Ly9ibG9nLmFsbGFuaW1lLnBybw==".decodeBase64()
 
@@ -22,11 +24,11 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
         "Dalvik/2.1.0 (Linux; U; Android 13; Pixel 5 Build/TQ3A.230705.001.B4)",
     )
 
-    private val playlistUtils by lazy { PlaylistUtils(client, playlistHeaders) }
+    private val playlistUtils by lazy { PlaylistUtils(client, headers) }
 
     private val json: Json by injectLazy()
 
-    fun videosFromServer(server: AllAnimeChi.Server, removeRaw: Boolean): List<Pair<Video, Float>> {
+    fun videosFromServer(server: AllAnimeChi.Server, useHosterName: Boolean, removeRaw: Boolean): List<Pair<Video, Float>> {
         val blogHeaders = apiHeaders.newBuilder().apply {
             set("host", blogUrl.toHttpUrl().host)
         }.build()
@@ -37,8 +39,8 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
 
         val videoList = videoData.links.flatMap {
             when {
-                it.hls == true -> getFromHls(server, it, removeRaw)
-                it.mp4 == true -> getFromMp4(server, it)
+                it.hls == true -> getFromHls(server, it, useHosterName, removeRaw)
+                it.mp4 == true -> getFromMp4(server, it, useHosterName)
                 else -> emptyList()
             }
         }
@@ -46,20 +48,26 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
         return videoList
     }
 
-    private fun getFromMp4(server: AllAnimeChi.Server, data: VideoData.LinkObject): List<Pair<Video, Float>> {
-        val baseName = "${server.sourceName} - ${data.resolutionStr}"
+    private fun getFromMp4(server: AllAnimeChi.Server, data: VideoData.LinkObject, useHosterName: Boolean): List<Pair<Video, Float>> {
+        val host = if (useHosterName) getHostName(data.link, server.sourceName) else server.sourceName
+
+        val baseName = "$host - ${data.resolutionStr}"
         val video = Video(data.link, baseName, data.link, headers = playlistHeaders)
         return listOf(
             Pair(video, server.priority),
         )
     }
 
-    private fun getFromHls(server: AllAnimeChi.Server, data: VideoData.LinkObject, removeRaw: Boolean): List<Pair<Video, Float>> {
+    private fun getFromHls(server: AllAnimeChi.Server, data: VideoData.LinkObject, useHosterName: Boolean, removeRaw: Boolean): List<Pair<Video, Float>> {
         if (removeRaw && data.resolutionStr.contains("raw", true)) return emptyList()
+        val host = if (useHosterName) getHostName(data.link, server.sourceName) else server.sourceName
 
         val linkHost = data.link.toHttpUrl().host
+
         // Doesn't seem to work
-        if (server.sourceName.equals("Luf-mp4", true) && linkHost.contains("maverickki")) return emptyList()
+        if (server.sourceName.equals("Luf-mp4", true)) {
+            return getFromGogo(server, data, host)
+        }
 
         // Hardcode some names
         val baseName = if (linkHost.contains("crunchyroll")) {
@@ -73,8 +81,7 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
                 .replace("vo_SUB", "Sub")
                 .replace("SUB", "Sub")
         } else {
-            "${server.sourceName} - ${data.resolutionStr}"
-                .replace("Luf-mp4", "Luf-mp4 (gogo)")
+            "$host - ${data.resolutionStr}"
         }
 
         // Get stuff
@@ -91,7 +98,7 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
 
         val videoList = playlistUtils.extractFromHls(
             data.link,
-            videoNameGen = { q -> "$baseName - $q" },
+            videoNameGen = { q -> "$baseName - ${data.resolutionStr} - $q" },
             masterHeadersGen = { _, _ -> masterHeaders },
         )
 
@@ -100,7 +107,32 @@ class InternalExtractor(private val client: OkHttpClient, private val apiHeaders
         }
     }
 
+    private fun getFromGogo(server: AllAnimeChi.Server, data: VideoData.LinkObject, hostName: String): List<Pair<Video, Float>> {
+        val host = data.link.toHttpUrl().host
+
+        // Seems to be dead
+        if (host.contains("maverickki", true)) return emptyList()
+
+        val videoList = playlistUtils.extractFromHls(
+            data.link,
+            videoNameGen = { q -> "$hostName - ${data.resolutionStr} - $q" },
+            referer = "https://playtaku.net/",
+        )
+
+        return videoList.map {
+            Pair(it, server.priority)
+        }
+    }
+
     // ============================= Utilities ==============================
+
+    private fun getHostName(host: String, fallback: String): String {
+        return host.toHttpUrlOrNull()?.host?.split(".")?.let {
+            it.getOrNull(it.size - 2)?.replaceFirstChar { c ->
+                if (c.isLowerCase()) c.titlecase(Locale.ROOT) else c.toString()
+            }
+        } ?: fallback
+    }
 
     @Serializable
     data class VideoData(
