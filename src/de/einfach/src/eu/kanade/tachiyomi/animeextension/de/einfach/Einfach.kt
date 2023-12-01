584a5856a397342e56fd99778a0a4aa35ecbff37
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-01 11:21:58 -0300

    feat: create Einfach base

---
+++ src/de/einfach/src/eu/kanade/tachiyomi/animeextension/de/einfach/Einfach.kt
@@ -0,0 +1,129 @@
+package eu.kanade.tachiyomi.animeextension.de.einfach
+
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
+
+class Einfach : ParsedAnimeHttpSource() {
+
+    override val name = "Einfach"
+
+    override val baseUrl = "https://einfach.to"
+
+    override val lang = "de"
+
+    override val supportsLatest = false
+
+    // ============================== Popular ===============================
+    override fun popularAnimeRequest(page: Int): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun popularAnimeSelector(): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun popularAnimeFromElement(element: Element): SAnime {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun popularAnimeNextPageSelector(): String? {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesRequest(page: Int): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun latestUpdatesSelector(): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SAnime {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =============================== Search ===============================
+    override fun fetchSearchAnime(page: Int, query: String, filters: AnimeFilterList): Observable<AnimesPage> {
+        return if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
+            val path = query.removePrefix(PREFIX_SEARCH)
+            client.newCall(GET("$baseUrl/$path"))
+                .asObservableSuccess()
+                .map(::searchAnimeByPathParse)
+        } else {
+            super.fetchSearchAnime(page, query, filters)
+        }
+    }
+
+    private fun searchAnimeByPathParse(response: Response): AnimesPage {
+        val details = animeDetailsParse(response.use { it.asJsoup() })
+        return AnimesPage(listOf(details), false)
+    }
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun searchAnimeSelector(): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun searchAnimeFromElement(element: Element): SAnime {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun searchAnimeNextPageSelector(): String? {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // =========================== Anime Details ============================
+    override fun animeDetailsParse(document: Document): SAnime {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================== Episodes ==============================
+    override fun episodeListSelector(): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun episodeFromElement(element: Element): SEpisode {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    // ============================ Video Links =============================
+    override fun videoListParse(response: Response): List<Video> {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun videoListSelector(): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun videoFromElement(element: Element): Video {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    override fun videoUrlParse(document: Document): String {
+        throw UnsupportedOperationException("Not used.")
+    }
+
+    companion object {
+        const val PREFIX_SEARCH = "path:"
+    }
+}
