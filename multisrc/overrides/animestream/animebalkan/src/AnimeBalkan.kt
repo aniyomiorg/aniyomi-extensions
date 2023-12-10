8693ee3b274a5326a902c01506d605f445db91b9
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-10 17:55:57 -0300

    feat: Add GoogleDrive Extractor
    thx dr sexxco, now i hate google even more!!!!!

--- multisrc/overrides/animestream/animebalkan/src/AnimeBalkan.kt
+++ multisrc/overrides/animestream/animebalkan/src/AnimeBalkan.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.animeextension.sr.animebalkan
 import android.util.Log
 import eu.kanade.tachiyomi.animeextension.sr.animebalkan.extractors.MailRuExtractor
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.googledriveextractor.GoogleDriveExtractor
 import eu.kanade.tachiyomi.lib.okruextractor.OkruExtractor
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
 import eu.kanade.tachiyomi.network.GET
@@ -31,8 +32,9 @@ class AnimeBalkan : AnimeStream(
         return super.getHosterUrl(element)
     }
 
-    private val okruExtractor by lazy { OkruExtractor(client) }
+    private val gdriveExtractor by lazy { GoogleDriveExtractor(client, headers) }
     private val mailruExtractor by lazy { MailRuExtractor(client, headers) }
+    private val okruExtractor by lazy { OkruExtractor(client) }
 
     override fun getVideoList(url: String, name: String): List<Video> {
         Log.i(name, "getVideoList -> URL => $url || Name => $name")
@@ -40,6 +42,17 @@ class AnimeBalkan : AnimeStream(
         return when {
             "Server OK" in name || "ok.ru" in url -> okruExtractor.videosFromUrl(url)
             "Server Ru" in name || "mail.ru" in url -> mailruExtractor.videosFromUrl(url)
+            "Server GD" in name || "google.com" in url -> {
+                // We need to do that bc the googledrive extractor is garbage.
+                val newUrl = when {
+                    url.contains("uc?id=") -> url
+                    else -> {
+                        val id = url.substringAfter("/d/").substringBefore("/")
+                        "https://drive.google.com/uc?id=$id"
+                    }
+                }
+                gdriveExtractor.videosFromUrl(newUrl)
+            }
             "Server AB" in name && baseUrl in url -> {
                 val doc = client.newCall(GET(url)).execute().use { it.asJsoup() }
                 val videoUrl = doc.selectFirst("source")?.attr("src")
