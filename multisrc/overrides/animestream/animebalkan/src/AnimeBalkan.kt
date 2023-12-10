16bf17c18388f530ed33112c81229d1873c6caaf
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-10 14:05:10 -0300

    fix: Fix dateFormatter

--- multisrc/overrides/animestream/animebalkan/src/AnimeBalkan.kt
+++ multisrc/overrides/animestream/animebalkan/src/AnimeBalkan.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.animeextension.sr.animebalkan
 
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class AnimeBalkan : AnimeStream(
     "sr",
@@ -8,4 +10,8 @@ class AnimeBalkan : AnimeStream(
     "https://animebalkan.org",
 ) {
     override val animeListUrl = "$baseUrl/animesaprevodom"
+
+    override val dateFormatter by lazy {
+        SimpleDateFormat("MMMM d, yyyy", Locale("bs")) // YES, Bosnian
+    }
 }
