5ac2498f3c249b90c5291081dcab7c9ea9b2a32c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-12 00:34:30 -0300

    feat: Set entry status correctly

--- multisrc/overrides/dooplay/voircartoon/src/VoirCartoon.kt
+++ multisrc/overrides/dooplay/voircartoon/src/VoirCartoon.kt
@@ -1,7 +1,9 @@
 package eu.kanade.tachiyomi.animeextension.fr.voircartoon
 
+import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
+import org.jsoup.nodes.Document
 
 class VoirCartoon : DooPlay(
     "fr",
@@ -20,4 +22,21 @@ class VoirCartoon : DooPlay(
 
     // =============================== Search ===============================
     override fun searchAnimeNextPageSelector() = popularAnimeNextPageSelector()
+
+    // =========================== Anime Details ============================
+    override fun animeDetailsParse(document: Document) =
+        super.animeDetailsParse(document).apply {
+            val statusText = document.selectFirst("div.mvic-info p:contains(Status:) > a[rel]")
+                ?.text()
+                .orEmpty()
+            status = parseStatus(statusText)
+        }
+
+    private fun parseStatus(status: String): Int {
+        return when (status) {
+            "Ongoing" -> SAnime.ONGOING
+            "Completed" -> SAnime.COMPLETED
+            else -> SAnime.UNKNOWN
+        }
+    }
 }
