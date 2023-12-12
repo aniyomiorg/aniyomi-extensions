e4521d570f05d471071b37dd05e45a38a4c03409
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-12 00:22:41 -0300

    fix: Fix popular anime page

--- multisrc/overrides/dooplay/voircartoon/src/VoirCartoon.kt
+++ multisrc/overrides/dooplay/voircartoon/src/VoirCartoon.kt
@@ -1,9 +1,17 @@
 package eu.kanade.tachiyomi.animeextension.fr.voircartoon
 
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
+import eu.kanade.tachiyomi.network.GET
 
 class VoirCartoon : DooPlay(
     "fr",
     "VoirCartoon",
     "https://voircartoon.com",
-)
+) {
+    // ============================== Popular ===============================
+    override fun popularAnimeRequest(page: Int) = GET("$baseUrl/tendance/page/$page/", headers)
+
+    override fun popularAnimeSelector() = latestUpdatesSelector()
+
+    override fun popularAnimeNextPageSelector() = "div.pagination a.arrow_pag > i#nextpagination"
+}
