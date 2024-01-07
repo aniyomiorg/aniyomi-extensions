4ddadd91951e47d5a4870a784c9ff018f40337fc
fakebhai <fake42867@gmail.com>
2024-01-07 17:14:48 +0530

    replace aniwave domain

--- src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/Aniwave.kt
+++ src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/nineanime/Aniwave.kt
@@ -380,8 +380,8 @@ class Aniwave : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         ListPreference(screen.context).apply {
             key = PREF_DOMAIN_KEY
             title = "Preferred domain"
-            entries = arrayOf("aniwave.to", "aniwave.bz", "aniwave.ws", "aniwave.vc")
-            entryValues = arrayOf("https://aniwave.to", "https://aniwave.bz", "https://aniwave.ws", "https://aniwave.vc")
+            entries = arrayOf("aniwave.to", "aniwave.li", "aniwave.ws", "aniwave.vc")
+            entryValues = arrayOf("https://aniwave.to", "https://aniwave.li", "https://aniwave.ws", "https://aniwave.vc")
             setDefaultValue(PREF_DOMAIN_DEFAULT)
             summary = "%s"
 
