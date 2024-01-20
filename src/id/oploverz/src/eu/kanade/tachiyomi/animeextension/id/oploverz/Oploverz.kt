2392762c40e0825ac9bcb625dde5cc959792fcca
Willy Nur Wahyudi <Willynurwahyudi@gmail.Com>
2024-01-20 19:39:00 +0700

    fix(id/oploverz): Update baseUrl (#2796)

--- src/id/oploverz/src/eu/kanade/tachiyomi/animeextension/id/oploverz/Oploverz.kt
+++ src/id/oploverz/src/eu/kanade/tachiyomi/animeextension/id/oploverz/Oploverz.kt
@@ -28,7 +28,7 @@ import java.util.Locale
 
 class Oploverz : ConfigurableAnimeSource, AnimeHttpSource() {
     override val name: String = "Oploverz"
-    override val baseUrl: String = "https://oploverz.cool"
+    override val baseUrl: String = "https://oploverz.wiki"
     override val lang: String = "id"
     override val supportsLatest: Boolean = true
 
