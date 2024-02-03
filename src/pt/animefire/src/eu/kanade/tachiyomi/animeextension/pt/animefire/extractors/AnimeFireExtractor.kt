2c71fbecb710806f5c9cd8bb995d142913e6379a
Edgard Lorraine Messias <edgardmessias@gmail.com>
2024-02-03 13:59:44 -0300

    fix(pt/animefire): Fix http 401 + update domain (#2861)

--- src/pt/animefire/src/eu/kanade/tachiyomi/animeextension/pt/animefire/extractors/AnimeFireExtractor.kt
+++ src/pt/animefire/src/eu/kanade/tachiyomi/animeextension/pt/animefire/extractors/AnimeFireExtractor.kt
@@ -4,19 +4,20 @@ import eu.kanade.tachiyomi.animeextension.pt.animefire.dto.AFResponseDto
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.network.GET
 import kotlinx.serialization.json.Json
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 import org.jsoup.nodes.Element
 
 class AnimeFireExtractor(private val client: OkHttpClient, private val json: Json) {
 
-    fun videoListFromElement(videoElement: Element): List<Video> {
+    fun videoListFromElement(videoElement: Element, headers: Headers): List<Video> {
         val jsonUrl = videoElement.attr("data-video-src")
         val response = client.newCall(GET(jsonUrl)).execute()
             .body.string()
         val responseDto = json.decodeFromString<AFResponseDto>(response)
         return responseDto.videos.map {
             val url = it.url.replace("\\", "")
-            Video(url, it.quality, url)
+            Video(url, it.quality, url, headers = headers)
         }
     }
 }
