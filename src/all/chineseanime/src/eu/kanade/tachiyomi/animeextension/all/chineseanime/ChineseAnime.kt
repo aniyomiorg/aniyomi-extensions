0f4c6ecca28a6b2ab1a0a942a1b65ca42616a9d5
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-03-10 13:31:52 -0300

    feat: Add StreamVid extractor

--- src/all/chineseanime/src/eu/kanade/tachiyomi/animeextension/all/chineseanime/ChineseAnime.kt
+++ src/all/chineseanime/src/eu/kanade/tachiyomi/animeextension/all/chineseanime/ChineseAnime.kt
@@ -5,6 +5,7 @@ import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animeextension.all.chineseanime.extractors.VatchusExtractor
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.dailymotionextractor.DailymotionExtractor
+import eu.kanade.tachiyomi.lib.streamvidextractor.StreamVidExtractor
 import eu.kanade.tachiyomi.lib.streamwishextractor.StreamWishExtractor
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
 
@@ -26,14 +27,17 @@ class ChineseAnime : AnimeStream(
     // ============================ Video Links =============================
     private val dailymotionExtractor by lazy { DailymotionExtractor(client, headers) }
     private val streamwishExtractor by lazy { StreamWishExtractor(client, headers) }
+    private val streamvidExtractor by lazy { StreamVidExtractor(client) }
     private val vatchusExtractor by lazy { VatchusExtractor(client, headers) }
 
     override fun getVideoList(url: String, name: String): List<Video> {
         val prefix = "$name - "
+        println("$prefix$url")
         return when {
             url.contains("dailymotion") -> dailymotionExtractor.videosFromUrl(url, prefix)
             url.contains("embedwish") -> streamwishExtractor.videosFromUrl(url, prefix)
             url.contains("vatchus") -> vatchusExtractor.videosFromUrl(url, prefix)
+            url.contains("donghua.xyz/v/") -> streamvidExtractor.videosFromUrl(url, prefix, true)
             else -> emptyList()
         }
     }
