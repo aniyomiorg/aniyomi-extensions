f4a9a99ee2ac9f42b99a17eaf3cb0b26fa8fa3f0
imper1aldev <23511335+imper1aldev@users.noreply.github.com>
2024-04-06 12:34:37 -0600

    Unnecessary properties removed

--- src/es/doramasflix/src/eu/kanade/tachiyomi/animeextension/es/doramasflix/DataModel.kt
+++ src/es/doramasflix/src/eu/kanade/tachiyomi/animeextension/es/doramasflix/DataModel.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.animeextension.es.doramasflix
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonObject
 
 // -----------------------Season models------------------------//
 @Serializable
@@ -27,7 +26,6 @@ data class ListSeason(
     @SerialName("serie_name")
     val serieName: String?,
     val poster: String?,
-    val backdrop: String?,
     @SerialName("__typename")
     val typename: String,
 )
@@ -53,48 +51,15 @@ data class ListEpisode(
     val serieName: String?,
     @SerialName("serie_name_es")
     val serieNameEs: String?,
-    @SerialName("serie_id")
-    val serieId: String?,
-    @SerialName("still_path")
-    val stillPath: String?,
     @SerialName("air_date")
     val airDate: String?,
     @SerialName("season_number")
     val seasonNumber: Long?,
     @SerialName("episode_number")
     val episodeNumber: Long?,
-    // val languages: List<Any?>,
     val poster: String?,
-    val backdrop: String?,
     @SerialName("__typename")
     val typename: String,
-)
-
-// -----------------------Details Model------------------------//
-
-data class DetailsModel(
-    val props: Props,
-    val page: String,
-    val query: Query,
-    val buildId: String,
-    val isFallback: Boolean,
-    val gip: Boolean,
-)
-
-data class Props(
-    val pageProps: PageProps,
-)
-
-data class PageProps(
-    val deviceType: String,
-    val slug: String,
-    // val apolloClient: Any?,
-    val apolloState: HashMap<String, HashMap<String, JsonObject>>,
-    val ssrComplete: Boolean,
-)
-
-data class Query(
-    val slug: String,
 )
 
 // -----------------------Pagination Model------------------------//
@@ -136,77 +101,18 @@ data class Item(
     @SerialName("name_es")
     val nameEs: String?,
     val slug: String,
-    val cast: List<Cast> = emptyList(),
     val names: String?,
     val overview: String?,
-    val languages: List<String?>? = emptyList(),
-    @SerialName("created_by")
-    val createdBy: List<CreatedBy> = emptyList(),
-    val popularity: Double?,
     @SerialName("poster_path")
     val posterPath: String?,
-    @SerialName("backdrop_path")
-    val backdropPath: String?,
-    @SerialName("first_air_date")
-    val firstAirDate: String?,
-    @SerialName("isTVShow")
-    val isTvshow: Boolean?,
     val poster: String?,
-    val backdrop: String?,
     val genres: List<Genre> = emptyList(),
-    val networks: List<Network> = emptyList(),
     @SerialName("__typename")
     val typename: String,
 )
 
 @Serializable
-data class Cast(
-    val adult: Boolean?,
-    val gender: Long?,
-    val id: Long?,
-    @SerialName("known_for_department")
-    val knownForDepartment: String?,
-    val name: String?,
-    @SerialName("original_name")
-    val originalName: String?,
-    val popularity: Double?,
-    @SerialName("profile_path")
-    val profilePath: String?,
-    val character: String?,
-    @SerialName("credit_id")
-    val creditId: String?,
-    val order: Long?,
-)
-
-@Serializable
-data class CreatedBy(
-    val adult: Boolean?,
-    val gender: Long?,
-    val id: Long?,
-    @SerialName("known_for_department")
-    val knownForDepartment: String?,
-    val name: String?,
-    @SerialName("original_name")
-    val originalName: String?,
-    val popularity: Double?,
-    @SerialName("profile_path")
-    val profilePath: String?,
-    @SerialName("credit_id")
-    val creditId: String?,
-    val department: String?,
-    val job: String?,
-)
-
-@Serializable
 data class Genre(
-    val name: String?,
-    val slug: String?,
-    @SerialName("__typename")
-    val typename: String?,
-)
-
-@Serializable
-data class Network(
     val name: String?,
     val slug: String?,
     @SerialName("__typename")
@@ -241,18 +147,6 @@ data class SearchDorama(
 )
 
 // -------------------------------------------------------
-@Serializable
-data class VideoModel(
-    val json: JsonVideo = JsonVideo(),
-)
-
-@Serializable
-data class JsonVideo(
-    val lang: String? = "",
-    val page: String? = "",
-    val link: String? = "",
-    val server: String? = "",
-)
 
 @Serializable
 data class VideoToken(
@@ -274,7 +168,6 @@ data class TokenModel(
     val isFallback: Boolean? = false,
     val isExperimentalCompile: Boolean? = false,
     val gssp: Boolean? = false,
-    // val scriptLoader: List<Any?>,
 )
 
 @Serializable
