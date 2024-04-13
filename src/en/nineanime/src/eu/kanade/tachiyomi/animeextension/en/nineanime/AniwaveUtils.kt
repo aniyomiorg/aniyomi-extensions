d367fcfecc1f2ba5658779dcfb5aae83baded3d8
RowdyRushya <66415100+rushi-chavan@users.noreply.github.com>
2024-04-13 16:50:47 -0700

    fix(en/nineanime): Fix keys and encryption algorithm

--- src/en/nineanime/src/eu/kanade/tachiyomi/animeextension/en/nineanime/AniwaveUtils.kt
+++ src/en/nineanime/src/eu/kanade/tachiyomi/animeextension/en/nineanime/AniwaveUtils.kt
@@ -8,7 +8,7 @@ import javax.crypto.spec.SecretKeySpec
 class AniwaveUtils {
 
     fun vrfEncrypt(input: String): String {
-        val rc4Key = SecretKeySpec("ysJhV6U27FVIjjuk".toByteArray(), "RC4")
+        val rc4Key = SecretKeySpec("tGn6kIpVXBEUmqjD".toByteArray(), "RC4")
         val cipher = Cipher.getInstance("RC4")
         cipher.init(Cipher.DECRYPT_MODE, rc4Key, cipher.parameters)
 
@@ -16,8 +16,9 @@ class AniwaveUtils {
         vrf = Base64.encode(vrf, Base64.URL_SAFE or Base64.NO_WRAP)
         vrf = Base64.encode(vrf, Base64.DEFAULT or Base64.NO_WRAP)
         vrf = vrfShift(vrf)
-        vrf = Base64.encode(vrf, Base64.DEFAULT)
-        vrf = rot13(vrf)
+        vrf = vrf.reversed().toByteArray()
+        vrf = Base64.encode(vrf, Base64.URL_SAFE or Base64.NO_WRAP)
+        //vrf = rot13(vrf)
         val stringVrf = vrf.toString(Charsets.UTF_8)
         return "vrf=${java.net.URLEncoder.encode(stringVrf, "utf-8")}"
     }
@@ -26,7 +27,7 @@ class AniwaveUtils {
         var vrf = input.toByteArray()
         vrf = Base64.decode(vrf, Base64.URL_SAFE)
 
-        val rc4Key = SecretKeySpec("hlPeNwkncH0fq9so".toByteArray(), "RC4")
+        val rc4Key = SecretKeySpec("LUyDrL4qIxtIxOGs".toByteArray(), "RC4")
         val cipher = Cipher.getInstance("RC4")
         cipher.init(Cipher.DECRYPT_MODE, rc4Key, cipher.parameters)
         vrf = cipher.doFinal(vrf)
@@ -48,7 +49,7 @@ class AniwaveUtils {
 
     private fun vrfShift(vrf: ByteArray): ByteArray {
         for (i in vrf.indices) {
-            val shift = arrayOf(-3, 3, -4, 2, -2, 5, 4, 5)[i % 8]
+            val shift = arrayOf(-2, -4, -5, 6, 2, -3, 3, 6)[i % 8]
             vrf[i] = vrf[i].plus(shift).toByte()
         }
         return vrf
