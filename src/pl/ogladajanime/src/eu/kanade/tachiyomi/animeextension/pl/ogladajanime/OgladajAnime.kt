c823b9522d1a04213e4ecdf44c8b3cb2ebb3c3dc
Hayanek <kontakt@wolfek.xyz>
2024-05-22 22:36:50 +0200

    Last last Change

--- src/pl/ogladajanime/src/eu/kanade/tachiyomi/animeextension/pl/ogladajanime/OgladajAnime.kt
+++ src/pl/ogladajanime/src/eu/kanade/tachiyomi/animeextension/pl/ogladajanime/OgladajAnime.kt
@@ -60,11 +60,6 @@ class OgladajAnime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     private val preferences: SharedPreferences by lazy {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
-    companion object {
-        private val DATE_FORMATTER by lazy {
-            SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY)
-        }
-    }
 
     // ============================== Popular ===============================
 
