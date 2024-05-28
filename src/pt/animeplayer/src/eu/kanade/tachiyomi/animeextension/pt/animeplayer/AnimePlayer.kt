69e1f1870432afc0e0c7dfa7e8c95066805c1cb4
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-05-28 10:15:15 +0000

    fix(pt/animeplayer): Fix popular anime, anime details and episode list pages (#3271)

--- src/pt/animeplayer/src/eu/kanade/tachiyomi/animeextension/pt/animeplayer/AnimePlayer.kt
+++ src/pt/animeplayer/src/eu/kanade/tachiyomi/animeextension/pt/animeplayer/AnimePlayer.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.pt.animeplayer
 
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.bloggerextractor.BloggerExtractor
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
@@ -7,6 +9,8 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class AnimePlayer : DooPlay(
     "pt-BR",
@@ -15,14 +19,73 @@ class AnimePlayer : DooPlay(
 ) {
 
     // ============================== Popular ===============================
-    override fun popularAnimeSelector() = "div#featured-titles article div.poster"
+    override fun popularAnimeSelector() = "div#archive-content article div.poster"
 
     override fun popularAnimeRequest(page: Int) = GET("$baseUrl/animes/")
+
+    override fun popularAnimeNextPageSelector() = "a > i#nextpagination"
 
     // =============================== Latest ===============================
     override val latestUpdatesPath = "episodios"
 
-    override fun latestUpdatesNextPageSelector() = "a > i#nextpagination"
+    override fun latestUpdatesNextPageSelector() = popularAnimeNextPageSelector()
+
+    // =========================== Anime Details ============================
+    override fun animeDetailsParse(document: Document) = SAnime.create().apply {
+        val doc = getRealAnimeDoc(document)
+        val content = doc.selectFirst("div#contenedor > div.data")!!
+        doc.selectFirst("div.sheader div.poster > img")!!.let {
+            thumbnail_url = it.getImageUrl()
+            title = it.attr("alt").ifEmpty {
+                content.selectFirst("div.data > h1")!!.text()
+            }
+        }
+
+        genre = content.select("div.sgeneros > a")
+            .eachText()
+            .joinToString()
+    }
+
+    // ============================== Episodes ==============================
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val doc = getRealAnimeDoc(response.asJsoup())
+        val seasonList = doc.select(seasonListSelector)
+        return if (seasonList.size < 1) {
+            SEpisode.create().apply {
+                setUrlWithoutDomain(doc.location())
+                episode_number = 1F
+                name = episodeMovieText
+            }.let(::listOf)
+        } else {
+            seasonList.flatMap(::getSeasonEpisodes)
+        }
+    }
+
+    override fun getSeasonEpisodes(season: Element): List<SEpisode> {
+        val seasonName = season.selectFirst("span.title")!!.text()
+        return season.select(episodeListSelector()).mapNotNull { element ->
+            try {
+                episodeFromElement(element, seasonName)
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    override fun episodeFromElement(element: Element): SEpisode = throw UnsupportedOperationException()
+
+    override fun episodeFromElement(element: Element, seasonName: String) = SEpisode.create().apply {
+        val epNum = element.selectFirst("div.episodiotitle p")!!.text()
+            .trim()
+            .let(episodeNumberRegex::find)
+            ?.groupValues
+            ?.last() ?: "0"
+        val href = element.selectFirst("a[href]")!!
+        episode_number = epNum.toFloatOrNull() ?: 0F
+        name = "$seasonName x Episódio $epNum"
+        setUrlWithoutDomain(href.absUrl("href"))
+    }
 
     // ============================ Video Links =============================
     override val prefQualityValues = arrayOf("360p", "720p")
@@ -33,7 +96,7 @@ class AnimePlayer : DooPlay(
     override fun videoListParse(response: Response): List<Video> {
         val playerUrl = response.asJsoup()
             .selectFirst("div.playex iframe")
-            ?.attr("abs:src")
+            ?.absUrl("src")
             ?.toHttpUrlOrNull()
             ?: return emptyList()
 
@@ -43,4 +106,7 @@ class AnimePlayer : DooPlay(
 
     // ============================== Filters ===============================
     override fun genresListSelector() = "ul.genres a"
+
+    // ============================= Utilities ==============================
+    override val animeMenuSelector = "div.pag_episodes div.item a[href] i.icon-bars"
 }
